6dc6377f75e70b775a50e4f5132d665fd87e681e
edeutsch <gasif555@gmail.com>
2002-01-25 22:49:13 +0000

    Updated example to latest standard

--- sbeams/lib/scripts/example.pl
+++ sbeams/lib/scripts/example.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -T
+#!/usr/local/bin/perl -T
 
 ###############################################################################
 # Program     : example.pl
@@ -12,46 +12,80 @@
 
 
 ###############################################################################
-# Get the script set up with everything it will need
+# Generic SBEAMS script setup
 ###############################################################################
 use strict;
-use vars qw ($sbeams $dbh $PROGRAM_FILE_NAME
+use Getopt::Long;
+use vars qw ($sbeams
+             $PROG_NAME $USAGE %OPTIONS $QUIET $VERBOSE $DEBUG $DATABASE
              $current_contact_id $current_username
              $current_work_group_id $current_work_group_name
              $current_project_id $current_project_name);
-use lib qw (../..);
+use lib qw (../perl);
 
+
+#### Set up SBEAMS package
 use SBEAMS::Connection;
 use SBEAMS::Connection::Settings;
 use SBEAMS::Connection::Tables;
 use SBEAMS::Connection::TableInfo;
-
 $sbeams = new SBEAMS::Connection;
 
 
-###############################################################################
-# Global Variables
-###############################################################################
-$PROGRAM_FILE_NAME = 'example.pl';
+#### Set program name and usage banner
+$PROG_NAME = "example.pl";
+$USAGE = <<EOU;
+Usage: $PROG_NAME [OPTIONS] parameters
+Options:
+  --verbose n         Set verbosity level.  default is 0
+  --quiet             Set flag to print nothing at all except errors
+  --debug n           Set debug flag
+  --database xxxx     Database where the default tables are found
+
+ e.g.:  $PROG_NAME foo
+
+EOU
+
+
+#### Process options
+unless (GetOptions(\%OPTIONS,"verbose:s","quiet","debug:s",
+  "database:s")) {
+  print "$USAGE";
+  exit;
+}
+$VERBOSE = $OPTIONS{"verbose"} || 0;
+$QUIET = $OPTIONS{"quiet"} || 0;
+$DEBUG = $OPTIONS{"debug"} || 0;
+if ($DEBUG) {
+  print "Options settings:\n";
+  print "  VERBOSE = $VERBOSE\n";
+  print "  QUIET = $QUIET\n";
+  print "  DEBUG = $DEBUG\n";
+  print "  DBVERSION = $DBVERSION\n";
+}
+
+
+$| = 1;
 main();
+exit 0;
 
 
 ###############################################################################
 # Main Program:
 #
 # Call $sbeams->Authentication and stop immediately if authentication
-# fails else continue.
+# fails else continue, else execute the important part of this script
 ###############################################################################
 sub main { 
 
-    #### Do the SBEAMS authentication and exit if a username is not returned
-    exit unless ($current_username =
-        $sbeams->Authenticate(work_group=>'Array_user'));
+  #### Do the SBEAMS authentication and exit if a username is not returned
+  exit unless ($current_username =
+    $sbeams->Authenticate(work_group=>'Array_user'));
 
-    #### Print the header, do what the program does, and print footer
-    $sbeams->printTextHeader();
-    showMainPage();
-    $sbeams->printTextFooter();
+  #### Print the header, do what the program does, and print footer
+  $sbeams->printTextHeader();
+  showMainPage();
+  $sbeams->printTextFooter();
 
 } # end main
 
@@ -61,21 +95,32 @@ sub main {
 ###############################################################################
 sub showMainPage {
 
-    $current_username = $sbeams->getCurrent_username;
-    $current_contact_id = $sbeams->getCurrent_contact_id;
-    $current_work_group_id = $sbeams->getCurrent_work_group_id;
-    $current_work_group_name = $sbeams->getCurrent_work_group_name;
-    $current_project_id = $sbeams->getCurrent_project_id;
-    $current_project_name = $sbeams->getCurrent_project_name;
-    $dbh = $sbeams->getDBHandle();
+  #### Define standard variables
+  my ($i,$element,$key,$value,$line,$result,$sql);
+
+  $current_username = $sbeams->getCurrent_username;
+  $current_contact_id = $sbeams->getCurrent_contact_id;
+  $current_work_group_id = $sbeams->getCurrent_work_group_id;
+  $current_work_group_name = $sbeams->getCurrent_work_group_name;
+  $current_project_id = $sbeams->getCurrent_project_id;
+  $current_project_name = $sbeams->getCurrent_project_name;
 
 
-    $sbeams->printUserContext(style=>'TEXT');
+  $sbeams->printUserContext(style=>'TEXT');
 
-    print qq!
-	You are successfully logged into the $DBTITLE system.  Your
-	task would go here and do whatever you like.
-    !;
+  print qq!
+      You are successfully logged into the $DBTITLE system.  Your
+      task would go here and do whatever you like.
+  !;
+
+  print "\n\n";
+  $sql = "SELECT organization_id,organization FROM $TB_ORGANIZATION";
+  my %organizations = $sbeams->selectTwoColumnHash($sql);
+
+  while ( ($key,$value) = each %organizations ) {
+    print "$key=$value\n";
+  }
+
 
 } # end showMainPage
 
