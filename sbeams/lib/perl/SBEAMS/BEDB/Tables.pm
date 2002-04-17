fb63eb41f4acfa37e90335af97107c2ffa17d325
edeutsch <gasif555@gmail.com>
2002-04-17 17:42:24 +0000

    Added lots of stuff.  New layout, all tables

--- sbeams/lib/perl/SBEAMS/BEDB/Tables.pm
+++ sbeams/lib/perl/SBEAMS/BEDB/Tables.pm
@@ -20,6 +20,11 @@ use vars qw(@ISA @EXPORT
 
     $TBBE_EST_LIBRARY
     $TBBE_EST
+    $TBBE_EST_STATISTIC
+    $TBBE_EST_SEQUENCE
+    $TBBE_ANNOTATION
+    $TBBE_CONTIG
+    $TBBE_CONTIG_EST
 
     $TBBE_QUERY_OPTION
 
@@ -36,6 +41,11 @@ require Exporter;
 
     $TBBE_EST_LIBRARY
     $TBBE_EST
+    $TBBE_EST_STATISTIC
+    $TBBE_EST_SEQUENCE
+    $TBBE_ANNOTATION
+    $TBBE_CONTIG
+    $TBBE_CONTIG_EST
 
     $TBBE_QUERY_OPTION
 
@@ -48,7 +58,12 @@ $TBBE_BIOSEQUENCE_SET       = 'BEDB.dbo.biosequence_set';
 $TBBE_BIOSEQUENCE           = 'BEDB.dbo.biosequence';
 
 $TBBE_EST_LIBRARY           = 'BEDB.dbo.est_library';
-$TBBE_EST                   = 'BEDB.dbp.est';
+$TBBE_EST                   = 'BEDB.dbo.est';
+$TBBE_EST_STATISTIC         = 'BEDB.dbo.est_statistic';
+$TBBE_EST_SEQUENCE          = 'BEDB.dbo.est_sequence';
+$TBBE_ANNOTATION            = 'BEDB.dbo.annotation';
+$TBBE_CONTIG                = 'BEDB.dbo.contig';
+$TBBE_CONTIG_EST            = 'BEDB.dbo.contig_est';
 
 $TBBE_QUERY_OPTION          = 'BEDB.dbo.query_option';
 
