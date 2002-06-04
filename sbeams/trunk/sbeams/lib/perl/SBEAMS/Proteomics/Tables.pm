698f27b3688d5736e47de1b25447e3a42ed3c25e
edeutsch <edeutsch@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2002-06-04 18:32:01 +0000

    Added more table generalizations for Macrogenics support

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/Proteomics/Tables.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/Proteomics/Tables.pm
@@ -12,6 +12,10 @@ package SBEAMS::Proteomics::Tables;
 
 
 use strict;
+
+use SBEAMS::Connection::Settings;
+
+
 use vars qw(@ISA @EXPORT 
     $TB_ORGANISM
 
@@ -39,6 +43,7 @@ use vars qw(@ISA @EXPORT
     $TBPR_QUERY_OPTION
 
 );
+
 
 require Exporter;
 @ISA = qw (Exporter);
@@ -72,30 +77,35 @@ require Exporter;
 );
 
 
-$TB_ORGANISM                  = 'sbeams.dbo.organism';
+#### Get the appropriate database prefixes for the SBEAMS core and this module
+my $core = $DBPREFIX{Core};
+my $mod = $DBPREFIX{Proteomics};
 
-$TBPR_BIOSEQUENCE_SET         = 'proteomics.dbo.biosequence_set';
-$TBPR_BIOSEQUENCE             = 'proteomics.dbo.biosequence';
 
-$TBPR_PROTEOMICS_EXPERIMENT   = 'proteomics.dbo.proteomics_experiment';
-$TBPR_GRADIENT_PROGRAM        = 'proteomics.dbo.gradient_program';
-$TBPR_GRADIENT_DELTA          = 'proteomics.dbo.gradient_delta';
-$TBPR_FRACTION                = 'proteomics.dbo.fraction';
-$TBPR_SEARCH_BATCH            = 'proteomics.dbo.search_batch';
-$TBPR_SEARCH_BATCH_PARAMETER  = 'proteomics.dbo.search_batch_parameter';
-$TBPR_SEARCH_BATCH_PARAMETER_SET  = 'proteomics.dbo.search_batch_parameter_set';
-$TBPR_SEARCH                  = 'proteomics.dbo.search';
-$TBPR_SEARCH_HIT              = 'proteomics.dbo.search_hit';
-$TBPR_SEARCH_HIT_PROTEIN      = 'proteomics.dbo.search_hit_protein';
-$TBPR_QUANTITATION            = 'proteomics.dbo.quantitation';
-$TBPR_MSMS_SPECTRUM           = 'proteomics.dbo.msms_spectrum';
-$TBPR_MSMS_SPECTRUM_PEAK      = 'proteomics.dbo.msms_spectrum_peak';
+$TB_ORGANISM                      = "${core}organism";
 
-$TBPR_SEARCH_HIT_ANNOTATION  = 'proteomics.dbo.search_hit_annotation';
-$TBPR_ANNOTATION_LABEL       = 'proteomics.dbo.annotation_label';
-$TBPR_ANNOTATION_CONFIDENCE  = 'proteomics.dbo.annotation_confidence';
-$TBPR_ANNOTATION_SOURCE      = 'proteomics.dbo.annotation_source';
-$TBPR_QUERY_OPTION           = 'proteomics.dbo.query_option';
+$TBPR_BIOSEQUENCE_SET             = "${mod}biosequence_set";
+$TBPR_BIOSEQUENCE                 = "${mod}biosequence";
+
+$TBPR_PROTEOMICS_EXPERIMENT       = "${mod}proteomics_experiment";
+$TBPR_GRADIENT_PROGRAM            = "${mod}gradient_program";
+$TBPR_GRADIENT_DELTA              = "${mod}gradient_delta";
+$TBPR_FRACTION                    = "${mod}fraction";
+$TBPR_SEARCH_BATCH                = "${mod}search_batch";
+$TBPR_SEARCH_BATCH_PARAMETER      = "${mod}search_batch_parameter";
+$TBPR_SEARCH_BATCH_PARAMETER_SET  = "${mod}search_batch_parameter_set";
+$TBPR_SEARCH                      = "${mod}search";
+$TBPR_SEARCH_HIT                  = "${mod}search_hit";
+$TBPR_SEARCH_HIT_PROTEIN          = "${mod}search_hit_protein";
+$TBPR_QUANTITATION                = "${mod}quantitation";
+$TBPR_MSMS_SPECTRUM               = "${mod}msms_spectrum";
+$TBPR_MSMS_SPECTRUM_PEAK          = "${mod}msms_spectrum_peak";
+
+$TBPR_SEARCH_HIT_ANNOTATION       = "${mod}search_hit_annotation";
+$TBPR_ANNOTATION_LABEL            = "${mod}annotation_label";
+$TBPR_ANNOTATION_CONFIDENCE       = "${mod}annotation_confidence";
+$TBPR_ANNOTATION_SOURCE           = "${mod}annotation_source";
+$TBPR_QUERY_OPTION                = "${mod}query_option";
 
 
 
