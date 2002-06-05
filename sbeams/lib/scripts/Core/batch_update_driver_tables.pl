49461fd4361684aac8de9c1b1038ac42b287d0e4
edeutsch <gasif555@gmail.com>
2002-06-05 01:44:18 +0000

    added proteomics driver tables

--- sbeams/lib/scripts/Core/batch_update_driver_tables.pl
+++ sbeams/lib/scripts/Core/batch_update_driver_tables.pl
@@ -8,6 +8,8 @@
   ./update_driver_tables.pl $CONFDIR/Core/Core_table_property.txt
   ./update_driver_tables.pl $CONFDIR/Core/Core_table_column.txt
 
+  ./update_driver_tables.pl $CONFDIR/Proteomics/Proteomics_table_property.txt
+  ./update_driver_tables.pl $CONFDIR/Proteomics/Proteomics_table_column.txt
 
 
 
