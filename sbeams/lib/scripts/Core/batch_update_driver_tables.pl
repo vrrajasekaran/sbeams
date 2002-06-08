24ed0cf5129f55bd13f2af18a22ea20478b7f8bc
edeutsch <gasif555@gmail.com>
2002-06-08 00:06:50 +0000

    Added way to purge existing tables completely and reload from scratch

--- sbeams/lib/scripts/Core/batch_update_driver_tables.pl
+++ sbeams/lib/scripts/Core/batch_update_driver_tables.pl
@@ -5,6 +5,11 @@
 
   set CONFDIR = "../../conf"
 
+  if ( 0 == 1 ) then
+    ./update_driver_tables.pl --delete_existing
+  endif
+
+
   ./update_driver_tables.pl $CONFDIR/Core/Core_table_property.txt
   ./update_driver_tables.pl $CONFDIR/Core/Core_table_column.txt
 
@@ -12,10 +17,28 @@
   ./update_driver_tables.pl $CONFDIR/Proteomics/Proteomics_table_column.txt
   ./update_driver_tables.pl $CONFDIR/Proteomics/Proteomics_table_column_manual.txt
 
+  ./update_driver_tables.pl $CONFDIR/Microarray/Microarray_table_property.txt
+  ./update_driver_tables.pl $CONFDIR/Microarray/Microarray_table_column.txt
+  ./update_driver_tables.pl $CONFDIR/Microarray/Microarray_table_column_manual.txt
 
+  ./update_driver_tables.pl $CONFDIR/Inkjet/Inkjet_table_property.txt
+  ./update_driver_tables.pl $CONFDIR/Inkjet/Inkjet_table_column.txt
+  ./update_driver_tables.pl $CONFDIR/Inkjet/Inkjet_table_column_manual.txt
 
+  ./update_driver_tables.pl $CONFDIR/PhenoArray/PhenoArray_table_property.txt
+  ./update_driver_tables.pl $CONFDIR/PhenoArray/PhenoArray_table_column.txt
 
+  ./update_driver_tables.pl $CONFDIR/SNP/SNP_table_property.txt
+  ./update_driver_tables.pl $CONFDIR/SNP/SNP_table_column.txt
 
+  ./update_driver_tables.pl $CONFDIR/Biosap/Biosap_table_property.txt
+  ./update_driver_tables.pl $CONFDIR/Biosap/Biosap_table_column.txt
+
+  ./update_driver_tables.pl $CONFDIR/BEDB/BEDB_table_property.txt
+  ./update_driver_tables.pl $CONFDIR/BEDB/BEDB_table_column.txt
+
+  ./update_driver_tables.pl $CONFDIR/GEAP/GEAP_table_property.txt
+  ./update_driver_tables.pl $CONFDIR/GEAP/GEAP_table_column.txt
 
 
 
