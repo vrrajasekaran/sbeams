85baafa095c6aec6704ea35d03006e546c75d769
edeutsch <edeutsch@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2002-11-05 01:43:57 +0000

    Added copyright and GPL notices

--- sbeams/trunk/sbeams/lib/scripts/Proteomics/tests/readParamsFile.pl
+++ sbeams/trunk/sbeams/lib/scripts/Proteomics/tests/readParamsFile.pl
@@ -8,6 +8,12 @@
 # Description : Test program to read a sequest sequest.params file using
 #               SBEAMS::Proteomics::Utilities
 #
+# SBEAMS is Copyright (C) 2000-2002 by Eric Deutsch
+# This program is governed by the terms of the GNU General Public License (GPL)
+# version 2 as published by the Free Software Foundation.  It is provided
+# WITHOUT ANY WARRANTY.  See the full description of GPL terms in the
+# LICENSE file distributed with this software.
+#
 ###############################################################################
 
 
