cb9a04ed165b0b95777cd96aa780feeac16349dd
edeutsch <gasif555@gmail.com>
2002-12-28 00:18:42 +0000

    Updated Cytoscape URLs and moved out to config file

--- sbeams/lib/perl/SBEAMS/Connection/DBInterface.pm
+++ sbeams/lib/perl/SBEAMS/Connection/DBInterface.pm
@@ -1720,12 +1720,12 @@ sub displayResultSetControls {
 
     #### For certain types of resultsets, we'll allow a cytoscape trigger
     if ($resultset_ref->{column_list_ref}->[0] eq 'interaction_id') {
-      my $url_prefix = "http://hazel:8080/test/sbeams";
+      my $url_prefix = "$CYTOSCAPE_URL/sbeams";
       print "<BR>\n";
       print "<A HREF=\"${url_prefix}?m=immunology&rs=$rs_params{set_name}\">[View this Resultset with Cytoscape]</A>";
     }
     if ($resultset_ref->{column_list_ref}->[0] eq 'Bait') {
-      my $url_prefix = "http://hazel:8080/test/sbeams";
+      my $url_prefix = "$CYTOSCAPE_URL/sbeams";
       print "<BR>\n";
       print "<A HREF=\"${url_prefix}?m=agingras&rs=$rs_params{set_name}\">[View this Resultset with Cytoscape]</A>";
     }
