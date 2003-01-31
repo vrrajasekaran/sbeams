03608b3d00306979ed426c5eb5fa8fd7f79d9cba
edeutsch <edeutsch@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2003-01-31 00:14:01 +0000

    Added getAccessibleProjects functionality

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/Connection/Permissions.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/Connection/Permissions.pm
@@ -712,6 +712,70 @@ sub get_best_permission{
 
 
 ###############################################################################
+# getAcessibleProjects
+###############################################################################
+sub getAcessibleProjects{
+  my $self = shift || croak("parameter self not passed");
+  my %args = @_;
+  my $SUB_NAME = "getAcessibleProjects";
+
+
+  ## Decode argument list
+  my $current_contact_id = $args{'contact_id'}
+    || $self->getCurrent_contact_id();
+
+
+  #### Define SQL to get all project to which the current user has access
+  my $sql = qq~
+      SELECT P.project_id,P.project_tag,P.name,UL.username,
+             MIN(CASE WHEN UWG.contact_id IS NULL THEN NULL
+                      ELSE GPP.privilege_id END) AS "best_group_privilege_id",
+             MIN(CASE WHEN P.PI_contact_id = $current_contact_id THEN 10
+                      ELSE UPP.privilege_id END) AS "best_user_privilege_id"
+      FROM $TB_PROJECT P
+      JOIN $TB_USER_LOGIN UL ON ( P.PI_contact_id = UL.contact_id )
+      LEFT JOIN $TB_USER_PROJECT_PERMISSION UPP
+      ON ( P.project_id = UPP.project_id
+	   AND UPP.contact_id='$current_contact_id' )
+      LEFT JOIN $TB_GROUP_PROJECT_PERMISSION GPP
+      ON ( P.project_id = GPP.project_id )
+      LEFT JOIN $TB_PRIVILEGE PRIV
+      ON ( GPP.privilege_id = PRIV.privilege_id )
+      LEFT JOIN $TB_USER_WORK_GROUP UWG
+      ON ( GPP.work_group_id = UWG.work_group_id
+	   AND UWG.contact_id='$current_contact_id' )
+      LEFT JOIN $TB_WORK_GROUP WG
+      ON ( UWG.work_group_id = WG.work_group_id )
+      WHERE 1=1
+      AND P.record_status != 'D'
+      AND UL.record_status != 'D'
+      AND ( UPP.record_status != 'D' OR UPP.record_status IS NULL )
+      AND ( GPP.record_status != 'D' OR GPP.record_status IS NULL )
+      AND ( PRIV.record_status != 'D' OR PRIV.record_status IS NULL )
+      AND ( UWG.record_status != 'D' OR UWG.record_status IS NULL )
+      AND ( WG.record_status != 'D' OR WG.record_status IS NULL )
+      AND ( UPP.privilege_id<=40 OR GPP.privilege_id<=40
+            OR P.PI_contact_id = '$current_contact_id' )
+      AND ( WG.work_group_name IS NOT NULL OR UPP.privilege_id IS NOT NULL
+            OR P.PI_contact_id = '$current_contact_id' )
+      GROUP BY P.project_id,P.project_tag,P.name,UL.username
+      ORDER BY UL.username,P.project_tag
+  ~;
+
+  my @rows = $self->selectSeveralColumns($sql);
+
+  my @project_ids = ();
+
+  foreach my $element (@rows) {
+    push(@project_ids,$element->[0]);
+  }
+
+  return (@project_ids);
+
+} # end getAcessibleProjects
+
+
+###############################################################################
 ###############################################################################
 ###############################################################################
 ###############################################################################
