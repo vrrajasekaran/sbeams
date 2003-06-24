2c0bb832b870d7206c1802f51abff84d4e86b63b
edeutsch <gasif555@gmail.com>
2003-06-24 06:22:55 +0000

    Added links to more tables

--- sbeams/lib/perl/SBEAMS/ProteinStructure/HTMLPrinter.pm
+++ sbeams/lib/perl/SBEAMS/ProteinStructure/HTMLPrinter.pm
@@ -114,7 +114,9 @@ sub display_page_header {
 	<tr><td>&nbsp;</td></tr>
 	<tr><td>Manage Tables:</td></tr>
 	<tr><td><a href="$CGI_BASE_DIR/$SBEAMS_SUBDIR/ManageTable.cgi?TABLE_NAME=PS_biosequence_set"><nobr>&nbsp;&nbsp;&nbsp;BioSequenceSets</nobr></a></td></tr>
+	<tr><td><a href="$CGI_BASE_DIR/$SBEAMS_SUBDIR/ManageTable.cgi?TABLE_NAME=PS_domain_match_type"><nobr>&nbsp;&nbsp;&nbsp;Match Types</nobr></a></td></tr>
 	<tr><td><a href="$CGI_BASE_DIR/$SBEAMS_SUBDIR/ManageTable.cgi?TABLE_NAME=PS_domain_match_source"><nobr>&nbsp;&nbsp;&nbsp;Match Sources</nobr></a></td></tr>
+	<tr><td><a href="$CGI_BASE_DIR/$SBEAMS_SUBDIR/ManageTable.cgi?TABLE_NAME=PS_dbxref"><nobr>&nbsp;&nbsp;&nbsp;DB xRefs</nobr></a></td></tr>
 	<tr><td>&nbsp;</td></tr>
 	</table>
 	</td>
