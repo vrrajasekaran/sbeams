68dac197574e07bcf931e8908beb6741a0ded9ac
mjohnson <mjohnson@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2003-06-25 18:32:52 +0000

    documentation index for users

---
+++ sbeams/trunk/sbeams/doc/user_index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<HTML>
+<?php
+  /* $Id$ */
+  $TITLE="SBEAMS Documentation";
+
+  include("share/style.inc.php");
+
+  include("share/header.inc.php");
+?>
+
+
+<!-- ------------------------ Start Main Page Content ------------------------ -->
+
+<TABLE BORDER=0 WIDTH="680" BGCOLOR="#ffffff" CELLPADDING="10">
+
+<TR>
+  <TD ALIGN="top">
+   <CENTER><FONT COLOR="red" SIZE="+2"><B>User Documentation</B></FONT></CENTER>
+  </TD> 
+</TR>
+
+
+<!-- ------------------------ Table of Contents Table ------------------------ -->
+<TR>
+  <TD>
+
+  <TABLE>
+  <TR VALIGN="top">
+
+    <TD>
+    <CENTER><B>SBEAMS Overview</B></CENTER><BR>
+    <UL>
+     <LI><A HREF="SBEAMSOutline.php">General Outline of the State of SBEAMS</A>
+     <LI><A HREF="/projects/sbeams/">Longer Concept Overview of SBEAMS</A>
+     <LI><A HREF="SecurityLayout.php">Description of Security Layout</A>
+     <LI><A HREF="Microarray/keymapfiles.notes">Misc notes about .key and .map files</A>
+    </UL>
+    </TD>
+
+    <TD>
+    <CENTER><B>Recipes</B></CENTER><BR>
+    <UL>
+     <LI><A HREF="Microarray/HOWTO_SubmitArrayRequest.php">Instructions for Submitting a Microarray Request</A>
+    </UL>
+    </TD>
+
+  </TR>
+  </TABLE>  
+
+  </TD>
+</TR>
+
+
+</TABLE>
+
+
+<!-- ------------------------ End Main Page Content ------------------------ -->
+<?php
+  include("share/footer.inc.php");
+?>
+
+
