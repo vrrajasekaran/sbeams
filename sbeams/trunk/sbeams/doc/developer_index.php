cb0091a94beede6f5632690da661b9d7098b0bff
mjohnson <mjohnson@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2003-06-25 18:33:16 +0000

    index for developer section of SBEAMS documentation

---
+++ sbeams/trunk/sbeams/doc/developer_index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<HTML>
+<?php
+  /* $Id$ */
+  $TITLE="SBEAMS Documentation";
+
+  include("share/style.inc.php");
+
+  include("share/header.inc.php");
+?>
+
+
+<!-- ------------------------ Start Main Page Content ------------------------ -->
+
+<TABLE BORDER=0 WIDTH="680" BGCOLOR="#ffffff" CELLPADDING="10">
+
+<TR>
+  <TD ALIGN="top">
+   <CENTER><FONT COLOR="red" SIZE="+2"><B>Developer Documentation</B></FONT></CENTER>
+  </TD> 
+</TR>
+
+
+<!-- ------------------------ Table of Contents Table ------------------------ -->
+<TR>
+  <TD>
+
+  <TABLE>
+
+  <TR VALIGN="top">
+
+<!-- ------------------------ Overview Links ------------------------ -->
+    <TD>
+    <CENTER><B>SBEAMS Overview</B></CENTER><BR>
+    <UL>
+     <LI><A HREF="SBEAMSOutline.php">General Outline of the State of SBEAMS</A>
+     <LI><A HREF="/projects/sbeams/">Longer Concept Overview of SBEAMS</A>
+     <LI><A HREF="SecurityLayout.php">Description of Security Layout</A>
+     <LI><A HREF="Proteomics/ProcPipeline.doc">General Proposal and Documenation for the Proteomics Data Pipeline (Word .doc)</A>
+     <LI><A HREF="Microarray/keymapfiles.notes">Misc notes about .key and .map files</A>
+    </UL>
+    </TD>
+
+<!-- ------------------------ Recipe Links ------------------------ -->
+    <TD>
+    <CENTER><B>Recipes</B></CENTER><BR>
+    <UL>
+     <LI><A HREF="Microarray/HOWTO_SubmitArrayRequest.php">Instructions for Submitting a Microarray Request</A>
+     <LI><A HREF="DevInstance.php">Notes on using an SBEAMS "dev instance"</A>
+    </UL>
+    </TD>
+  </TR>
+
+  <TR>
+    <TD>
+     <BR>
+    </TD>
+  </TR>
+
+  <TR VALIGN="top">
+
+<!-- ------------------------ API Links ------------------------ -->
+    <TD>
+    <CENTER><B>API</B></CENTER><BR>
+    <UL>
+     <LI><A HREF="/dev2/sbeams/doc/POD/SBEAMS/Connection.html">SBEAMS::Connection</A>
+     <LI><A HREF="/dev2/sbeams/doc/POD/SBEAMS/Connection/Authenticator.html">SBEAMS::Connection::Authenticator</A>
+     <LI><A HREF="/dev2/sbeams/doc/POD/SBEAMS/Connection/DBConnector.html">SBEAMS::Connection::DBConnector</A>
+     <LI><A HREF="/dev2/sbeams/doc/POD/SBEAMS/Connection/DBInterface.html">SBEAMS::Connection::DBInterface</A>
+     <LI><A HREF="/dev2/sbeams/doc/POD/SBEAMS/Connection/ErrorHandler.html">SBEAMS::Connection::ErrorHandler</A>
+     <LI><A HREF="/dev2/sbeams/doc/POD/SBEAMS/Connection/HTMLPrinter.html">SBEAMS::Connection::HTMLPrinter</A>
+     <LI><A HREF="/dev2/sbeams/doc/POD/SBEAMS/Connection/Permissions.html">SBEAMS::Connection::Permissions</A>
+     <LI><A HREF="/dev2/sbeams/doc/POD/SBEAMS/Connection/PubMedFetcher.html">SBEAMS::Connection::PubMedFetcher</A>
+     <LI><A HREF="/dev2/sbeams/doc/POD/SBEAMS/Connection/Settings.html">SBEAMS::Connection::Settings</A>
+     <LI><A HREF="/dev2/sbeams/doc/POD/SBEAMS/Connection/TableInfo.html">SBEAMS::Connection::TableInfo</A>
+     <LI><A HREF="/dev2/sbeams/doc/POD/SBEAMS/Connection/Tables.html">SBEAMS::Connection::Tables</A>
+     <LI><A HREF="/dev2/sbeams/doc/POD/SBEAMS/Connection/Utilities.html">SBEAMS::Connection::Utilities</A>
+    </UL>
+    </TD>
+
+<!-- ------------------------ Schema Links ------------------------ -->
+    <TD>
+    <CENTER><B>Schema</B></CENTER><BR>
+    <UL>
+     <LI><A HREF="Core/Core_Schema.gif">Core database schema</A>
+     <LI><A HREF="Microarray/Microarray_Schema.gif">Microarray database schema</A>
+     <LI><A HREF="PhenoArray/PhenoArray_Schema.gif">PhenoArray database schema</A>
+     <LI><A HREF="Proteomics/Proteomics_Schema.gif">Proteomics database schema</A>
+     <LI><A HREF="SNP/SNP_Schema.gif">SNP database schema</A>
+    </UL>
+    </TD>
+
+  </TR>
+  </TABLE>  
+
+  </TD>
+</TR>
+
+
+</TABLE>
+
+
+<!-- ------------------------ End Main Page Content ------------------------ -->
+<?php
+  include("share/footer.inc.php");
+?>
+
+
