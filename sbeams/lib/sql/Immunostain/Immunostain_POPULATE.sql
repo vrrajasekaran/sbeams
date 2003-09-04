bee329be2545f0f47166cf67305b8125ce62015d
edeutsch <gasif555@gmail.com>
2003-09-04 06:28:03 +0000

    Added a Immunostain population script

---
+++ sbeams/lib/sql/Immunostain/Immunostain_POPULATE.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO query_option ( option_type,option_key,option_value,sort_order )
+VALUES ( 'input_form_options','minimum_detail','Minimum Detail',10 );
+INSERT INTO query_option ( option_type,option_key,option_value,sort_order )
+VALUES ( 'input_form_options','medium_detail','Medium Detail',20 );
+INSERT INTO query_option ( option_type,option_key,option_value,sort_order )
+VALUES ( 'input_form_options','full_detail','Full Detail',30 );
+
+INSERT INTO query_option ( option_type,option_key,option_value,sort_order )
+VALUES ( 'SS_display_options','MergeLevelsAndPivotCellTypes','Merge Levels and Pivot Cell Types',90 );
+INSERT INTO query_option ( option_type,option_key,option_value,sort_order )
+VALUES ( 'SS_display_options','ShowSQL','Show SQL Query',160 );
+
