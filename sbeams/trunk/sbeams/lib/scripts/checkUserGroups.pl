75a25500e85d5f92f33b6743fd30006bf16daeb1
edeutsch <edeutsch@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2003-12-04 09:34:40 +0000

    major rewrite to use new conventions

--- sbeams/trunk/sbeams/lib/scripts/checkUserGroups.pl
+++ sbeams/trunk/sbeams/lib/scripts/checkUserGroups.pl
@@ -1,7 +1,7 @@
-#!/usr/local/bin/perl
+#!/usr/local/bin/perl -w
 
 ###############################################################################
-# Program     : checkUsers.pl
+# Program     : checkUserGroups.pl
 # Author      : Eric Deutsch <edeutsch@systemsbiology.org>
 # $Id$
 #
@@ -17,187 +17,330 @@
 ###############################################################################
 
 
+
 ###############################################################################
-# Get the script set up with everything it will need
+# Generic SBEAMS setup for all the needed modules and objects
 ###############################################################################
 use strict;
-use vars qw ($sbeams $dbh $PROGRAM_FILE_NAME
-             $current_contact_id $current_username
-             $current_work_group_id $current_work_group_name
-             $current_project_id $current_project_name);
-use lib qw (../perl);
+use Getopt::Long;
+use FindBin;
 
+use lib "$FindBin::Bin/../perl";
+use vars qw ($sbeams $sbeamsMOD $q $current_username
+             $PROG_NAME $USAGE %OPTIONS $QUIET $VERBOSE $DEBUG $TESTONLY
+            );
+
+
+#### Set up SBEAMS core module
 use SBEAMS::Connection;
 use SBEAMS::Connection::Settings;
 use SBEAMS::Connection::Tables;
-use SBEAMS::Connection::TableInfo;
+$sbeams = SBEAMS::Connection->new();
 
-$sbeams = new SBEAMS::Connection;
-$| = 1;
+use CGI;
+$q = CGI->new();
 
 
 ###############################################################################
-# Global Variables
+# Set program name and usage banner for command like use
 ###############################################################################
-$PROGRAM_FILE_NAME = 'importLayoutInfo.pl';
+$PROG_NAME = $FindBin::Script;
+$USAGE = <<EOU;
+Usage: $PROG_NAME [OPTIONS]
+Options:
+  --verbose n         Set verbosity level.  default is 0
+  --quiet             Set flag to print nothing at all except errors
+  --debug n           Set debug flag
+  --testonly          If set, rows in the database are not changed or added
+  --check             Just print information about what would be done
+  --NIS_group         Specify the name of the NIS group from which to
+                      extract users
+  --SBEAMS_group      Name of the SBEAMS group into which to add users
+  --privilege_level   Name of the privilege level the users should get
+  --include_users     Comma-separated list of users to process
+  --exclude_users     Comma-separated list of users to exclude from processing
+
+ e.g.:  $PROG_NAME --check --NIS yeast --SBEAMS Yeast --priv data_groupmodifier
+
+EOU
+
+#### Process options
+unless (GetOptions(\%OPTIONS,"verbose:s","quiet","debug:s","testonly",
+        "NIS_group:s","SBEAMS_group:s","privilege_level:s",
+        "include_users:s","exclude_users:s","check",
+  )) {
+  print "$USAGE";
+  exit;
+}
+
+$VERBOSE = $OPTIONS{"verbose"} || 0;
+$QUIET = $OPTIONS{"quiet"} || 0;
+$DEBUG = $OPTIONS{"debug"} || 0;
+$TESTONLY = $OPTIONS{"testonly"} || 0;
+if ($DEBUG) {
+  print "Options settings:\n";
+  print "  VERBOSE = $VERBOSE\n";
+  print "  QUIET = $QUIET\n";
+  print "  DEBUG = $DEBUG\n";
+  print "  TESTONLY = $TESTONLY\n";
+}
+
+
+###############################################################################
+# Set Global Variables and execute main()
+###############################################################################
 main();
+exit(0);
 
 
 ###############################################################################
 # Main Program:
 #
-# Call $sbeams->Authentication and stop immediately if authentication
-# fails else continue.
+# Call $sbeams->Authenticate() and exit if it fails or continue if it works.
 ###############################################################################
 sub main {
 
-    #### Do the SBEAMS authentication and exit if a username is not returned
-    exit unless ($current_username =
-        $sbeams->Authenticate(work_group=>'Admin'));
+  #### Do the SBEAMS authentication and exit if a username is not returned
+  exit unless ($current_username = $sbeams->Authenticate(
+    work_group=>'Admin',
+  ));
 
-    #### Print the header, do what the program does, and print footer
-    $sbeams->printTextHeader();
-    showMainPage();
-    $sbeams->printTextFooter();
+
+  $sbeams->printPageHeader() unless ($QUIET);
+  handleRequest();
+  $sbeams->printPageFooter() unless ($QUIET);
+
 
 } # end main
 
 
 ###############################################################################
-# Show the main welcome page
+# handleRequest
 ###############################################################################
-sub showMainPage {
+sub handleRequest {
+  my %args = @_;
 
-    $current_username = $sbeams->getCurrent_username;
-    $current_contact_id = $sbeams->getCurrent_contact_id;
-    $current_work_group_id = $sbeams->getCurrent_work_group_id;
-    $current_work_group_name = $sbeams->getCurrent_work_group_name;
+  #### Set the command-line options
+  my $NIS_group = $OPTIONS{"NIS_group"} || '';
+  my $SBEAMS_group = $OPTIONS{"SBEAMS_group"} || '';
+  my $privilege_level = $OPTIONS{"privilege_level"} || '';
+  my $include_users = $OPTIONS{"include_users"} || '';
+  my $exclude_users = $OPTIONS{"exclude_users"} || '';
+  my $NOWRITE =  $OPTIONS{"check"} || 0;
 
 
-    $sbeams->printUserContext(style=>'TEXT');
+  #### Verify required parameters
+  unless ($SBEAMS_group) {
+    print "ERROR: You must specify a --SBEAMS_group\n\n";
+    print "$USAGE";
+    exit;
+  }
+
+
+  #### Verify required parameters
+  unless ($NIS_group || $include_users) {
+    print "ERROR: You must specify either --NIS_group or --include_users\n\n";
+    print "$USAGE";
+    exit;
+  }
+
+
+  #### If there are any parameters left, complain and print usage
+  if ($ARGV[0]){
+    print "ERROR: Unresolved command line parameter '$ARGV[0]'.\n";
+    print "$USAGE";
+    exit;
+  }
+
+
+  #### Print out the header
+  unless ($QUIET) {
+    $sbeams->printUserContext();
     print "\n";
+  }
 
 
-    #### Define a mapping of YP groups to SBEAMS groups
-    my %groupMapping = (
-      proteomics => "Proteomics_user",
-      comp_bio => "Comp_Bio"
-    );
-
-    my $NOWRITE = 1;
-
-    my @excluded_users = ( "culloden","visigoth1","lfeltz","administrator",
-      "mharris","rwelti","mrobinson","jroach","jbuhler","asmit",
-      "aamiry","btjaden","nesvi","mjohnson" );
-    @excluded_users = ();
+  #### Get the work_group_id for the supplied name
+  my $work_group_id;
+  unless ($work_group_id = $sbeams->get_work_group_id($SBEAMS_group)) {
+    die "Failed to get work_group_id for '$SBEAMS_group'";
+  }
 
 
-    my ($unixgroup,$dbgroup,$group_list,$member,$group,$line);
-    my ($contact_id,$work_group_id,$sql_query,$sql_statement);
-    my (@group_members,@groups,@results,@parsed_line);
+  #### Get the privilege_levels
+  my $sql;
+  $sql = qq~
+    SELECT name,privilege_id
+      FROM $TB_PRIVILEGE P
+     WHERE record_status != 'D'
+  ~;
+  my %privilege_ids = $sbeams->selectTwoColumnHash($sql);
 
 
-    # Set PATH to something innocuous to keep Taint happy
-    $ENV{PATH}="/bin:/usr/bin";
+  #### Parse the include and exclude users
+  my @exclude_users = split(',',$exclude_users);
+  my @include_users = split(',',$include_users);
+  my @group_members;
 
 
-    #### Loop over each group
-    while ( ($unixgroup,$dbgroup) = each %groupMapping ) {
+  if ($NIS_group) {
+    my ($group_list) = `/usr/bin/ypmatch $NIS_group group`;
+    $group_list =~ s/[\n\r]//g;
+    $group_list =~ s/.+://;
+    @group_members = split(",",$group_list);
+  } else {
+    @group_members = @include_users;
+  }
 
-      unless ($work_group_id = $sbeams->get_work_group_id($dbgroup)) {
-        die "Failed to get work_group_id for 'dbgroup'";
-      }
+  foreach my $member (@group_members) {
 
-      ($group_list) = `/usr/bin/ypmatch $unixgroup group`;
-      $group_list =~ s/[\n\r]//g;
-      $group_list =~ s/.+://;
-      @group_members = split(",",$group_list);
+    #### Skip to the next if this one is exclude
+    next if (grep { /$member/ } @exclude_users);
 
-      foreach $member (@group_members) {
-
-        next if (grep { /$member/ } @excluded_users);
-
-        #### See if the user exists in the database
-        ($contact_id) = $sbeams->getContact_id($member);
-        unless ($contact_id) {
-          print "$member: (not in db)\n";
-
-          ($line) = `/usr/bin/ypmatch $member passwd`;
-          $line =~ s/[\n\r]//g;
-          @parsed_line = split(":",$line);
-          $line = $parsed_line[4];
-          @parsed_line = split(",",$line);
-          my $real_name = $parsed_line[0];
-          my @name_parts = split(/\s+/,$real_name);
-          my $first_name = $name_parts[0];
-          my $last_name = $name_parts[1];
-          my $department = $parsed_line[2];
-          print "    add: $real_name ($department)\n";
-
-          $sql_statement = "INSERT INTO $TB_CONTACT ".
-            "( first_name,last_name,contact_type_id,organization_id,department,".
-            "created_by_id,modified_by_id,owner_group_id ) ".
-            "VALUES ( '$first_name','$last_name',2,1,'$department',".
-            "$current_contact_id,$current_contact_id,$current_work_group_id )";
-          print "$sql_statement\n";
-          $sbeams->executeSQL($sql_statement) unless $NOWRITE;
-
-          ($contact_id) = $sbeams->getLastInsertedPK() unless $NOWRITE;
-
-          $sql_statement = "INSERT INTO $TB_USER_LOGIN ".
-            "( contact_id,username,privilege_id,".
-            "created_by_id,modified_by_id,owner_group_id ) ".
-            "VALUES ( $contact_id,'$member',30,".
-            "$current_contact_id,$current_contact_id,$current_work_group_id )";
-          print "$sql_statement\n";
-          $sbeams->executeSQL($sql_statement) unless $NOWRITE;
-
-
-        } else {
-          print "$member: $contact_id\n";
-        }
-
-
-        #### Get the work groups that the user belongs to
-        $sql_query = qq~
-		SELECT work_group_name
-		  FROM user_login UL
-		  JOIN user_work_group UWG ON ( UL.contact_id = UWG.contact_id )
-		  JOIN work_group WG ON ( UWG.work_group_id = WG.work_group_id )
-		 WHERE username = '$member'
-		 ORDER BY username,work_group_name
-        ~;
-
-        @groups = $sbeams->selectOneColumn($sql_query);
-        if (@groups) {
-
-          foreach $group (@groups) {
-            print "$member: $group\n";
-          }
-
-        }
-
-        unless (grep { /$dbgroup/ } @groups) {
-          $sql_statement = "INSERT INTO $TB_USER_WORK_GROUP ".
-            "( contact_id,work_group_id,privilege_id,created_by_id,modified_by_id,".
-            "owner_group_id ) ".
-            "VALUES ( $contact_id,$work_group_id,30,$current_contact_id,".
-            "$current_contact_id,$current_work_group_id )";
-          print "$sql_statement\n";
-          $sbeams->executeSQL($sql_statement) unless $NOWRITE;
-        }
-
-        print "\n";
-      }
-
+    #### Skip to the next if this one isn't include
+    if ($include_users) {
+      next unless (grep { /$member/ } @include_users);
     }
 
 
+    #### See if the user exists in the database
+    my ($contact_id) = $sbeams->getContact_id($member);
+    if ($contact_id) {
 
-} # end showMainPage
+      print "$member($contact_id): ";
+
+    } else {
+
+      print "$member(?): (not in db)\n";
+
+      my ($line) = `/usr/bin/ypmatch $member passwd`;
+      $line =~ s/[\n\r]//g;
+
+      #### If this user isn't even there, then squawk
+      unless ($line) {
+	print "  ERROR: User $member isn't even in NIS!!\n";
+	next;
+      }
+
+      #### Extract information from passwd file
+      my @parsed_line = split(":",$line);
+      $line = $parsed_line[4];
+      @parsed_line = split(",",$line);
+      my $real_name = $parsed_line[0] || '';
+      my @name_parts = split(/\s+/,$real_name);
+      my $first_name = $name_parts[0] || '';
+      my $last_name = $name_parts[1] || '';
+      my $department = $parsed_line[2] || '';
+      print "    add: $real_name ($department)\n";
+
+      #### Define the data columns for the new contact
+      my %rowdata = (
+        first_name => $first_name,
+	last_name => $last_name,
+	contact_type_id => 2,
+	organization_id => 1,
+      );
+
+      #### INSERT the row
+      $contact_id = $sbeams->updateOrInsertRow(
+  	insert => 1,
+  	table_name => $TB_CONTACT,
+  	rowdata_ref => \%rowdata,
+        PK => 'contact_id',
+        return_PK => 1,
+        add_audit_parameters => 1,
+  	verbose => $VERBOSE,
+  	testonly => $TESTONLY,
+      ) unless ($NOWRITE);
 
 
+      #### Define the data columns for the new user_login
+      %rowdata = (
+        contact_id => $contact_id,
+	username => $member,
+	privilege_id => 20,
+      );
+
+      #### INSERT the row
+      my $user_login_id = $sbeams->updateOrInsertRow(
+  	insert => 1,
+  	table_name => $TB_USER_LOGIN,
+  	rowdata_ref => \%rowdata,
+        PK => 'user_login_id',
+        return_PK => 1,
+        add_audit_parameters => 1,
+  	verbose => $VERBOSE,
+  	testonly => $TESTONLY,
+      ) unless ($NOWRITE);
+
+    }
+
+    #### Make sure we have a contact_id now or skip
+    unless ($contact_id) {
+      print "\n";
+      next;
+    }
 
 
+    #### Get the work groups that the user belongs to
+    $sql = qq~
+    	SELECT work_group_name,P.name
+    	  FROM user_login UL
+    	  JOIN user_work_group UWG ON ( UL.contact_id = UWG.contact_id )
+    	  JOIN work_group WG ON ( UWG.work_group_id = WG.work_group_id )
+    	  JOIN $TB_PRIVILEGE P ON ( UWG.privilege_id = P.privilege_id )
+    	 WHERE username = '$member'
+    	 ORDER BY username,work_group_name
+    ~;
+
+    my %groups = $sbeams->selectTwoColumnHash($sql);
+    if (%groups) {
+      print "--current: ",join(',',keys(%groups)),"\n" if ($VERBOSE);
+    }
+
+
+    #### If the user is already a member of this group
+    if (grep { /$SBEAMS_group/ } keys(%groups)) {
+
+     print "OK - already a member at level ".
+       $groups{$SBEAMS_group}."\n";
+
+
+    #### If not, then we need to add
+    } else {
+
+      unless ($privilege_ids{$privilege_level}) {
+	print "ERROR: Unable to resolve privilege_level '$privilege_level' ".
+	  "which is needed to added new user_work_group records.\n\n";
+	return;
+      }
+
+      print "add to $SBEAMS_group group as $privilege_level\n";
+
+      my %rowdata = (
+        contact_id => $contact_id,
+	work_group_id => $work_group_id,
+	privilege_id => $privilege_ids{$privilege_level},
+      );
+
+      $sbeams->updateOrInsertRow(
+  	insert => 1,
+  	table_name => $TB_USER_WORK_GROUP,
+  	rowdata_ref => \%rowdata,
+        PK => 'user_work_group_id',
+        return_PK => 1,
+        add_audit_parameters => 1,
+  	verbose => $VERBOSE,
+  	testonly => $TESTONLY,
+      ) unless ($NOWRITE);
+
+    }
+
+    print "\n";
+
+  } # end foreach member
+
+
+} # end handleRequest
 
 
