bd45ced76129d1ebfb7b87edddda2c24cd2230f1
sbeams <sbeams@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2004-01-20 07:15:58 +0000

    fixed my fix

--- sbeams/includes/header.inc.php
+++ sbeams/includes/header.inc.php
@@ -1,6 +1,6 @@
 <?php
   /* $Id$ */
-  $SBEAMS="/";
+  $SBEAMS="";
 ?>
 
 <!-- Begin the whole-page table -->
@@ -11,13 +11,13 @@
 <!-- -------------- Top Line Header: logo and big title ------------------- -->
 <tr>
 <td bgcolor="#cdd1e7">
-<a href="http://db.systemsbiology.net/"><img height=64 width=64 border=0 alt="ISB DB" src="<?php echo $SBEAMS ?>/images/dbsmltblue.gif"></a><a href="<?php echo $SBEAMS ?>/sbeams/cgi/main.cgi"><img height=64 width=64 border=0 alt="SBEAMS" src="<?php echo $SBEAMS ?>/images/sbeamssmltblue.gif"></a>
+<a href="http://db.systemsbiology.net/"><img height=64 width=64 border=0 alt="ISB DB" src="<?php echo $SBEAMS ?>images/dbsmltblue.gif"></a><a href="<?php echo $SBEAMS ?>cgi/main.cgi"><img height=64 width=64 border=0 alt="SBEAMS" src="<?php echo $SBEAMS ?>images/sbeamssmltblue.gif"></a>
 </td>
 
 <?php
   if ($TITLE) {
 ?>
-<td background="<?php echo $SBEAMS ?>/images/plaintop.jpg">
+<td background="<?php echo $SBEAMS ?>images/plaintop.jpg">
 <center><H1><?php echo $TITLE ?></H1></center>
 </td></tr>
 <?php
