8118927af6e0d1f482f3afcf085c51d94b420185
mjohnson <mjohnson@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2004-08-05 16:59:47 +0000

    Oligo.pm for SBEAMS::Oligo

---
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/Oligo.pm
@@ -0,0 +1,72 @@
+package SBEAMS::Oligo;
+
+###############################################################################
+# Program     : SBEAMS::Oligo
+# Author      : Eric Deutsch <edeutsch@systemsbiology.org>
+# $$
+#
+# Description : Perl Module to handle all SBEAMS - Oligo specific items.
+#
+###############################################################################
+
+
+use strict;
+use vars qw($VERSION @ISA $sbeams);
+use CGI::Carp qw(fatalsToBrowser croak);
+
+use SBEAMS::Oligo::DBInterface;
+use SBEAMS::Oligo::HTMLPrinter;
+use SBEAMS::Oligo::TableInfo;
+use SBEAMS::Oligo::Settings;
+
+@ISA = qw(SBEAMS::Oligo::DBInterface
+          SBEAMS::Oligo::HTMLPrinter
+          SBEAMS::Oligo::TableInfo
+          SBEAMS::Oligo::Settings);
+
+
+###############################################################################
+# Global Variables
+###############################################################################
+$VERSION = '0.02';
+
+
+###############################################################################
+# Constructor
+###############################################################################
+sub new {
+    my $this = shift;
+    my $class = ref($this) || $this;
+    my $self = {};
+    bless $self, $class;
+    return($self);
+}
+
+
+###############################################################################
+# Receive the main SBEAMS object
+###############################################################################
+sub setSBEAMS {
+    my $self = shift;
+    $sbeams = shift;
+    return($sbeams);
+}
+
+
+###############################################################################
+# Provide the main SBEAMS object
+###############################################################################
+sub getSBEAMS {
+    my $self = shift;
+    return($sbeams);
+}
+
+
+###############################################################################
+
+1;
+
+__END__
+###############################################################################
+###############################################################################
+###############################################################################
