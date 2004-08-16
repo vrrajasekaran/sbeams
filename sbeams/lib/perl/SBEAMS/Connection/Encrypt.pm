8a513b6e5c16c4c04e621be2f4a4262ad6966f59
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2004-08-16 21:51:31 +0000

    Added check to ensure 'hexness' for decryption.

--- sbeams/lib/perl/SBEAMS/Connection/Encrypt.pm
+++ sbeams/lib/perl/SBEAMS/Connection/Encrypt.pm
@@ -89,6 +89,11 @@ sub decrypt {
   die ( "No string provided for decryption" ) unless $this->{encrypted};
   die ( "No key provided for decryption" ) unless $this->{key};
 
+  if ( $this->{decrypted} !~ /^[a-fA-F0-9]+$/ ) {
+    print STDERR "Rejected attempt to decrypt non-hex value\n";
+    return undef;
+    }
+
   my $cipher = new Crypt::CBC( $this->{key}, 'IDEA');
   $this->{decrypted} = $cipher->decrypt_hex( $this->{encrypted} );
   return $this->{decrypted}
