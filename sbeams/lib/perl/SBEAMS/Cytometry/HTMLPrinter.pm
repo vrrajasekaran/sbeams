e3646bdb86b8944fa710ade1a5ff38a649f41927
Vikas Wariyal <83917893+warvik54@users.noreply.github.com>
2004-10-19 23:25:06 +0000

    added samplename convention

--- sbeams/lib/perl/SBEAMS/Cytometry/HTMLPrinter.pm
+++ sbeams/lib/perl/SBEAMS/Cytometry/HTMLPrinter.pm
@@ -114,7 +114,11 @@ sub display_page_header {
     <tr><td><a href="$CGI_BASE_DIR/$SBEAMS_SUBDIR/ManageTable.cgi?TABLE_NAME=CY_sort_entity"><nobr>&nbsp;&nbsp;&nbsp;Sort Entity</nobr></a></td></tr>
     <tr><td><a href="$CGI_BASE_DIR/$SBEAMS_SUBDIR/ManageTable.cgi?TABLE_NAME=CY_tissue_type"><nobr>&nbsp;&nbsp;&nbsp;Tissue Type</nobr></a></td></tr>
   <tr><td><a href="$CGI_BASE_DIR/$SBEAMS_SUBDIR/ManageTable.cgi?TABLE_NAME=CY_sort_type"><nobr>&nbsp;&nbsp;&nbsp;Sort Technique</nobr></a></td></tr>
-
+  <tr></tr>
+  <tr></tr>
+  <tr><td>Documentation</td></tr>
+ <tr><td><a href="$HTML_BASE_DIR/doc/$SBEAMS_SUBDIR/SampleNameConvention.php"><nobr>&nbsp;&nbsp;&nbsp;Sample Name Coding</nobr></a></td></tr>
+       
 	<tr><td>&nbsp;</td></tr>
 
 	</table>
