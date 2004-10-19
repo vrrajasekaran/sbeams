cc84291e66a64e04c04d47eccc24fbc19ce84d6a
mkorb <mkorb@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2004-10-19 22:24:22 +0000

    intial check in

---
+++ sbeams/trunk/sbeams/doc/Cytometry/SampleNameConvention.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<HTML>
+<?php
+  /* $Id$ */
+  $TITLE="Cytometry Sample Name Convention";
+
+  include("../includes/style.inc.php");
+
+  include("../includes/header.inc.php");
+
+  include("../includes/navbar.inc.php");
+?>
+
+
+
+<!-- --------------------------- Main Page Content ------------------------ -->
+
+<table border=0 width="100%" bgcolor="#ffffff" cellpadding=10>
+<tr><td align="top">
+
+
+
+<H2>Cytometry Sample Name Convention</H2>
+
+Immunostain image files will be named according to the following convention
+for consistency, with 6 items separated by spaces:
+
+<PRE>
+02-002p_M_CD44_F_Hoechst 
+  |          |    |       |         |         |
+  |          |    |       |         |         +------ <font color=red>Subsequent Sort Entity</font> 
+  |          |    |       |         |        
+  |          |    |       |         +-------------  <font color=red>Subsequent Sort Type </font> (M = Magnetic Sort, F = Flow Sort)
+  |          |    |       | 
+  |          |    |       +---------------------  <font color=red>First Sort Entity </font>
+  |          |    |    
+  |          |    +---------------------------- <font color=red>First Sort Type </font> (M = Magnetic Sort, F = Flow Sort)
+  |          |   
+  |          +-------------------------------- <font color=red>Tissue Type </font> (p = prostate, b = bladder)
+  +---------------------------------------- <font color=red>Specimen Name as listed in the Immunostain database</font> 
+
+</PRE>
+<P> <font color=red>Sort Type</font> and<font colo =red> Sort Entity</font> can be repeated as a unit n-times, depending on the number of sorts.</P>
+
+<BR>
+<BR>
+<BR>
+
+
+
+<?php
+  include("../includes/footer.inc.php");
+?>
+
