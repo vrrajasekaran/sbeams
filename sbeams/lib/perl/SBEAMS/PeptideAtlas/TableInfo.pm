6cd8200452d3795ec5acac778ca287dcaae4c850
edeutsch <gasif555@gmail.com>
2004-12-03 07:14:43 +0000

    added sample_id column to allow click for more info

--- sbeams/lib/perl/SBEAMS/PeptideAtlas/TableInfo.pm
+++ sbeams/lib/perl/SBEAMS/PeptideAtlas/TableInfo.pm
@@ -80,7 +80,7 @@ sub returnTableInfo {
       
     if ($info_key eq "BASICQuery") {
       return( <<"      END_QUERY" ); 
-     	SELECT sample_tag, sample_title, 
+     	SELECT sample_id,sample_tag, sample_title, 
         anatomical_site_term, developmental_stage_term, pathology_term, 
         cell_type_term, data_contributors, is_public
       FROM $TBAT_SAMPLE 
