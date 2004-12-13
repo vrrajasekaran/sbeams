3e0f38a3eeb69abf3b759762fd630d0ac1391a6e
mjohnson <86867288+sreem20@users.noreply.github.com>
2004-12-13 18:59:37 +0000

    unit tests for SBEAMSClient.java

---
+++ sbeams/lib/java/SBEAMS/unitTests/SBEAMSClientTest.java
@@ -0,0 +1,54 @@
+// ExperimentRepositoryTest.java
+//--------------------------------------------------------------------------------------
+package SBEAMS.unitTests;
+//--------------------------------------------------------------------------------------
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+import SBEAMS.SBEAMSClient; 
+
+//--------------------------------------------------------------------------------------
+public class SBEAMSClientTest extends TestCase {
+
+  private String testUser = "cytoscape";
+  private String testPassword = "cytoscape";
+  private SBEAMSClient client;
+
+//------------------------------------------------------------------------------
+public SBEAMSClientTest (String name) 
+{
+  super (name);
+}
+//------------------------------------------------------------------------------
+public void setUp () throws Exception
+{
+  }
+//------------------------------------------------------------------------------
+public void tearDown () throws Exception
+{
+}
+//--------------------------------------------------------------------------------------
+public void testSBEAMSClientSetup () throws Exception
+{
+  System.out.println ("testSBEAMSClientSetup");
+  client = new SBEAMSClient(testUser, testPassword);
+  client.fetchSbeamsPage ("http://db.systemsbiology.net/sbeams/cgi/main.cgi");
+  assertTrue (client.getCookie() != null);
+} // testFileRepository
+//--------------------------------------------------------------------------------------
+public void testSecureSBEAMSClientSetup () throws Exception
+{
+  System.out.println ("testSecureSBEAMSClientSetup");
+  client = new SBEAMSClient(testUser, testPassword);
+  client.fetchSbeamsPage ("https://db.systemsbiology.net/sbeams/cgi/main.cgi");
+  assertTrue (client.getCookie() != null);
+} // testFileRepository
+//--------------------------------------------------------------------------------------
+public static void main (String [] args) 
+{
+  junit.textui.TestRunner.run (new TestSuite (SBEAMSClientTest.class));
+}
+//--------------------------------------------------------------------------------------
+} // ExperimentRepositoryTest
