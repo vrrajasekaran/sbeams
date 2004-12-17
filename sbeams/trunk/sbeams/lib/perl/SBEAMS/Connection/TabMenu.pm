30b6b9ec15bc84a031fa999a0640fbe7ed826a76
edeutsch <edeutsch@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2004-12-17 08:49:49 +0000

    fixed major security hole where passwords were being logged!!

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/Connection/TabMenu.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/Connection/TabMenu.pm
@@ -72,6 +72,9 @@ sub new {
   # Do we want to include current params?
   push @urlparams, -query => 1;
   $this->{_absQueryURL} = $cgi->url( @urlparams );
+
+  #### If there's a password in here, strip it out!
+  $this->{_absQueryURL} =~ s/password=.*?;/password=xxx;/;
   $log->debug( "Query URL is $this->{_absQueryURL}" );
 
 
