c65ef58923accadda7b160f4b7b4011c315562ac
mjohnson <86867288+sreem20@users.noreply.github.com>
2005-01-24 22:45:00 +0000

    formatting/indenting changes.  No functionality added or removed.

--- sbeams/lib/java/Microarray/DataLoader/WizardAdapter.java
+++ sbeams/lib/java/Microarray/DataLoader/WizardAdapter.java
@@ -1,7 +1,7 @@
 package DataLoader;
 public abstract class WizardAdapter implements WizardListener {
-    public WizardAdapter() {}
-    public void wizardCancelled(Wizard wizard) {}
-    public void wizardFinished(Wizard wizard) {}
-    public void wizardPanelChanged(Wizard wizard) {}
+  public WizardAdapter() {}
+  public void wizardCancelled(Wizard wizard) {}
+  public void wizardFinished(Wizard wizard) {}
+  public void wizardPanelChanged(Wizard wizard) {}
 }
