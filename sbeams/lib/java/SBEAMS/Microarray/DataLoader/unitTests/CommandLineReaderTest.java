04a23c240530e0ddbbd3fc61b42be37906503567
mjohnson <86867288+sreem20@users.noreply.github.com>
2005-01-28 23:33:35 +0000

    relocated source code. Originally at $SBEAMS/lib/java/.  Moved to conform to updated SBEAMS directory structure

---
+++ sbeams/lib/java/SBEAMS/Microarray/DataLoader/unitTests/CommandLineReaderTest.java
@@ -0,0 +1,43 @@
+/*
+ * Created on Jan 6, 2005
+ */
+package DataLoader.unitTests;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import DataLoader.CommandLineReader;
+//-----------------------------------------------------------------------------------------------
+/**
+ * @author mjohnson
+ */
+public class CommandLineReaderTest extends TestCase {
+//------------------------------------------------------------------------------
+  public CommandLineReaderTest (String name) {
+	super (name);
+  }
+//------------------------------------------------------------------------------
+  public void setUp () throws Exception {
+  }
+//------------------------------------------------------------------------------
+  public void tearDown () throws Exception {
+  }
+//-----------------------------------------------------------------------------------------------
+  public void testNoArgs() throws Exception {
+	System.out.println ("testNoArgs");
+	String[] arguments = {};
+	CommandLineReader clr = new CommandLineReader(arguments);
+	assertTrue(clr.helpRequested());
+  }
+//-----------------------------------------------------------------------------------------------
+  public void testWithArgs() throws Exception {
+	System.out.println ("testWithArgs");
+	String[] arguments = {"-m", "MatrixFile", "-t", "ExperimentTitle"};
+	CommandLineReader clr = new CommandLineReader(arguments);
+	assertTrue((clr.getMatrixFile()).equals("MatrixFile"));
+	assertTrue((clr.getExperimentTitle()).equals("ExperimentTitle"));
+  }
+//-----------------------------------------------------------------------------------------------
+  public static void main (String[] args) {
+	junit.textui.TestRunner.run (new TestSuite(CommandLineReaderTest.class));
+  }
+//-----------------------------------------------------------------------------------------------
+}
