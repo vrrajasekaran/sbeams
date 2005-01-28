d40eb879d40e8182c42f1b60c422e5311a67e8c4
mjohnson <86867288+sreem20@users.noreply.github.com>
2005-01-28 23:30:06 +0000

    relocating source code to exist in $SBEAMS/lib/java/SBEAMS/Microarray/DataLoader

--- sbeams/lib/java/Microarray/DataLoader/WizardAdapter.java
+++
@@ -1,7 +0,0 @@
-package DataLoader;
-public abstract class WizardAdapter implements WizardListener {
-  public WizardAdapter() {}
-  public void wizardCancelled(Wizard wizard) {}
-  public void wizardFinished(Wizard wizard) {}
-  public void wizardPanelChanged(Wizard wizard) {}
-}
--- sbeams/lib/java/Microarray/DataLoader/WizardListener.java
+++
@@ -1,7 +0,0 @@
-package DataLoader;
-//-----------------------------------------------------------------------------------------------
-public interface WizardListener {
-	public void wizardFinished(Wizard wizard);
-	public void wizardCancelled(Wizard wizard);
-	public void wizardPanelChanged(Wizard wizard);
-}
