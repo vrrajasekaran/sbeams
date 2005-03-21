696cb77d55660d56ee68ab5a519d4e2b489fd88d
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-03-21 21:34:58 +0000

    Changes to put proteomics selected tables under project control: proteomics_experiment biosequence_set biosequence fraction search_batch search_hit_annotation peptide_summary

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/Proteomics/TableInfo.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/Proteomics/TableInfo.pm
@@ -23,6 +23,9 @@ use SBEAMS::Connection::Settings;
 use SBEAMS::Connection::Tables;
 use SBEAMS::Proteomics::Settings;
 use SBEAMS::Proteomics::Tables;
+use SBEAMS::Connection::Log;
+
+my $log = SBEAMS::Connection::Log->new();
 
 
 ###############################################################################
@@ -43,17 +46,25 @@ sub new {
 # Return the required information about the specified Table
 ###############################################################################
 sub returnTableInfo {
-    my $self = shift;
-    my $table_name = shift || croak("parameter table_name not specified");
-    my $info_key = shift || croak("parameter info_key not specified");
-    my $parameters_ref = shift;
+  my $self = shift;
+  my $table_name = shift || croak("parameter table_name not specified");
+  my $info_key = shift || croak("parameter info_key not specified");
+  my $parameters_ref = shift;
 
-    my @row;
-    my $sql_query;
-    my $result;
+  my @row;
+  my $sql_query;
+  my $result;
 
-    #### Obtain main SBEAMS object for later use
-    my $sbeams = $self->getSBEAMS();
+  #### Obtain main SBEAMS object for later use
+  my $sbeams = $self->getSBEAMS();
+
+  # Fetch accessible projects
+  my @accessible_project_ids = $self->getSBEAMS()->getAccessibleProjects();
+	my $accessible_project_ids = join( ",", @accessible_project_ids ) || '0';
+
+  # Define project SQL hash.
+  my %projectSQL;
+  $log->debug( 'Getting project SQL ' ) if $info_key eq 'projPermSQL';
 
 
 ###############################################################################
@@ -65,94 +76,212 @@ sub returnTableInfo {
 #
 ###############################################################################
 ###############################################################################
-    if ($table_name eq "blxxxxxx") {
+  if ($table_name eq "blxxxxxx") {
 
-        if ($info_key eq "BASICQuery") {
-            return qq~
-		SELECT hardware_id,HT.name,make,model,serial_number,uri
+    if ($info_key eq "BASICQuery") {
+      return qq~
+  		SELECT hardware_id,HT.name,make,model,serial_number,uri
 		  FROM xxxxxxxx H
-		 INNER JOIN xxxxxxxxx HT
-		       ON (H.hardware_type_id=HT.hardware_type_id)
-		 WHERE H.record_status!='D'
-            ~;
-        }
-
-        if ($info_key eq "FULLQuery") {
-            return qq~
-		SELECT H.*
-		  FROM xxxxxxx H
+	 	  INNER JOIN xxxxxxxxx HT
+              ON (H.hardware_type_id=HT.hardware_type_id)
+      WHERE H.record_status!='D'
+      ~;
+    } elsif ($info_key eq "FULLQuery") {
+     return qq~
+		 SELECT H.*
+		 FROM xxxxxxx H
 		 INNER JOIN xxxxxx HT
-		       ON (H.hardware_type_id=HT.hardware_type_id)
+		         ON (H.hardware_type_id=HT.hardware_type_id)
 		 WHERE H.record_status!='D'
-            ~;
-        }
-
-
+     ~;
     }
 
+  } elsif ($table_name eq "PR_search_hit_annotation") {
 
-    if ($table_name eq "PR_search_hit_annotation") {
-
-        if ($info_key eq "BASICQuery") {
-            return qq~
-		SELECT *
+    if ($info_key eq "BASICQuery") {
+      return qq~
+	  	SELECT *
 		  FROM $TBPR_SEARCH_HIT SH
-		 WHERE SH.search_hit_id = '$parameters_ref->{search_hit_id}'
-            ~;
-        }
-
-        if ($info_key eq "FULLQuery") {
-            return qq~
-		SELECT *
+		  WHERE SH.search_hit_id = '$parameters_ref->{search_hit_id}'
+      ~;
+    } elsif ($info_key eq "FULLQuery") {
+      return qq~
+		  SELECT *
 		  FROM $TBPR_SEARCH_HIT SH
-		 WHERE SH.search_hit_id = '$parameters_ref->{search_hit_id}'
-            ~;
-        }
+		  WHERE SH.search_hit_id = '$parameters_ref->{search_hit_id}'
+      ~;
+    } elsif ( $info_key eq 'projPermSQL' ) {
+      $projectSQL{fsql} =<<"      SQL";
+      SELECT project_id 
+      FROM $TBPR_PROTEOMICS_EXPERIMENT PE
+      JOIN $TBPR_FRACTION F ON F.experiment_id = PE.experiment_id
+      JOIN $TBPR_MSMS_SPECTRUM MS ON F.fraction_id = MS.fraction_id 
+      JOIN $TBPR_SEARCH S ON S.msms_spectrum_id = MS.msms_spectrum_id 
+      JOIN $TBPR_SEARCH_HIT SH ON S.search_id = SH.search_id 
+      WHERE search_hit_id = KEYVAL 
+      SQL
 
+      $projectSQL{dbsql} =<<"      SQL";
+      SELECT project_id 
+      FROM $TBPR_PROTEOMICS_EXPERIMENT PE
+      JOIN $TBPR_FRACTION F ON F.experiment_id = PE.experiment_id
+      JOIN $TBPR_MSMS_SPECTRUM MS ON F.fraction_id = MS.fraction_id 
+      JOIN $TBPR_SEARCH S ON S.msms_spectrum_id = MS.msms_spectrum_id 
+      JOIN $TBPR_SEARCH_HIT SH ON S.search_id = SH.search_id 
+      JOIN $TBPR_SEARCH_HIT_ANNOTATION SHA ON SHA.search_hit_id = SH.search_hit_id 
+      WHERE search_hit_annotation_id = KEYVAL 
+      SQL
+      $log->debug( "Evaluate " . eval "$projectSQL{dbsql}" );
 
+      return \%projectSQL
     }
+  # End if search_hit_annotation
 
+  } elsif ($table_name eq "PR_proteomics_experiment") {
 
-    if ($table_name eq "PR_proteomics_experiment") {
+    if ($info_key eq "BASICQuery") {
+      return qq~
+  		SELECT experiment_id,username,P.project_id AS "proj",
+  		       name AS 'project_name',PE.experiment_id AS "exp",
+  		       experiment_tag,experiment_name,O.organism_name,
+                         experiment_description
+  		FROM $TBPR_PROTEOMICS_EXPERIMENT PE
+  		INNER JOIN $TB_USER_LOGIN UL ON (PE.contact_id=UL.contact_id)
+  		INNER JOIN $TB_PROJECT P ON (PE.project_id=P.project_id)
+  		LEFT JOIN $TB_ORGANISM O ON (PE.organism_id=O.organism_id)
+  		WHERE PE.record_status!='D'
+  		  AND UL.record_status!='D'
+  		  AND P.record_status!='D'
+        AND P.project_id IN ( $accessible_project_ids )
+      ORDER BY username,experiment_tag
+      ~;
+      } elsif ($info_key eq "FULLQuery") {
+       return qq~
+  		 SELECT PE.*
+  		 FROM $TBPR_PROTEOMICS_EXPERIMENT PE
+  		 WHERE PE.record_status!='D'
+         AND PE.project_id IN ( $accessible_project_ids )
+       ~;
+  
+     } elsif ( $info_key eq 'projPermSQL' ) {
+       $projectSQL{fsql} = '';
 
-        my @accessible_project_ids = $self->getSBEAMS()->getAccessibleProjects();
-	my $accessible_project_ids = join( ",", @accessible_project_ids ) || '0';
+       $projectSQL{dbsql} =<<"       SQL";
+       SELECT project_id 
+       FROM $TBPR_PROTEOMICS_EXPERIMENT PE 
+       WHERE experiment_id = KEYVAL 
+       SQL
 
-        if ($info_key eq "BASICQuery") {
-            return qq~
-		SELECT experiment_id,username,P.project_id AS "proj",
-		       name AS 'project_name',PE.experiment_id AS "exp",
-		       experiment_tag,experiment_name,O.organism_name,
-                       experiment_description
-		  FROM $TBPR_PROTEOMICS_EXPERIMENT PE
-		 INNER JOIN $TB_USER_LOGIN UL ON (PE.contact_id=UL.contact_id)
-		 INNER JOIN $TB_PROJECT P ON (PE.project_id=P.project_id)
-		  LEFT JOIN $TB_ORGANISM O ON (PE.organism_id=O.organism_id)
-		 WHERE PE.record_status!='D'
-		   AND UL.record_status!='D'
-		   AND P.record_status!='D'
-                   AND P.project_id IN ( $accessible_project_ids )
-		 ORDER BY username,experiment_tag
-            ~;
+       return \%projectSQL
+     }
+   # End if proteomics_expt
 
-        }
+  } elsif ( $table_name eq 'PR_biosequence_set' ) {
+     
+    if ( $info_key eq 'projPermSQL' ) {
+      $projectSQL{fsql} = '';
 
+      $projectSQL{dbsql} =<<"      SQL";
+      SELECT project_id 
+      FROM $TBPR_BIOSEQUENCE_SET 
+      WHERE biosequence_set_id = KEYVAL 
+      SQL
 
-        if ($info_key eq "FULLQuery") {
-            return qq~
-		SELECT PE.*
-		  FROM $TBPR_PROTEOMICS_EXPERIMENT PE
-		 WHERE PE.record_status!='D'
-                   AND PE.project_id IN ( $accessible_project_ids )
-            ~;
-
-        }
-
+      return \%projectSQL
     }
+  # End if biosequence_set
+     
+  } elsif ( $table_name eq 'PR_gradient_program' ) {
+     
+    if ( $info_key eq 'projPermSQL' ) {
+      $projectSQL{fsql} = '';
 
+      $projectSQL{dbsql} =<<"      SQL";
+      SELECT project_id 
+      FROM $TBPR_GRADIENT_PROGRAM
+      WHERE gradient_program_id = KEYVAL
+      SQL
 
+      return \%projectSQL
+    }
+  # End if gradient_program
+     
+  } elsif ( $table_name eq 'APD_peptide_summary' ) {
+     
+    if ( $info_key eq 'projPermSQL' ) {
+      $projectSQL{fsql} = '';
 
-    if ($table_name eq "PR_publication") {
+      $projectSQL{dbsql} =<<"      SQL";
+      SELECT project_id 
+      FROM $TBAPD_PEPTIDE_SUMMARY
+      WHERE peptide_summary_id = KEYVAL
+      SQL
+
+      return \%projectSQL
+    }
+  # End if peptide_summary
+     
+  } elsif ( $table_name eq 'PR_fraction' ) {
+     
+    if ( $info_key eq 'projPermSQL' ) {
+      $projectSQL{fsql} =<<"      SQL";
+      SELECT project_id 
+      FROM $TBPR_PROTEOMICS_EXPERIMENT 
+      WHERE experiment_id = KEYVAL 
+      SQL
+
+      $projectSQL{dbsql} =<<"      SQL";
+      SELECT project_id 
+      FROM $TBPR_PROTEOMICS_EXPERIMENT PE 
+      JOIN $TBPR_FRACTION F ON F.experiment_id = PE.experiment_id
+      WHERE fraction_id = KEYVAL 
+      SQL
+
+      return \%projectSQL
+    }
+  # End if fraction
+
+  } elsif ( $table_name eq 'PR_search_batch' ) {
+     
+    if ( $info_key eq 'projPermSQL' ) {
+      $projectSQL{fsql} =<<"      SQL";
+      SELECT project_id 
+      FROM $TBPR_PROTEOMICS_EXPERIMENT 
+      WHERE experiment_id = KEYVAL 
+      SQL
+
+      $projectSQL{dbsql} =<<"      SQL";
+      SELECT project_id 
+      FROM $TBPR_PROTEOMICS_EXPERIMENT PE 
+      JOIN $TBPR_SEARCH_BATCH SB ON SB.experiment_id = PE.experiment_id
+      WHERE search_batch_id = KEYVAL 
+      SQL
+
+      return \%projectSQL
+    }
+  # End if search_batch
+
+  } elsif ( $table_name eq 'PR_biosequence' ) {
+     
+    if ( $info_key eq 'projPermSQL' ) {
+      $projectSQL{fsql} =<<"      SQL";
+      SELECT project_id 
+      FROM $TBPR_BIOSEQUENCE_SET 
+      WHERE biosequence_set_id = KEYVAL 
+      SQL
+
+      $projectSQL{dbsql} =<<"      SQL";
+      SELECT project_id 
+      FROM $TBPR_BIOSEQUENCE_SET BS
+      JOIN $TBPR_BIOSEQUENCE B ON B.biosequence_set_id = BS.biosequence_set_id
+      WHERE biosequence_id = KEYVAL 
+      SQL
+
+      return \%projectSQL
+    }
+  # End if biosequence
+
+  } elsif ($table_name eq "PR_publication") {
 
         if ($info_key eq "BASICQuery") {
             return qq~
@@ -247,6 +376,7 @@ sub getParentProject {
 
   #### Get sbeams object, needed for Core tables and SQL queries
   my $sbeams = $self->getSBEAMS();
+  $log->debug( $table_name );
 
   #### Define the project_id, starting as undef, it gets filled if there is one
   my $project_id;
@@ -259,12 +389,108 @@ sub getParentProject {
   );
   return($project_id) if ($project_id);
 
+  # Value may be cached/defined in input parameters... for reasons unknown!
+  # return $parameters_ref->{project_id} if $parameters_ref->{project_id};
+  # $log->debug( 'Got past the short-circuits with ' . $table_name );
+
+  # Fetch SQL to retrieve project_id from table_name, if it is available.
+  my $sqlref = $self->returnTableInfo( $table_name, 'projPermSQL' );
+
+  # If we don't have it 
+  unless ( ref( $sqlref ) && $sqlref->{dbsql} ) {
+    $log->error( "dbsql not defined for $table_name\n" );
+    return undef;
+  }
+
 
   #############################################################################
   #### Process actions for individual tables
 
-  #### If table is xxxx
-  if ($table_name eq "xxxx") {
+  if ( $table_name  eq 'PR_proteomics_experiment' ) {
+
+    if ($action eq 'UPDATE' || $action eq 'DELETE') {
+
+      # The array table has project_id in it
+      if ( $parameters_ref->{experiment_id} ) {
+        $sqlref->{dbsql} =~ s/KEYVAL/$parameters_ref->{experiment_id}/;
+        ( $project_id ) = $sbeams->selectOneColumn( $sqlref->{dbsql} );
+      }
+    }
+    return ( $project_id ) ? $project_id : undef;
+
+  } elsif ( $table_name  eq 'PR_biosequence_set' ) {
+
+    if ($action eq 'UPDATE' || $action eq 'DELETE') {
+
+      # The array table has project_id in it
+      if ( $parameters_ref->{biosequence_set_id} ) {
+        $sqlref->{dbsql} =~ s/KEYVAL/$parameters_ref->{biosequence_set_id}/;
+        ( $project_id ) = $sbeams->selectOneColumn( $sqlref->{dbsql} );
+      }
+    }
+    return ( $project_id ) ? $project_id : undef;
+
+  } elsif ( $table_name  eq 'PR_gradient_program' ) {
+
+    if ($action eq 'UPDATE' || $action eq 'DELETE') {
+
+      # The array table has project_id in it
+      if ( $parameters_ref->{gradient_program_id} ) {
+        $sqlref->{dbsql} =~ s/KEYVAL/$parameters_ref->{gradient_program_id}/;
+        ( $project_id ) = $sbeams->selectOneColumn( $sqlref->{dbsql} );
+      }
+    }
+    return ( $project_id ) ? $project_id : undef;
+
+  } elsif ( $table_name  eq 'APD_peptide_summary' ) {
+
+    if ($action eq 'UPDATE' || $action eq 'DELETE') {
+
+      # The array table has project_id in it
+      if ( $parameters_ref->{peptide_summary_id} ) {
+        $sqlref->{dbsql} =~ s/KEYVAL/$parameters_ref->{peptide_summary_id}/;
+        ( $project_id ) = $sbeams->selectOneColumn( $sqlref->{dbsql} );
+      }
+    }
+    return ( $project_id ) ? $project_id : undef;
+
+  } elsif ( $table_name  eq 'PR_fraction' ) {
+
+    if ($action eq 'UPDATE' || $action eq 'DELETE') {
+
+      # The array table has project_id in it
+      if ( $parameters_ref->{fraction_id} ) {
+        $sqlref->{dbsql} =~ s/KEYVAL/$parameters_ref->{fraction_id}/;
+        ( $project_id ) = $sbeams->selectOneColumn( $sqlref->{dbsql} );
+      }
+    }
+    return ( $project_id ) ? $project_id : undef;
+
+  } elsif ( $table_name  eq 'PR_search_batch' ) {
+
+    if ($action eq 'UPDATE' || $action eq 'DELETE') {
+
+      # The array table has project_id in it
+      if ( $parameters_ref->{search_batch_id} ) {
+        $sqlref->{dbsql} =~ s/KEYVAL/$parameters_ref->{search_batch_id}/;
+        ( $project_id ) = $sbeams->selectOneColumn( $sqlref->{dbsql} );
+      }
+    }
+    return ( $project_id ) ? $project_id : undef;
+
+  } elsif ( $table_name  eq 'PR_search_hit_annotation' ) {
+
+    if ($action eq 'UPDATE' || $action eq 'DELETE') {
+
+      # The array table has project_id in it
+      if ( $parameters_ref->{search_hit_annotation_id} ) {
+        $sqlref->{dbsql} =~ s/KEYVAL/$parameters_ref->{search_hit_annotation_id}/;
+        ( $project_id ) = $sbeams->selectOneColumn( $sqlref->{dbsql} );
+      }
+    }
+    return ( $project_id ) ? $project_id : undef;
+
+  } elsif ($table_name eq "zzzz") {
 
     #### If the user wants to INSERT, determine how it fits into project
     if ($action eq 'INSERT') {
