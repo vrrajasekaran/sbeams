213cc4d4aae0d55e1a73404a80524ba516d8afbe
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-04-04 19:08:05 +0000

    Files to build required work_groups and TGS entries.

---
+++ sbeams/trunk/sbeams/lib/refdata/Microarray/microarray_table_group_security.sql
@@ -0,0 +1,49 @@
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 50,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'admin','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_admin';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 25,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'arrays','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_admin';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 20,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'common','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_admin';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 20,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'infrastructure','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_admin';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 25,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'process','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_admin';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 30,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'project','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_admin';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 20,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'request','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_admin';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 30,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'rowprivate','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_admin';
+
+
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 50,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'admin','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_user';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 40,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'arrays','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_user';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 20,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'common','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_user';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 20,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'infrastructure','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_user';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 30,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'process','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_user';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 30,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'project','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_user';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 20,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'request','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_user';
+
+INSERT INTO table_group_security (privilege_id,record_status,created_by_id,date_created,date_modified,table_group,comment,modified_by_id,owner_group_id,work_group_id)
+SELECT 30,'N',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'rowprivate','',2,1,work_group_id FROM work_group WHERE work_group_name = 'Microarray_user';
