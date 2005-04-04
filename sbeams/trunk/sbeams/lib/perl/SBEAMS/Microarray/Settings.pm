162bdcb2e2cf657ef13136821f4df5435e5ea0eb
pmoss <pmoss@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-04-04 22:57:45 +0000

    Tweaked the setting layout to be more readable

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/Microarray/Settings.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/Microarray/Settings.pm
@@ -38,14 +38,19 @@ require Exporter;
 
 #### Define new variables
 $SBEAMS_PART            = 'MicroArray';
+
+#Edit Location to suit installation requirements
 $AFFY_DEFAULT_DIR	= '/net/arrays/Affymetrix/core/probe_data';
+$BIOCONDUCTOR_DELIVERY_PATH = '/net/arrays/Affymetrix/core/data_analysis/delivery';
+$ADD_ANNOTATION_OUT_FOLDER = "$PHYSICAL_BASE_DIR/tmp/Microarray/Add_affy_annotation";
+
 @AFFY_DEFAULT_FILES	= qw(CHP CEL XML INFO RPT R_CHP JPEG EGRAM_PF.jpg EGRAM_T.jpg EGRAM_F.jpg);		#files that will be used to determine if a group of files, all sharing the same basename, are all present when uploading Affy arrays 
 
 $AFFY_ZIP_REQUEST_DIR 	= '/net/arrays/Affy_Zip_Request';
 
 $AFFY_R_CHP_ANALYSIS_PROTOCOL = 'R Mas5.0 CHP';				#Current protocol that describes the R script to produce the CHP like file
 
-$BIOCONDUCTOR_DELIVERY_PATH = '/net/arrays/Affymetrix/core/data_analysis/delivery';#/net/dblocal/www/html/devPM/sbeams/bioconductor/delivery
+$BIOCONDUCTOR_DELIVERY_PATH = '/net/arrays/Affymetrix/core/data_analysis/delivery';
 $ADD_ANNOTATION_OUT_FOLDER = "$PHYSICAL_BASE_DIR/tmp/Microarray/Add_affy_annotation";
 #### Override variables from main Settings.pm
 $SBEAMS_SUBDIR          = 'Microarray';
