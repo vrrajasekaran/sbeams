83717d46f89d836c8b9dd206ab4d98fec4e36e23
pmoss <pmoss@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-04-12 17:55:35 +0000

    Added 4 more entries to the CSS

--- sbeams/lib/perl/SBEAMS/Connection/HTMLPrinter.pm
+++ sbeams/lib/perl/SBEAMS/Connection/HTMLPrinter.pm
@@ -254,13 +254,15 @@ sub printStyleSheet {
 	A:visited.nav {  font-family: Helvetica, Arial, sans-serif; color: #000000}
 	A:hover.nav {  font-family: Helvetica, Arial, sans-serif; color: red;}
 	.nav {  font-family: Helvetica, Arial, sans-serif; color: #000000}
+	.white_bg{background-color: #FFFFFF }
 	.grey_bg{ background-color: #CCCCCC }
 	.med_gray_bg{ background-color: #CCCCCC; ${FONT_SIZE_LG}pt; font-weight: bold; Padding:2}
 	.grey_header{ font-family: Helvetica, Arial, sans-serif; color: #000000; font-size: ${FONT_SIZE_HG}pt; background-color: #CCCCCC; font-weight: bold; padding:1 2}
 	.rev_gray{background-color: #555555; ${FONT_SIZE_LG}pt; font-weight: bold; color:white; line-height: 25px;}
 	.blue_bg{ font-family: Helvetica, Arial, sans-serif; background-color: #4455cc; ${FONT_SIZE_HG}pt; font-weight: bold; color: white}
+	.lite_blue_bg{font-family: Helvetica, Arial, sans-serif; background-color: #eeeeff; ${FONT_SIZE_HG}pt; color: #cc1111; font-weight: bold;border-style: solid; border-width: 1px; border-color: #555555 #cccccc #cccccc #555555;}
 	.orange_bg{ background-color: #FFCC66; ${FONT_SIZE_LG}pt; font-weight: bold}
-	.red_bg{ background-color: #882222; ${FONT_SIZE_LG}pt; font-weight: bold; color:white;}
+	.red_bg{ background-color: #882222; ${FONT_SIZE_LG}pt; font-weight: bold; color:white; Padding:2}
 	.small_cell {font-size: 8; background-color: #CCCCCC; white-space: nowrap  }
 	.anno_cell {white-space: nowrap  }
 	.present_cell{border: none}
