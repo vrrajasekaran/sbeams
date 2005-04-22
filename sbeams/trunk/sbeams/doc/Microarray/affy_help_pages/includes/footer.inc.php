a11a09be45d53dd6f39c3164e9fb0849482a5a15
pmoss <pmoss@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-04-22 23:33:37 +0000

    Adding Affy help pages

---
+++ sbeams/trunk/sbeams/doc/Microarray/affy_help_pages/includes/footer.inc.php
@@ -0,0 +1,23 @@
+
+<!-- ------------------------ End of main content ----------------------- -->
+
+
+
+</td></tr>
+</table>
+
+
+</td></tr>
+</table>
+
+<BR>
+<hr size=1 noshade width="55%" align="left" color="#FF8700">
+<TABLE border="0">
+<TR><TD><IMG SRC="images/ISB_symbol_tiny.jpg"></TD>
+<TD><nowrap>ISB</nowrap></A></TD></TR>
+</TABLE>
+<BR>
+<BR>
+
+</body>
+</html>
