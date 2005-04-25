f97bf3c8c191135f41e38e4cb09c515651e0eb26
pmoss <pmoss@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-04-25 21:16:43 +0000

    Moved some php variables around

--- sbeams/trunk/sbeams/doc/Microarray/affy_help_pages/includes/transformation_info.inc.php
+++ sbeams/trunk/sbeams/doc/Microarray/affy_help_pages/includes/transformation_info.inc.php
@@ -1,13 +1,17 @@
 <?php
   /* $Id$ */
-
+/*
 $TOMCAT_SERVER = "http://helene/xalanservlet/xalan_transform_dev.jsp?PMA=foo";
 $XSLT_URL = "http://affy.systemsbiology.net/Affy_help/Affy_help.xslt";
 $XML_URL  = "http://affy.systemsbiology.net/Affy_help";
-
-$BASE_PATH = "/net/dblocal/www/html/devPM/sbeams/doc/Microarray/affy_help_pages/Affy_help";
+*/
+$URL_BASE = "http://affy.systemsbiology.net/dev1/sbeams/doc/Microarray/affy_help_pages";
+$BASE_PATH = "/net/dblocal/wwwspecial/affy/dev1/sbeams/doc/Microarray/affy_help_pages/Affy_help";
 $XSLT_PATH = "$BASE_PATH/Affy_help.xslt";
 $XML_PATH  = $BASE_PATH;
+
+$SERVER = "www.sbeams.org";
+$HOME_PAGE_URL = "http://intranet.systemsbiology.net";
 
 
 ?>
