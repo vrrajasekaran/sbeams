0b7ba6042c2ae25b6d50b728296502378d991d75
pmoss <pmoss@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-05-06 21:41:47 +0000

    Removed WITH DROP_EXISTING statments from three indexes on Condition table, since no indexes exists when initially setting up the tables

--- sbeams/trunk/sbeams/lib/sql/Microarray/Microarray_CREATEINDEXES.sql
+++ sbeams/trunk/sbeams/lib/sql/Microarray/Microarray_CREATEINDEXES.sql
@@ -75,18 +75,18 @@ CREATE NONCLUSTERED INDEX idx_afa_id_probe_set_id_protocol_id
    ON dbo.affy_gene_intensity (  affy_array_id,probe_set_id, protocol_id )    
 
 go
--- affy_gene_intensity_   affy_array_id,probe_set_id, protocol_id
+-- condition  condition_id
 CREATE NONCLUSTERED INDEX idx_condition_id
    ON dbo.gene_expression ( condition_id )    
-  WITH DROP_EXISTING
-go
 
+go
+-- condition  gene_name
 CREATE NONCLUSTERED INDEX idx_gene_name
    ON dbo.gene_expression ( gene_name )    
-  WITH DROP_EXISTING
+ 
 go
-
+-- condition  biosequence_id
 CREATE NONCLUSTERED INDEX idx_biosequence_id
    ON dbo.gene_expression ( biosequence_id )    
-  WITH DROP_EXISTING
+
 go
