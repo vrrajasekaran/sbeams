f2fb5c872227eb1a67595c939104a30798ad2b0f
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-08-05 00:24:47 +0000

    Modified header fingerprint; added header name info; added caching of parsed info for later processing.

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/Biomarker/ParseSampleWorkbook.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/Biomarker/ParseSampleWorkbook.pm
@@ -1,4 +1,4 @@
-zckage SBEAMS::Biomarker::ParseSampleWorkbook;
+package SBEAMS::Biomarker::ParseSampleWorkbook;
 
 ###############################################################################
 #
@@ -29,7 +29,7 @@ sub new {
 #+
 # Based on file format as of 2005-08-01
 #-
-sub parseFile {
+sub parse_file {
 	my $this = shift;
 	my %args = @_;
 
@@ -39,20 +39,125 @@ sub parseFile {
   my @headings;
   # column heading indicies
   my %idx;
+  my %seen;
 
   open( WBOOK, "<$args{wbook}" ) || die "unable to open $args{wbook}";
   while( my $line = <WBOOK> ){
     chomp $line;
+    next if $line =~ /^\s*$/;
     my @line = split( "\t", $line, -1 );
     unless( @headings ) {
       # Meaningful headings fingerprint?
-      next unless ( $line[0] =~ /Sample Setup Order/ &&
-                    $line[1] =~ /MS Sample Run Number/ &&
-                    $line[1] =~ /ISB Sample ID/ );
+      next unless ( $line[0] =~ /Sample Setup Order/  &&
+                    $line[1] =~ /MS Sample Run Number/ );
       @headings = @line;
+      my $i = 0;
+      foreach (@headings) {
+        $idx{$_} = $i++;
+      }
+      $this->{_headidx} = \%idx;
+      next;
     }
 
+    push @{$this->{_data}, \@line;
+    next;
 
+    # Vestigal
+    my $i = 0;
+    for my $item ( @line ) {
+      if ( defined $item && $item !~ /^\s*$/ ){
+        print "$headings[$i]\t$item\n";
+        $seen{$i}++;
+      }
+      $i++;
+    }
   }
+  return;
 
+  # Vestigal
+  for my $k ( sort { $a <=> $b } keys(%seen) ) {
+  }
 }
+
+1;
+
+__DATA__
+
+# Attributes 
+'Sample Setup Order'
+'MS Sample Run Number'
+'Name of Investigators'
+'PARAM:time of sample collection '
+'PARAM:meal'
+'PARAM:alcohole'
+'PARAM:smoke'
+'PARAM:Date of Sample Collection'
+'Study Histology'
+
+# Bioource 
+'ISB sample ID'
+'Patient_id'
+'External Sample ID'
+'Name of Institute'
+'species'
+'age'
+'gender'
+
+'Sample type'
+
+# Biosample
+'amount of sample received'
+'Location of orginal sample'
+
+# Disease
+'Disease:Breast cancer'
+'Disease:Ovarian cancer'
+'Disease:Prostate cancer'
+'Disease:Blader Cancer'
+'Disease:Skin cancer'
+'Disease:Lung cancer'
+'Disease: Huntington\'s Disease'
+'diabetic'
+
+# tissue_type
+'heart'
+'blood'
+'liver'
+'neuron'
+'lung'
+'bone'
+
+biosource_disease
+'Disease Stage'
+
+#orphan
+'Disease Info: Group'
+'Prep Replicate id'
+'Sample Prep Name'
+'status of sample prep'
+'date of finishing prep'
+'amount of sample used in prep'
+'Sample prep method'
+'person prepared the samples'
+'Volume of re-suspended sample'
+'location of finished sample prep'
+'MS Replicate Number'
+'MS Run Name'
+'status of MS'
+'date finishing MS'
+'Random Sample Run order'
+'order of samples ran per day'
+'MS run protocol'
+'Volume Injected'
+'location of data'
+'status of Conversion'
+'Date finishing conversion'
+'name of raw files'
+'location of raw files'
+'name of mzXML'
+'location of mzXML'
+'person for MS analysis'
+'date finishing alignment'
+'location of alignment files'
+'person for data analysis'
+'peplist peptide peaks file location'
