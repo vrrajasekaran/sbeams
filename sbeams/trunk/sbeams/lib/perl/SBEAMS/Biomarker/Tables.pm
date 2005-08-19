f643538f024b2e27325e42f34bc5ed52a4fba606
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-08-19 01:34:58 +0000

    Ongoing construction

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/Biomarker/Tables.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/Biomarker/Tables.pm
@@ -19,11 +19,13 @@ use SBEAMS::Connection::Settings;
 use vars qw(@ISA @EXPORT 
     $TB_ORGANISM
 
-    $TBModTmpTAG_BIOSEQUENCE_SET
-    $TBModTmpTAG_DBXREF
-    $TBModTmpTAG_BIOSEQUENCE
-    $TBModTmpTAG_BIOSEQUENCE_PROPERTY_SET
-    $TBModTmpTAG_QUERY_OPTION
+    $TBBL_BIOSEQUENCE_SET
+    $TBBL_DBXREF
+    $TBBL_BIOSEQUENCE
+    $TBBL_BIOSEQUENCE_PROPERTY_SET
+    $TBBL_QUERY_OPTION
+		$TBBM_TREATMENT
+		$TBBM_TREATMENT_TYPE
 
 );
 
@@ -34,26 +36,31 @@ require Exporter;
 @EXPORT = qw (
     $TB_ORGANISM
 
-    $TBModTmpTAG_BIOSEQUENCE_SET
-    $TBModTmpTAG_DBXREF
-    $TBModTmpTAG_BIOSEQUENCE
-    $TBModTmpTAG_BIOSEQUENCE_PROPERTY_SET
-    $TBModTmpTAG_QUERY_OPTION
+    $TBBL_BIOSEQUENCE_SET
+    $TBBL_DBXREF
+    $TBBL_BIOSEQUENCE
+    $TBBL_BIOSEQUENCE_PROPERTY_SET
+    $TBBL_QUERY_OPTION
+		$TBBM_TREATMENT
+		$TBBM_TREATMENT_TYPE
 
 );
 
 
 #### Get the appropriate database prefixes for the SBEAMS core and this module
 my $core = $DBPREFIX{Core};
+my $bl = $DBPREFIX{Biolink};
 my $mod = $DBPREFIX{Biomarker};
 
 $TB_ORGANISM                      = "${core}organism";
 
-$TBModTmpTAG_BIOSEQUENCE_SET       = "${mod}biosequence_set";
-$TBModTmpTAG_DBXREF                = "${mod}dbxref";
-$TBModTmpTAG_BIOSEQUENCE           = "${mod}biosequence";
-$TBModTmpTAG_BIOSEQUENCE_PROPERTY_SET   = "${mod}biosequence_property_set";
-$TBModTmpTAG_QUERY_OPTION          = "${mod}query_option";
+$TBBL_BIOSEQUENCE_SET       = "${bl}biosequence_set";
+$TBBL_DBXREF                = "${bl}dbxref";
+$TBBL_BIOSEQUENCE           = "${bl}biosequence";
+$TBBL_BIOSEQUENCE_PROPERTY_SET   = "${bl}biosequence_property_set";
+$TBBL_QUERY_OPTION          = "${bl}query_option";
+$TBBM_TREATMENT             = "${mod}treatment";
+$TBBM_TREATMENT_TYPE        = "${mod}treatment_type";
 
 
 
