d3576a45008a044ab2474d858bd3753b5a9dfecf
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-08-19 01:39:01 +0000

    Ongoing changes.

--- sbeams/lib/scripts/Biomarker/uploadWorkbook.pl
+++ sbeams/lib/scripts/Biomarker/uploadWorkbook.pl
@@ -1,12 +1,62 @@
 #!/usr/local/bin/perl -w
 #
 
+use FindBin qw($Bin);
+use Getopt::Long;
 
-use lib( "../../perl" );
+use lib( "$Bin/../../perl" );
 use SBEAMS::Biomarker::ParseSampleWorkbook;
 
-my $parser = SBEAMS::Biomarker::ParseSampleWorkbook->new();
-for my $file ( @ARGV ) {
-  $parser->parse_file( wbook => $file );
+use strict;
+
+# Main
+{
+  my $params = processParams();
+  
+  # Instantiate new parser
+  my $parser = SBEAMS::Biomarker::ParseSampleWorkbook->new();
+  
+  # Give parser workbook file to chew on
+  $parser->parse_file( wbook => $params->{workbook} );
+  
+  # Allow parser to chew data into digestable bits
+  $parser->process_data();
+  
+  sub processParams {
+    my %params;
+    GetOptions( \%params, "workbook=s" );
+  
+    unless( $params{workbook} ) {
+      printUsage( "Missing required parameter 'workbook'" );
+    }
+    return \%params;
+  }
 }
 
+sub printUsage {
+  my $msg = shift || '';
+
+  $|++;
+
+  print <<"  END_USAGE";
+  $msg
+
+  Usage: 
+  uploadWorkbook.pl --workbook path/to/workbook/filename.txt
+
+  Arguements:
+  -w --workbook      Filename of workbook file (tsv) to upload
+
+  END_USAGE
+  
+  exit; 
+}
+
+__DATA__
+
+  my %hidx = %{$parser->{_headidx}};
+  for my $k ( keys( %hidx ) ){
+    my $name;
+    ( $name = $file ) =~ s/Sample_progress_workbook.csv//g;
+    my $pos = ( length( $hidx{$k} ) == 1 ) ? '0' . $hidx{$k} : $hidx{$k};
+  }
