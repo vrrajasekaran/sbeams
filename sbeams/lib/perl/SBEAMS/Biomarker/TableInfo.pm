37d456724b6bb41e488da2bb1e87be9ae2dfad6c
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-08-23 00:23:50 +0000

    Building tableInfo responses.

--- sbeams/lib/perl/SBEAMS/Biomarker/TableInfo.pm
+++ sbeams/lib/perl/SBEAMS/Biomarker/TableInfo.pm
@@ -1,26 +1,21 @@
 package SBEAMS::Biomarker::TableInfo;
-
-###############################################################################
-# Program     : SBEAMS::Biomarker::TableInfo
-# Author      : Eric Deutsch <edeutsch@systemsbiology.org>
+# Description : SBEAMS::Biomarker component which returns various table info.
 # $Id$
-#
-# Description : This is part of the SBEAMS::Biomarker module which returns
-#               information about various tables.
-#
-###############################################################################
 
 use strict;
 use CGI::Carp qw(fatalsToBrowser croak);
 
 use SBEAMS::Biomarker::Settings;
 use SBEAMS::Biomarker::Tables;
+
 use SBEAMS::Connection::Tables;
+use SBEAMS::Connection::Log;
 
+my $log = SBEAMS::Connection::Log->new();
 
-###############################################################################
+#+
 # Constructor
-###############################################################################
+#-
 sub new {
     my $this = shift;
     my $class = ref($this) || $this;
@@ -30,68 +25,193 @@ sub new {
 }
 
 
-###############################################################################
-# Return Table Info
+#+
+# Returns information based on subject table and key value. Note that arguments
+# have a requisite order, table_name then info_key.
 #
-# Return the required information about the specified Table
+# arguements:  table_name  Name of table in table_properties table (req)
+# arguements:  info_key    'Key' for specifying which info is needed (req)
+#
+# returns:     one or more bits of information, depending on key.
+#-
 ###############################################################################
 sub returnTableInfo {
-    my $self = shift;
-    my $table_name = shift || croak("parameter table_name not specified");
-    my $info_key = shift || croak("parameter info_key not specified");
+  my $self = shift;
+  my $table_name = shift || croak("parameter table_name not specified");
+  my $info_key = shift || croak("parameter info_key not specified");
 
-    my @row;
-    my $sql_query;
-    my $result;
+  my @row;
+  my $sql_query;
+  my $result;
 
+  my $sbeams = $self->getSBEAMS();
+  
+  my $tinfo = $self->getTableProperties($table_name);
+  my $cinfo = $self->getColumnProperties($table_name);
 
-###############################################################################
-#
-# First we have table-specific overrides of the default answers
-#
-# This is mostly just Queries now.  This should be pushed out into a
-# nicely formatted file of queries.
-#
-###############################################################################
-###############################################################################
-    if ($table_name eq "blxxxxxx") {
+  my $dbtable = $sbeams->evalSQL( $tinfo->{db_table_name} );
 
-        if ($info_key eq "BASICQuery") {
-            return qq~
-		SELECT hardware_id,HT.name,make,model,serial_number,uri
-		  FROM xxxxxxxx H
-		  JOIN xxxxxxxxx HT
-		       ON (H.hardware_type_id=HT.hardware_type_id)
-		 WHERE H.record_status!='D'
-            ~;
-        }
+#  for my $t ( keys( %$tinfo ) ) {
+#    $log->debug( "TINFO: $t => $tinfo->{$t}" );
+#  }
 
-        if ($info_key eq "FULLQuery") {
-            return qq~
-		SELECT H.*
-		  FROM xxxxxxx H
-		  JOIN xxxxxx HT
-		       ON (H.hardware_type_id=HT.hardware_type_id)
-		 WHERE H.record_status!='D'
-            ~;
-        }
+ 
 
 
+  # First we have table-specific overrides of the default answers
+  if ($table_name eq "TABLESQUE") {
+
+    if ($info_key eq "BASICQuery") {
+      return qq~
+   		SELECT 
+      FROM $table_name T
+      JOIN sometable S
+      ON ( H.some_field = S.some_field )
+      WHERE T.record_status!='D'
+      AND S.record_status!='D'
+      ~;
+
+    } elsif ($info_key eq "FULLQuery") {
+      return qq~
+   		SELECT 
+      FROM $table_name T
+      JOIN sometable S
+      ON ( H.some_field = S.some_field )
+      WHERE T.record_status!='D'
+      AND S.record_status!='D'
+      ~;
     }
 
+  } elsif ($table_name eq "TABLESQUE") {
 
+  } else { # Non table-specific responses.
 
-###############################################################################
+    if ($info_key eq "BASICQuery") {
+      return qq~
+ 		  SELECT *
+      FROM $tinfo
+      WHERE record_status!='D'
+      ~;
 
-    #### Obtain main SBEAMS object and fall back to its TableInfo handler
-    my $sbeams = $self->getSBEAMS();
-    my @temp_result = $sbeams->returnTableInfo($table_name,$info_key);
-    return @temp_result;
+    } elsif ($info_key eq "FULLQuery") {
+      return qq~
+ 		  SELECT *
+      FROM $table_name
+      WHERE record_status!='D'
+      ~;
+    } elsif ($info_key eq "FULLQuery") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "BASICQuery") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "CATEGORY") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "data_columns") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "data_scales") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "data_types") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "DB_TABLE_NAME") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "fk_tables") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "FULLQuery") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "FULLQuery") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "FULLQuery") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "input_types") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "key_columns") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "ManageTableAllowed") {
+      $log->debug( $info_key );
+      return( $tinfo->{manage_table_allowed} );
+    } elsif ($info_key eq "MENU_OPTIONS") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "MULTI_INSERT_COLUMN") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "ordered_columns") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "PK_COLUMN_NAME") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "PROGRAM_FILE_NAME") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "QueryTypes") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "required_columns") {
+      $log->debug( $info_key );
+    } elsif ($info_key eq "url_cols") {
+      $log->debug( $info_key );
 
+    } 
+
+    # fall through to parent sbeams tableInfo handler 
+    return $sbeams->returnTableInfo($table_name, $info_key);
+  }
 }
 
+#+
+# Routine to fetch and return table_property information for a particular table
+# arguments:    table_name (required)
+# returns:      ref to hash of col_name => value for specified table
+#-
+sub getTableProperties {
+  my $self = shift;
+  my $table_name = shift || die "Missing required table_name parameter";
+  
+  # See if we have this cached
+  if ( $self->{_tinfo}->{$table_name} ) {
+    $log->debug( "Using cached table info" );
+  } else {
 
+    my $tabSQL =<<"    END_TSQL";
+    SELECT category, table_group, manage_table_allowed, db_table_name,
+    PK_column_name, multi_insert_column, table_url, manage_tables, next_step
+    FROM $TB_TABLE_PROPERTY
+    WHERE table_name = '$table_name'
+    END_TSQL
 
+    my @prop_array = $self->getSBEAMS()->selectHashArray( $tabSQL );
+    $self->{_tinfo}->{$table_name} = $prop_array[0];
+  }
+  return $self->{_tinfo}->{$table_name} 
+
+} # End getTableProperties
+
+#+
+# Routine to fetch and return column_property information for the columns in
+# a particular table
+# arguments:    table_name (required)
+# returns:      ref to array of col_name => value hashrefs for specified table
+#-
+sub getColumnProperties {
+  my $self = shift;
+  my $table_name = shift || die "Missing required table_name parameter";
+
+  # See if we have this cached
+  if ( $self->{_cinfo}->{$table_name} ) {
+    $log->debug( "Using cached column info" );
+    
+  } else {
+
+    my $colSQL =<<"    END_CSQL";
+    SELECT column_index, column_name, column_title, data_type, data_scale,
+    data_precision, nullable, default_value, is_auto_inc, fk_table,
+    fk_column_name, is_required, input_type, input_length, onChange,
+    is_data_column, is_display_column, is_key_field, column_text,
+    optionlist_query, url
+    FROM $TB_TABLE_COLUMN
+    WHERE table_name = '$table_name'
+    END_CSQL
+
+    $self->{_tinfo}->{$table_name} = $self->getSBEAMS()->selectHashArray( $colSQL );
+  }
+  return $self->{_cinfo}->{$table_name} 
+
+} # End getColumnProperties
+  
 
 ###############################################################################
 # getParentProject
