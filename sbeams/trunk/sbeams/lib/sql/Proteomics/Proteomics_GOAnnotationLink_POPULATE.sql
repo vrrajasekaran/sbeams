60fca8a345c068c0b9804bd85caeb81cfa10d47b
edeutsch <edeutsch@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-09-07 12:02:15 +0000

    added files needed to create the links to BioLink GO tables Instructions are still in BioLink doc

---
+++ sbeams/trunk/sbeams/lib/sql/Proteomics/Proteomics_GOAnnotationLink_POPULATE.sql
@@ -0,0 +1,77 @@
+
+DROP INDEX biosequence_annotated_gene.idx_biosequence_id
+DROP INDEX biosequence_annotated_gene.idx_annotated_gene_id
+
+TRUNCATE TABLE biosequence_annotated_gene
+
+INSERT INTO biosequence_annotated_gene
+SELECT BS.biosequence_id,AG.annotated_gene_id
+  FROM biosequence BS
+  JOIN biosequence_set BSS ON ( BS.biosequence_set_id = BSS.biosequence_set_id )
+  JOIN BioLink..annotated_gene AG ON ( BS.biosequence_accession = AG.gene_accession )
+ WHERE BSS.set_tag LIKE 'YeastORF%'
+   AND AG.organism_namespace_id=2
+
+
+INSERT INTO biosequence_annotated_gene
+SELECT BS.biosequence_id,AG.annotated_gene_id
+  FROM biosequence BS
+  JOIN biosequence_set BSS ON ( BS.biosequence_set_id = BSS.biosequence_set_id )
+  JOIN BioLink..annotated_gene AG ON ( BS.biosequence_accession = AG.gene_accession )
+ WHERE BSS.set_tag LIKE 'Dros%'
+   AND AG.organism_namespace_id=1
+
+
+INSERT INTO biosequence_annotated_gene
+SELECT BS.biosequence_id,AG.annotated_gene_id
+  FROM biosequence BS
+  JOIN biosequence_set BSS ON ( BS.biosequence_set_id = BSS.biosequence_set_id )
+  JOIN BioLink..annotated_gene AG ON ( BS.biosequence_accession = AG.gene_accession )
+ WHERE BSS.set_tag LIKE 'ATH%'
+   AND AG.organism_namespace_id=6
+
+
+INSERT INTO biosequence_annotated_gene
+SELECT BS.biosequence_id,AG.annotated_gene_id
+  FROM biosequence BS
+  JOIN biosequence_set BSS ON ( BS.biosequence_set_id = BSS.biosequence_set_id )
+  JOIN BioLink..annotated_gene AG ON ( BS.biosequence_accession = AG.gene_accession )
+ WHERE BSS.set_tag LIKE 'HuNCI%'
+    OR BSS.set_tag LIKE 'HsNCI%'
+   AND AG.organism_namespace_id=3
+
+
+INSERT INTO biosequence_annotated_gene
+SELECT DISTINCT BS.biosequence_id,AG.annotated_gene_id
+  FROM biosequence BS
+  JOIN biosequence_set BSS ON ( BS.biosequence_set_id = BSS.biosequence_set_id )
+  JOIN BioLink..goa_association GOAA ON ( BS.biosequence_gene_name = GOAA.ipi_accession AND GOAA.ref_db_tag = 'SPTR' )
+  JOIN BioLink..annotated_gene AG ON ( GOAA.ref_db_symbol = AG.gene_accession )
+ WHERE BSS.set_tag IN ( 'HuIPI','HepC','OldHuIPI','HIVHepCHuIPI200308' )
+    OR BSS.set_tag LIKE 'HsIPI_v%'
+   AND AG.organism_namespace_id=4
+
+
+CREATE NONCLUSTERED INDEX idx_biosequence_id ON dbo.biosequence_annotated_gene ( biosequence_id )
+CREATE NONCLUSTERED INDEX idx_annotated_gene_id ON dbo.biosequence_annotated_gene ( annotated_gene_id )
+
+-------
+
+For PeptideAtlas we use:
+
+INSERT INTO biosequence_annotated_gene
+SELECT BS.biosequence_id,AG.annotated_gene_id
+  FROM biosequence BS
+  JOIN biosequence_set BSS ON ( BS.biosequence_set_id = BSS.biosequence_set_id )
+  JOIN BioLink..annotated_gene AG ON ( BS.biosequence_gene_name = AG.gene_accession )
+ WHERE BSS.set_tag LIKE 'SGD%'
+   AND AG.organism_namespace_id=2
+
+SELECT TOP 100 * FROM biosequence_set
+
+SELECT TOP 100 * FROM biosequence WHERE biosequence_set_id=10
+
+SELECT * FRom BioLink..organism_namespace
+
+SELECT TOP 100 * FROM BioLink..annotated_gene WHERE organism_namespace_id=2
+
