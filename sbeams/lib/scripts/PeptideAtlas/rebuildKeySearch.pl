510177b8a8e5f990b0f42a576de309862ee339a4
edeutsch <gasif555@gmail.com>
2005-10-11 07:47:39 +0000

    script to rebuild the PeptideAtlas search key list

---
+++ sbeams/lib/scripts/PeptideAtlas/rebuildKeySearch.pl
@@ -0,0 +1,145 @@
+#!/usr/local/bin/perl -w
+
+###############################################################################
+# Program     : rebuildKeySearch.pl
+# Author      : Eric Deutsch <edeutsch@systemsbiology.org>
+# $Id$
+#
+# Description : This script rebuilds the PeptideAtlas key search mechanism
+#
+###############################################################################
+
+
+###############################################################################
+# Generic SBEAMS setup for all the needed modules and objects
+###############################################################################
+use strict;
+use Getopt::Long;
+use FindBin;
+
+use lib "$FindBin::Bin/../../perl";
+use vars qw ($sbeams $sbeamsMOD $q $current_username 
+             $PROG_NAME $USAGE %OPTIONS $QUIET $VERBOSE $DEBUG $TESTONLY
+            );
+
+
+#### Set up SBEAMS core module
+use SBEAMS::Connection qw($q);
+use SBEAMS::Connection::Settings;
+use SBEAMS::Connection::Tables;
+
+use SBEAMS::PeptideAtlas;
+use SBEAMS::PeptideAtlas::Settings;
+use SBEAMS::PeptideAtlas::Tables;
+
+use SBEAMS::PeptideAtlas::KeySearch;
+
+$sbeams = new SBEAMS::Connection;
+$sbeamsMOD = new SBEAMS::PeptideAtlas;
+$sbeamsMOD->setSBEAMS($sbeams);
+$sbeams->setSBEAMS_SUBDIR($SBEAMS_SUBDIR);
+
+
+###############################################################################
+# Set program name and usage banner for command like use
+###############################################################################
+$PROG_NAME = $FindBin::Script;
+$USAGE = <<EOU;
+Usage: $PROG_NAME [OPTIONS]
+Options:
+  --verbose n            Set verbosity level.  default is 0
+  --quiet                Set flag to print nothing at all except errors
+  --debug n              Set debug flag
+  --testonly             If set, rows in the database are not changed or added
+  --GOA_directory        Directory where the latest GOA files are
+
+ e.g.: $PROG_NAME 
+EOU
+
+#### Process options
+unless (GetOptions(\%OPTIONS,"verbose:s","quiet","debug:s","testonly",
+                   "GOA_directory:s",
+    )) {
+
+    die "\n$USAGE";
+
+}
+
+$VERBOSE = $OPTIONS{"verbose"} || 0;
+$QUIET = $OPTIONS{"quiet"} || 0;
+$DEBUG = $OPTIONS{"debug"} || 0;
+$TESTONLY = $OPTIONS{"testonly"} || 0;
+
+if ($DEBUG) {
+    print "Options settings:\n";
+    print "  VERBOSE = $VERBOSE\n";
+    print "  QUIET = $QUIET\n";
+
+}
+
+
+
+###############################################################################
+# Set Global Variables and execute main()
+###############################################################################
+main();
+exit(0);
+
+
+###############################################################################
+# Main Program:
+#
+# Call $sbeams->Authenticate() and exit if it fails or continue if it works.
+###############################################################################
+sub main {
+
+  #### Do the SBEAMS authentication and exit if a username is not returned
+  exit unless (
+    $current_username = $sbeams->Authenticate(
+      work_group=>'PeptideAtlas_admin')
+  );
+
+  $sbeams->printPageHeader() unless ($QUIET);
+  handleRequest();
+  $sbeams->printPageFooter() unless ($QUIET);
+
+} # end main
+
+
+
+###############################################################################
+# handleRequest
+###############################################################################
+sub handleRequest {
+
+  my %args = @_;
+
+  ##### PROCESS COMMAND LINE OPTIONS AND RESTRICTIONS #####
+
+  #### Set the command-line options
+  my $GOA_directory = $OPTIONS{"GOA_directory"};
+
+
+  #### If there are any unresolved parameters, exit
+  if ($ARGV[0]){
+    print "ERROR: Unresolved command line parameter '$ARGV[0]'.\n";
+    print "$USAGE";
+    exit;
+  }
+
+  my $keySearch = new SBEAMS::PeptideAtlas::KeySearch;
+
+  $keySearch->setSBEAMS($sbeams);
+
+  $keySearch->rebuildKeyIndex(
+    GOA_directory => $GOA_directory,
+    verbose => $VERBOSE,
+    testonly => $TESTONLY,
+  );
+  
+
+} # end handleRequest
+
+
+
+
