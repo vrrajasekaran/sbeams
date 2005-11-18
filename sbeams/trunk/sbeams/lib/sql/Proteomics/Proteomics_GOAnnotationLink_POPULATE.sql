5b72a00459850000bcbfd50d05d1d658b644959c
edeutsch <edeutsch@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2005-11-18 00:41:24 +0000

    Added support for Mouse linking and tidied

--- sbeams/trunk/sbeams/lib/sql/Proteomics/Proteomics_GOAnnotationLink_POPULATE.sql
+++ sbeams/trunk/sbeams/lib/sql/Proteomics/Proteomics_GOAnnotationLink_POPULATE.sql
@@ -4,6 +4,8 @@ DROP INDEX biosequence_annotated_gene.idx_annotated_gene_id
 
 TRUNCATE TABLE biosequence_annotated_gene
 
+
+-- Yeast
 INSERT INTO biosequence_annotated_gene
 SELECT BS.biosequence_id,AG.annotated_gene_id
   FROM biosequence BS
@@ -13,6 +15,7 @@ SELECT BS.biosequence_id,AG.annotated_gene_id
    AND AG.organism_namespace_id=2
 
 
+-- Drosophila
 INSERT INTO biosequence_annotated_gene
 SELECT BS.biosequence_id,AG.annotated_gene_id
   FROM biosequence BS
@@ -22,6 +25,7 @@ SELECT BS.biosequence_id,AG.annotated_gene_id
    AND AG.organism_namespace_id=1
 
 
+-- Arabidopsis
 INSERT INTO biosequence_annotated_gene
 SELECT BS.biosequence_id,AG.annotated_gene_id
   FROM biosequence BS
@@ -31,6 +35,7 @@ SELECT BS.biosequence_id,AG.annotated_gene_id
    AND AG.organism_namespace_id=6
 
 
+-- Human NCI biosequence_sets
 INSERT INTO biosequence_annotated_gene
 SELECT BS.biosequence_id,AG.annotated_gene_id
   FROM biosequence BS
@@ -41,15 +46,27 @@ SELECT BS.biosequence_id,AG.annotated_gene_id
    AND AG.organism_namespace_id=3
 
 
+-- Human IPI biosequence_sets
 INSERT INTO biosequence_annotated_gene
 SELECT DISTINCT BS.biosequence_id,AG.annotated_gene_id
   FROM biosequence BS
   JOIN biosequence_set BSS ON ( BS.biosequence_set_id = BSS.biosequence_set_id )
-  JOIN BioLink..goa_association GOAA ON ( BS.biosequence_gene_name = GOAA.ipi_accession AND GOAA.ref_db_tag = 'SPTR' )
+  JOIN BioLink..goa_association GOAA ON ( BS.biosequence_gene_name = GOAA.ipi_accession AND GOAA.ref_db_tag = 'UniProt' )
   JOIN BioLink..annotated_gene AG ON ( GOAA.ref_db_symbol = AG.gene_accession )
  WHERE BSS.set_tag IN ( 'HuIPI','HepC','OldHuIPI','HIVHepCHuIPI200308' )
     OR BSS.set_tag LIKE 'HsIPI_v%'
    AND AG.organism_namespace_id=4
+
+
+INSERT INTO biosequence_annotated_gene
+SELECT DISTINCT BS.biosequence_id,AG.annotated_gene_id
+  FROM biosequence BS
+  JOIN biosequence_set BSS ON ( BS.biosequence_set_id = BSS.biosequence_set_id )
+  JOIN BioLink..goa_association GOAA ON ( BS.biosequence_gene_name = GOAA.ipi_accession AND GOAA.ref_db_tag = 'UniProt' )
+  JOIN BioLink..annotated_gene AG ON ( GOAA.ref_db_symbol = AG.gene_accession )
+ WHERE BSS.set_tag LIKE 'MmIPI%'
+   AND AG.organism_namespace_id=5
+
 
 
 CREATE NONCLUSTERED INDEX idx_biosequence_id ON dbo.biosequence_annotated_gene ( biosequence_id )
