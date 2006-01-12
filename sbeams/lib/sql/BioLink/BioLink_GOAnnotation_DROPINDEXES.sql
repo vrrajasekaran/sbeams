c076547689547aad7195cfd091617790389e389a
edeutsch <gasif555@gmail.com>
2006-01-12 23:30:00 +0000

    added another index which will speed up some queries not constraining by annotation

--- sbeams/lib/sql/BioLink/BioLink_GOAnnotation_DROPINDEXES.sql
+++ sbeams/lib/sql/BioLink/BioLink_GOAnnotation_DROPINDEXES.sql
@@ -1,7 +1,21 @@
 
 DROP INDEX annotated_gene.idx_annotated_gene_gene_accession
+GO
+
 DROP INDEX gene_annotation.idx_gene_annotation_annotated_gene_id
+GO
+
+DROP INDEX gene_annotation.idx_gene_annotation_typeidxlevel
+GO
+
 DROP INDEX gene_annotation.idx_gene_annotation_annotation
+GO
+
+
+
 DROP INDEX biosequence_annotated_gene.idx_biosequence_id
+GO
+
 DROP INDEX biosequence_annotated_gene.idx_annotated_gene_id
+GO
 
