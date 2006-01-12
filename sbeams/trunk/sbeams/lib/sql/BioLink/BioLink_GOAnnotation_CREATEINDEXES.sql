6422fd6376f6cfed121c0b239191762dfcf3b965
edeutsch <edeutsch@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2006-01-12 23:30:00 +0000

    added another index which will speed up some queries not constraining by annotation

--- sbeams/trunk/sbeams/lib/sql/BioLink/BioLink_GOAnnotation_CREATEINDEXES.sql
+++ sbeams/trunk/sbeams/lib/sql/BioLink/BioLink_GOAnnotation_CREATEINDEXES.sql
@@ -1,7 +1,16 @@
 
 CREATE NONCLUSTERED INDEX idx_annotated_gene_gene_accession ON dbo.annotated_gene ( gene_accession )
+GO
+
 CREATE NONCLUSTERED INDEX idx_gene_annotation_annotated_gene_id ON dbo.gene_annotation ( annotated_gene_id )
-CREATE NONCLUSTERED INDEX idx_gene_annotation_annotation ON dbo.gene_annotation ( gene_annotation_type_id,idx,annotation )
+GO
+
+CREATE NONCLUSTERED INDEX idx_gene_annotation_typeidxlevel ON dbo.gene_annotation ( gene_annotation_type_id,idx,hierarchy_level )
+GO
+
+CREATE NONCLUSTERED INDEX idx_gene_annotation_annotation ON dbo.gene_annotation ( gene_annotation_type_id,idx,hierarchy_level,annotation )
+GO
+
 
 
 
