1e55dcef78c32137e31a6cac470d35a7b31b0456
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2006-01-25 21:58:17 +0000

    Converted G0 to ';', removed dbo. prefix.

--- sbeams/trunk/sbeams/lib/sql/Microarray/Microarray_CREATEINDEXES.sql
+++ sbeams/trunk/sbeams/lib/sql/Microarray/Microarray_CREATEINDEXES.sql
@@ -1,92 +1,87 @@
 -- affy_db_links index on affy_annotation id and db_id
-CREATE NONCLUSTERED INDEX idx_affy_annotation_id__db_id
-   ON dbo.affy_db_links ( affy_annotation_id, db_id )
-go
+CREATE INDEX idx_affy_annotation_id__db_id
+   ON affy_db_links ( affy_annotation_id, db_id );
+
 -- affy_db_links affy_db_links_id --
-CREATE NONCLUSTERED INDEX idx_affy_db_links_id
-   ON dbo.affy_db_links ( affy_db_links_id )
-go
+CREATE INDEX idx_affy_db_links_id
+   ON affy_db_links ( affy_db_links_id );
+
 -- gene_ontology
-CREATE NONCLUSTERED INDEX idx_affy_annotation_id
-   ON dbo.gene_ontology ( affy_annotation_id )
-go	
+CREATE INDEX idx_affy_annotation_id
+   ON gene_ontology ( affy_annotation_id )
+	
 -- gene_ontology affy_db_links_id
-CREATE NONCLUSTERED INDEX idx_affy_db_links_id
-   ON dbo.gene_ontology ( affy_db_links_id )
-go
+CREATE INDEX idx_affy_db_links_id
+   ON gene_ontology ( affy_db_links_id );
+
 -- protein_families
-CREATE NONCLUSTERED INDEX idx_affy_annotation_id
-   ON dbo.protein_families ( affy_annotation_id )
-go	
+CREATE INDEX idx_affy_annotation_id
+   ON protein_families ( affy_annotation_id );
+	
 -- protein_families affy_db_links_id
-CREATE NONCLUSTERED INDEX idx_affy_db_links_id
-   ON dbo.protein_families ( affy_db_links_id )
-go
+CREATE INDEX idx_affy_db_links_id
+   ON protein_families ( affy_db_links_id );
+
 -- interpro
-CREATE NONCLUSTERED INDEX idx_affy_annotation_id
-   ON dbo.interpro ( affy_annotation_id )
-go	
+CREATE INDEX idx_affy_annotation_id
+   ON interpro ( affy_annotation_id );
+	
 -- interpro affy_db_links_id
-CREATE NONCLUSTERED INDEX idx_affy_db_links_id
-   ON dbo.interpro ( affy_db_links_id )
-go
+CREATE INDEX idx_affy_db_links_id
+   ON interpro ( affy_db_links_id );
+
 -- protein_domain
-CREATE NONCLUSTERED INDEX idx_affy_annotation_id
-   ON dbo.protein_domain ( affy_annotation_id )
-go	
+CREATE INDEX idx_affy_annotation_id
+   ON protein_domain ( affy_annotation_id );
+	
 -- protein_domain affy_db_links_id
-CREATE NONCLUSTERED INDEX idx_affy_db_links_id
-   ON dbo.protein_domain ( affy_db_links_id )
-go
+CREATE INDEX idx_affy_db_links_id
+   ON protein_domain ( affy_db_links_id );
+
 -- trans_membrane
-CREATE NONCLUSTERED INDEX idx_affy_annotation_id
-   ON dbo.trans_membrane ( affy_annotation_id )
-go
+CREATE INDEX idx_affy_annotation_id
+   ON trans_membrane ( affy_annotation_id );
+
 --trans_membrane and number of Domains
-CREATE NONCLUSTERED INDEX idx_numberofdomains__affy_annotation_id
-   ON dbo.trans_membrane ( number_of_domains, affy_annotation_id   )
-go
+CREATE INDEX idx_numberofdomains__affy_annotation_id
+   ON trans_membrane ( number_of_domains, affy_annotation_id   );
+
 -- alignment
-CREATE NONCLUSTERED INDEX idx_affy_annotation_id
-   ON dbo.alignment ( affy_annotation_id )
-go
+CREATE INDEX idx_affy_annotation_id
+   ON alignment ( affy_annotation_id );
+
 -- overlapping_transcript
-CREATE NONCLUSTERED INDEX idx_affy_annotation_id
-   ON dbo.overlapping_transcript ( affy_annotation_id )   
-go
+CREATE INDEX idx_affy_annotation_id
+   ON overlapping_transcript ( affy_annotation_id );
+
 -- affy_annotation
-CREATE NONCLUSTERED INDEX idx_affy_annotation_id
-   ON dbo.affy_annotation ( affy_annotation_id )   
-go 
--- affy_annotation probe_set_id annotation_set_id
-CREATE NONCLUSTERED INDEX idx_affy_annotation_probe_set_id__annotation_set_id
-   ON dbo.affy_annotation ( probe_set_id, affy_annotation_set_id)   
-go
--- affy_gene_intensity 
-CREATE NONCLUSTERED INDEX idx_probe_set_id
-   ON dbo.affy_gene_intensity ( probe_set_id )    
-go
--- affy_gene_intensity_ affy_array_id
-CREATE NONCLUSTERED INDEX idx_affy_array_id
-   ON dbo.affy_gene_intensity ( affy_array_id )     
-go
--- affy_gene_intensity_   affy_array_id,probe_set_id, protocol_id
-CREATE NONCLUSTERED INDEX idx_afa_id_probe_set_id_protocol_id
-   ON dbo.affy_gene_intensity (  affy_array_id,probe_set_id, protocol_id )    
-
-go
--- condition  condition_id
-CREATE NONCLUSTERED INDEX idx_condition_id
-   ON dbo.gene_expression ( condition_id )    
-
-go
--- condition  gene_name
-CREATE NONCLUSTERED INDEX idx_gene_name
-   ON dbo.gene_expression ( gene_name )    
+CREATE INDEX idx_affy_annotation_id
+   ON affy_annotation ( affy_annotation_id );   
  
-go
--- condition  biosequence_id
-CREATE NONCLUSTERED INDEX idx_biosequence_id
-   ON dbo.gene_expression ( biosequence_id )    
+-- affy_annotation probe_set_id annotation_set_id
+CREATE INDEX idx_affy_annotation_probe_set_id__annotation_set_id
+   ON affy_annotation ( probe_set_id, affy_annotation_set_id);   
 
-go
+-- affy_gene_intensity 
+CREATE INDEX idx_probe_set_id
+   ON affy_gene_intensity ( probe_set_id );
+
+-- affy_gene_intensity_ affy_array_id
+CREATE INDEX idx_affy_array_id
+   ON affy_gene_intensity ( affy_array_id );     
+
+-- affy_gene_intensity_   affy_array_id,probe_set_id, protocol_id
+CREATE INDEX idx_afa_id_probe_set_id_protocol_id
+   ON affy_gene_intensity (  affy_array_id,probe_set_id, protocol_id );   
+
+-- condition  condition_id
+CREATE INDEX idx_condition_id
+   ON gene_expression ( condition_id );
+
+-- condition  gene_name
+CREATE INDEX idx_gene_name
+   ON gene_expression ( gene_name );    
+ 
+-- condition  biosequence_id
+CREATE INDEX idx_biosequence_id
+   ON gene_expression ( biosequence_id );    
