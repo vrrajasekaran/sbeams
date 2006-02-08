5eae9224fd68f31da7bfd207b616b46947f86d86
edeutsch <edeutsch@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2006-02-08 07:39:04 +0000

    fixed good index and commented out an index that is probably not useful

--- sbeams/trunk/sbeams/lib/sql/BioLink/BioLink_GOAnnotation_CREATEINDEXES.sql
+++ sbeams/trunk/sbeams/lib/sql/BioLink/BioLink_GOAnnotation_CREATEINDEXES.sql
@@ -5,12 +5,14 @@ GO
 CREATE NONCLUSTERED INDEX idx_gene_annotation_annotated_gene_id ON dbo.gene_annotation ( annotated_gene_id )
 GO
 
-CREATE NONCLUSTERED INDEX idx_gene_annotation_typeidxlevel ON dbo.gene_annotation ( gene_annotation_type_id,idx,hierarchy_level )
+CREATE NONCLUSTERED INDEX idx_gene_annotation_typeidxlevel ON dbo.gene_annotation ( annotated_gene_id,gene_annotation_type_id,idx,hierarchy_level )
 GO
 
-CREATE NONCLUSTERED INDEX idx_gene_annotation_annotation ON dbo.gene_annotation ( gene_annotation_type_id,idx,hierarchy_level,annotation )
-GO
+-- CREATE NONCLUSTERED INDEX idx_gene_annotation_annotation ON dbo.gene_annotation ( annotated_gene_id,gene_annotation_type_id,idx,hierarchy_level,annotation )
+-- GO
 
+
+-- select * from sysindexes where name like 'idx%'
 
 
 
