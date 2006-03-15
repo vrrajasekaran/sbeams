ca6cae3ba47da14d9ea4d81cda93ce80daa5a425
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2006-03-15 18:23:07 +0000

    Added pI calculator, updated residue MW, added getCurrentCutoff method.

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/Glycopeptide/Utilities.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/Glycopeptide/Utilities.pm
@@ -1,5 +1,7 @@
 package SBEAMS::Glycopeptide::Utilities;
 
+use SBEAMS::Connection qw( $log );
+
 sub new {
   my $class = shift;
   my $this = {};
@@ -13,7 +15,6 @@ sub new {
 sub match_count {
   my $self = shift;
   my %args = @_;
-#  print STDERR "pep: $args{pepseq}\n";
   return unless $args{pepseq} && $args{protseq};
 
   my @cnt = split( $args{pepseq}, $args{protseq}, -1 );
@@ -74,15 +75,20 @@ sub get_site_positions {
     my $posn = length($`);
     push @posn, $posn;# pos($string); # - length($&) # start position of match
   }
-#  print STDERR "Found $posn[0] for NxS/T in $args{seq}\n";
+#  $log->debug( "Found $posn[0] for NxS/T in $args{seq}\n" );
   return \@posn;
 }
 
 sub get_current_prophet_cutoff {
   my $self = shift;
-  my $sbeams = $self->getSBEAMS();
-  my $cutoff = $sbeams->getSessionAttribute( key => 'glyco_prophet_cutoff' );
-  $cutoff = 0.5 if !defined $cutoff;
+  my $sbeams = $self->getSBEAMS() || new SBEAMS::Connection;
+  my $cutoff = $sbeams->get_cgi_param( 'glyco_prophet_cutoff' );
+  if ( $cutoff ) {
+    $self->set_prophet_cutoff( $cutoff );
+  } else  {
+    $cutoff = $sbeams->getSessionAttribute( key => 'glyco_prophet_cutoff' );
+  }
+  $cutoff ||= 0.5; 
   return $cutoff;
 }
 
@@ -105,7 +111,7 @@ sub clean_pepseq {
   $seq =~ s/d/n/g;
   $seq =~ s/U/n/g;
 
-  # Trim of leading/lagging amino acids
+  # Trim off leading/lagging amino acids
   $seq =~ s/^.\.//g;
   $seq =~ s/\..$//g;
   return $seq;
@@ -134,25 +140,23 @@ sub getResidueMasses {
     Y => 163.1760,   # Tyrosine
     P =>  97.1167,   # Proline
     H => 137.1411,   # Histidine
-    E => 129.1155,   # Glutamic_Acid (Glutamate)
+    E => 129.1155,   # Glutamic Acid (Glutamate)
     Q => 128.1307,   # Glutamine
-    D => 115.0886,   # Aspartic_Acid (Aspartate)
+    D => 115.0886,   # Aspartic Acid (Aspartate)
     N => 114.1038,   # Asparagine
     K => 128.1741,   # Lysine
     R => 156.1875,   # Arginine
 
-    X => 113.1594,   # L or I
+    X => 118.8860,   # Unknown, avg of 20 common AA.
     B => 114.5962,   # avg N and D
     Z => 128.6231,   # avg Q and E
-    U => 100,        # ?????
-
   );
 
   return \%residue_masses;
 
 }
 
-sub getPeptideMass {
+sub calculatePeptideMass {
   my $self = shift;
   my %args = @_;
   die "Missing required parameter sequence" unless $args{sequence};
@@ -162,15 +166,213 @@ sub getPeptideMass {
   my $mass = 0;
   foreach my $r ( @seq ) {
     if ( !defined $self->{_rmass}->{$r} ) {
-#      print STDERR "Undefined residue $r is getPeptideMass\n";
-      $self->{_rmass}->{$r} = $self->{_rmass}->{U} # Assign 'average' mass.
+      $log->error("Undefined residue $r in getPeptideMass");
+      $self->{_rmass}->{$r} = $self->{_rmass}->{X} # Assign 'average' mass.
     }
     $mass += $self->{_rmass}->{$r};
   }
-  $mass += 18; # N and C termini have extra H, OH.
-  return $mass;
+  $mass += 18.0153; # N and C termini have extra H, OH.
+  return sprintf( "%0.2f", $mass);
 }
 
+#+
+# Returns hashref with isoelectric points of various single amino acids.
+#-
+sub getResidueIsoelectricPoints {
+  my $self = shift;
+  my %pi = ( A => 6.00,
+             R => 11.15,
+             N => 5.41,
+             D => 2.77,
+             C => 5.02,
+             Q => 5.65,
+             E => 3.22,
+             G => 5.97,
+             H => 7.47,
+             I => 5.94,
+             L => 5.98,
+             K => 9.59,
+             M => 5.74,
+             F => 5.48,
+             P => 6.30,
+             S => 5.68,
+             T => 5.64,
+             W => 5.89,
+             Y => 5.66,
+             V => 5.96,
+             
+             X => 6.03,   # Unknown, avg of 20 common AA.
+             B => 4.09,   # avg N and D
+             Z => 4.44   # avg Q and E 
+           );
+  return \%pi;
+}
+
+
+#+ 
+# Simple minded pI calculator, simply takes an average.
+#-
+sub calculatePeptidePI_old {
+  my $self = shift;
+  my %args = @_;
+  die "Missing required parameter sequence" unless $args{sequence};
+  $self->{_rpka} ||= $self->getResiduePKAs();
+  my $seq = uc( $args{sequence} );
+  my @seq = split( "", $seq );
+#  my $pi = 2.2 + 9.5; # Average C and N terminal pKA
+  my $pi = 3.1 + 8.0; # Average C and N terminal pKA
+  my $cnt = 2;        # We have termini, if nothing else
+  foreach my $r ( @seq ) {
+    next if !defined $self->{_rpka}->{$r}; 
+#    print "Calculating with $self->{_rpka}->{$r}\n";
+    $pi += $self->{_rpka}->{$r};
+    $cnt++;
+  }
+#  print "total pi is $pi, total cnt is $cnt\n";
+  return sprintf( "%0.1f", $pi/$cnt );
+}
+
+
+#+
+# pI calculator algorithm taken from proteomics toolkit 'Isotope Servlet'
+#-
+sub calculatePeptidePI {
+  my $self = shift;
+  my %args = @_;
+  die "Missing required parameter sequence" unless $args{sequence};
+
+  # Get pKa values
+  $self->{_rpkav} ||= $self->getResiduePKAvalues();
+  my %pka = %{$self->{_rpkav}};
+
+  # split sequence into an array
+  my $seq = uc( $args{sequence} );
+  my @seq = split "", $seq;
+  my %cnt;
+
+  my $side_total = 0;
+  for my $aa ( keys(%pka) ) {
+    # Only consider amino acids that can carry a charge
+    next unless $pka{$aa}->[2];
+
+    # Count the occurences of each salient amino acid (C, D, E, H, K, R, Y)
+    $cnt{$aa} = eval "$seq =~ tr/$aa/$aa/";
+    $side_total += $cnt{$aa};
+  }
+
+  # pKa at C/N termini vary by amino acid
+  my $nterm_pka = $pka{$seq[0]}->[0];
+  my $cterm_pka = $pka{$seq[$#seq]}->[1];
+
+  # Range of pH values
+  my $ph_min = 0;
+  my $ph_max = 14;
+  my $ph_mid;
+
+  # Don't freak out if we can't converge
+  my $max_iterations = 200;
+
+  # This is all approximate anyway
+  my $precision = 0.01;
+
+  # Loop de loop
+  for( my $i = 0; $i <= $max_iterations; $i++ ) {
+    $ph_mid =  $ph_min + ($ph_max - $ph_min)/2; 
+
+    # Positive contributors
+    my $cNter = 10**-$ph_mid / ( 10**-$nterm_pka + 10**-$ph_mid );
+    my $carg  = $cnt{R} * 10**-$ph_mid / ( 10**-$pka{R}->[2] + 10**-$ph_mid );
+    my $chis  = $cnt{H} * 10**-$ph_mid / ( 10**-$pka{H}->[2] + 10**-$ph_mid );
+    my $clys  = $cnt{K} * 10**-$ph_mid / ( 10**-$pka{K}->[2] + 10**-$ph_mid );
+
+    # Negative contributors
+    my $cCter = 10**-$cterm_pka / ( 10**-$cterm_pka + 10**-$ph_mid );
+    my $casp  = $cnt{D} * 10**-$pka{D}->[2] / ( 10**-$pka{D}->[2] + 10**-$ph_mid );
+    my $cglu  = $cnt{E} * 10**-$pka{E}->[2] / ( 10**-$pka{E}->[2] + 10**-$ph_mid );
+    my $ccys  = $cnt{C} * 10**-$pka{C}->[2] / ( 10**-$pka{C}->[2] + 10**-$ph_mid );
+    my $ctyr  = $cnt{Y} * 10**-$pka{Y}->[2] / ( 10**-$pka{Y}->[2] + 10**-$ph_mid );
+    
+    # Charge, trying to minimize absolute value
+    my $charge = $carg + $clys + $chis + $cNter - ($casp + $cglu + $ctyr + $ccys + $cCter);
+    
+    if ( $charge > 0.0) {
+      $ph_min = $ph_mid; 
+    } else {
+      $ph_max = $ph_mid;
+    }
+    last if abs($ph_max - $ph_min) < $precision;
+  }
+
+  # Let lack of return precision reflect the fact that this is an estimate 
+  return sprintf( "%0.1f", ($ph_max + $ph_min)/ 2 );
+}
+
+#+
+# Returns ref to hash of one-letter amino acid => arrayref of N, 
+# C and side-chain pKa values
+#-
+sub getResiduePKAvalues {
+  my $self = shift;
+                   # -NH3, -COOH -R grp
+  my %pka = ( A => [ 3.55, 7.59, 0.0 ],
+
+              D => [ 4.55, 7.50, 4.05 ], # IS => ionizable sidechain
+              N => [ 3.55, 7.50, 0.0 ],
+              B => [ 4.35, 7.50, 2.0 ], # Asx
+
+              C => [ 3.55, 7.50, 9.00  ], # IS
+
+              E => [ 4.75, 7.70, 4.45 ], # IS
+              Q => [ 3.55, 7.00, 0.0 ],
+              Z => [ 4.15, 7.25, 2.2 ], # Glx
+
+              F => [ 3.55, 7.50, 0.0 ],
+              G => [ 3.55, 7.50, 0.0 ],
+              H => [ 3.55, 7.50, 5.98  ], # IS
+              I => [ 3.55, 7.50, 0.0 ],
+              K => [ 3.55, 7.50, 10.0 ], # IS
+              L => [ 3.55, 7.50, 0.0 ],
+              M => [ 3.55, 7.00, 0.0 ],
+              P => [ 3.55, 8.36, 0.0 ],
+              R => [ 3.55, 7.50, 12.0  ], # IS
+              S => [ 3.55, 6.93, 0.0  ],
+              T => [ 3.55, 6.82, 0.0  ],
+              V => [ 3.55, 7.44, 0.0 ],
+              W => [ 3.55, 7.50, 0.0 ],
+              Y => [ 3.55, 7.50, 10.0 ], # IS
+              
+              X => [ 3.55, 7.50, 2.3 ], # Unknown aa
+              );
+
+  return \%pka;
+}
+
+
+#+
+# Returns hash of amino acid to pKa value; various tables exist.
+#-
+sub getResiduePKAs {
+  my $self = shift;
+  my $old = shift;
+  my %pka1 = ( C => 8.4, 
+               D => 3.9,
+               E => 4.1,
+               H => 6.0,
+               K => 10.5,
+               R => 12.5,
+               Y => 10.5 );
+  return \%pka1 if $old;
+
+  my %pka = ( C => 9.0, 
+              D => 4.05,
+              E => 4.45,
+              H => 5.98,
+              K => 10.0,
+              R => 12.0,
+              Y => 10.0 );
+
+  return \%pka;
+}
 
 1;
 
