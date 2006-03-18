e924826885de5642544740e5d3bf48f776c24f77
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2006-03-18 01:33:09 +0000

    Added getRomanNumeral, used to convert our Yeast chromosome numbers to those that the Entrez chromosome browser can deal with.

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/Connection/Utilities.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/Connection/Utilities.pm
@@ -793,6 +793,36 @@ sub truncateString {
   return $string;
 }
 
+sub getRomanNumeral {
+  my $self = shift;
+  my %args = @_;
+  return undef unless $args{number};
+  unless ( $self->{_rnumerals} ) {
+    my %num = ( 1  => 'I',
+                2  => 'II',
+                3  => 'III',
+                4  => 'IV',
+                5  => 'V',
+                6  => 'VI',
+                7  => 'VII',
+                8  => 'VIII',
+                9  => 'IX',
+                10 => 'X',
+                11 => 'XI',
+                12 => 'XII',
+                13 => 'XIII',
+                14 => 'XIV',
+                15 => 'XV',
+                16 => 'XVI',
+                17 => 'XVII',
+                18 => 'XVIII',
+                19 => 'XIX',
+                20 => 'XX' );
+    $self->{_rnumerals} = \%num;
+  }
+  return $self->{_rnumerals}->{$args{number}};
+}
+
 1;
 
 __END__
