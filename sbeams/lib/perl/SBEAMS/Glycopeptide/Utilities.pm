8033c60e2487a657ee290cefb0a84cfa8d2ddfc9
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2006-08-31 23:25:14 +0000

    Added method to parse TMHMM info into internal/external/transmembrane ranges

--- sbeams/lib/perl/SBEAMS/Glycopeptide/Utilities.pm
+++ sbeams/lib/perl/SBEAMS/Glycopeptide/Utilities.pm
@@ -181,6 +181,38 @@ sub get_current_prophet_cutoff {
   return $cutoff;
 }
 
+sub get_transmembrane_info {
+  my $self = shift;
+  my %args = @_;
+  return unless $args{tm_info};
+  my $string = $args{tm_info};
+  my $plen = $args{end} || '_END_';
+
+  my @tminfo;
+  my $start = 0;
+  my $side = '';
+  my ($posn, $beg, $end );
+  while ( $string =~ m/[^oi]*[oi]/g ) {
+    next unless $&;
+    my $range = $&;
+    my ($beg, $end);
+    if ( !$side ) {
+      $side = ( $range eq 'i' ) ? 'intracellular' : 'extracellular';
+      $posn = 1;
+    } else {
+      $range =~ m/(\d+)\-(\d+)([io])/g;
+      $beg = $1;
+      $end = $2;
+      push @tminfo, [ $side, $posn, ($beg - 1) ];
+      push @tminfo, ['tm', $beg, $end ];
+      $posn = $end + 1;
+      $side = ( $3 eq 'i' ) ? 'intracellular' : 'extracellular';
+    }
+  }
+  push @tminfo, [ $side, $posn, $plen ];
+  return \@tminfo;
+}
+
 sub getCurrentBuild {
   my $self = shift;
   my $sql = "Select ipi_version_name FROM $TBGP_IPI_VERSION";
