82e8eb35858f53dd13b228d99e1a61f5c22c5af5
edeutsch <edeutsch@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2007-03-28 00:01:49 +0000

    new code for selecting optimum peptides fpr targeting

---
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/PeptideAtlas/BestPeptideSelector.pm
@@ -0,0 +1,304 @@
+package SBEAMS::PeptideAtlas::BestPeptideSelector;
+
+###############################################################################
+# Class       : SBEAMS::PeptideAtlas::BestPeptideSelector
+# Author      : Eric Deutsch <edeutsch@systemsbiology.org>
+#
+=head1 SBEAMS::PeptideAtlas::BestPeptideSelector
+
+=head2 SYNOPSIS
+
+  SBEAMS::PeptideAtlas::BestPeptideSelector
+
+=head2 DESCRIPTION
+
+This is part of the SBEAMS::PeptideAtlas module which handles
+things related to finding the best peptides to use for some other application
+
+=cut
+#
+###############################################################################
+
+use strict;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
+require Exporter;
+@ISA = qw();
+$VERSION = q[$Id$];
+@EXPORT_OK = qw();
+
+use SBEAMS::Connection;
+use SBEAMS::Connection::Tables;
+use SBEAMS::Connection::Settings;
+use SBEAMS::PeptideAtlas::Tables;
+
+
+###############################################################################
+# Global variables
+###############################################################################
+use vars qw($VERBOSE $TESTONLY $sbeams);
+
+
+###############################################################################
+# Constructor
+###############################################################################
+sub new {
+    my $this = shift;
+    my $class = ref($this) || $this;
+    my $self = {};
+    bless $self, $class;
+    $VERBOSE = 0;
+    $TESTONLY = 0;
+    return($self);
+} # end new
+
+
+###############################################################################
+# setSBEAMS: Receive the main SBEAMS object
+###############################################################################
+sub setSBEAMS {
+    my $self = shift;
+    $sbeams = shift;
+    return($sbeams);
+} # end setSBEAMS
+
+
+
+###############################################################################
+# getSBEAMS: Provide the main SBEAMS object
+###############################################################################
+sub getSBEAMS {
+    my $self = shift;
+    return $sbeams || SBEAMS::Connection->new();
+} # end getSBEAMS
+
+
+
+###############################################################################
+# setTESTONLY: Set the current test mode
+###############################################################################
+sub setTESTONLY {
+    my $self = shift;
+    $TESTONLY = shift;
+    return($TESTONLY);
+} # end setTESTONLY
+
+
+
+###############################################################################
+# setVERBOSE: Set the verbosity level
+###############################################################################
+sub setVERBOSE {
+    my $self = shift;
+    $VERBOSE = shift;
+    return($TESTONLY);
+} # end setVERBOSE
+
+
+
+###############################################################################
+# getBestPeptides -- Selects the best piptides from some application
+###############################################################################
+sub getBestPeptides {
+  my $METHOD = 'getBestPeptides';
+  my $self = shift || die ("self not passed");
+  my %args = @_;
+
+  #### Process parameters
+  my $atlas_build_id = $args{atlas_build_id}
+    or die("ERROR[$METHOD]: Parameter atlas_build_id not passed");
+
+  my $biosequence_id = $args{biosequence_id}
+    or die("ERROR[$METHOD]: Parameter biosequence_id not passed");
+
+  my $resultset_ref = $args{resultset_ref}
+    or die("ERROR[$METHOD]: Parameter resultset_ref not passed");
+
+  my $query_parameters_ref = $args{query_parameters_ref}
+    or die("ERROR[$METHOD]: Parameter query_parameters_ref not passed");
+
+  my $column_titles_ref = $args{column_titles_ref}
+    or die("ERROR[$METHOD]: Parameter column_titles_ref not passed");
+
+
+  my $n_rows = scalar(@{$resultset_ref->{data_ref}});
+  my $cols = $resultset_ref->{column_hash_ref};
+
+
+  #### Loop over all rows, calculating some max stats of certain columns
+  my $max_n_observations = 0;
+  my $max_empirical_observability_score = 0;
+  for (my $i=0; $i<$n_rows; $i++) {
+    my $n_observations = $resultset_ref->{data_ref}->[$i]->[$cols->{n_observations}];
+    if ($n_observations > $max_n_observations) {
+      $max_n_observations = $n_observations;
+    }
+
+    my $empirical_observability_score = $resultset_ref->{data_ref}->[$i]->[$cols->{empirical_proteotypic_score}];
+    if ($empirical_observability_score > $max_empirical_observability_score) {
+      $max_empirical_observability_score = $empirical_observability_score;
+    }
+
+  }
+
+  #### Loop over all rows, calculating a suitability score
+  for (my $i=0; $i<$n_rows; $i++) {
+    my $n_observations = $resultset_ref->{data_ref}->[$i]->[$cols->{n_observations}];
+    my $best_probability = $resultset_ref->{data_ref}->[$i]->[$cols->{best_probability}];
+    my $n_protein_mappings = $resultset_ref->{data_ref}->[$i]->[$cols->{n_protein_mappings}];
+    my $n_genome_locations = $resultset_ref->{data_ref}->[$i]->[$cols->{n_genome_locations}];
+    my $empirical_observability_score = $resultset_ref->{data_ref}->[$i]->[$cols->{empirical_proteotypic_score}];
+    my $preceding_residue = $resultset_ref->{data_ref}->[$i]->[$cols->{preceding_residue}];
+    my $peptide_sequence = $resultset_ref->{data_ref}->[$i]->[$cols->{peptide_sequence}];
+
+    $best_probability += 0.03 if ($best_probability == 1.000);
+    my $empirical_observability_fraction = 0;
+    my $divisor = 3;
+
+    if ($max_empirical_observability_score == 0) {
+      $divisor = 2;
+    } else {
+      $empirical_observability_fraction = 
+	$empirical_observability_score / $max_empirical_observability_score;
+    }
+
+    my $suitability_score = (
+      $n_observations / $max_n_observations +
+      ( $best_probability - 0.9 ) / 0.1 +
+      $empirical_observability_fraction
+    ) / $divisor;
+
+    if ($n_protein_mappings > 1) {
+      if ($n_genome_locations > 1) {
+	$suitability_score = 0.0;
+      }
+    }
+
+    if ($preceding_residue !~ /[KR\-]/ || $peptide_sequence !~ /[KR\-]$/) {
+      $suitability_score *= 0.2;
+    }
+
+
+    $resultset_ref->{data_ref}->[$i]->[$cols->{suitability_score}] =
+      $suitability_score;
+  }
+
+
+  $self->sortBySuitabilityScore(
+    resultset_ref=>$resultset_ref,
+  );
+
+  return $resultset_ref;
+
+} # end getBestPeptides
+
+
+###############################################################################
+# getBestPeptidesDisplay -- Selects the best piptides from some application
+###############################################################################
+sub getBestPeptidesDisplay {
+  my $METHOD = 'getBestPeptidesDisplay';
+  my $self = shift || die ("self not passed");
+  my %args = @_;
+
+  #### Process parameters
+  my $atlas_build_id = $args{atlas_build_id}
+    or die("ERROR[$METHOD]: Parameter atlas_build_id not passed");
+
+  my $best_peptide_information = $args{best_peptide_information}
+    or die("ERROR[$METHOD]: Parameter best_peptide_information not passed");
+
+  my $query_parameters_ref = $args{query_parameters_ref}
+    or die("ERROR[$METHOD]: Parameter query_parameters_ref not passed");
+
+  my $column_titles_ref = $args{column_titles_ref}
+    or die("ERROR[$METHOD]: Parameter column_titles_ref not passed");
+
+  my $base_url = $args{base_url}
+    or die("ERROR[$METHOD]: Parameter base_url not passed");
+
+
+  my $resultset_ref = $best_peptide_information;
+
+  #### Define the hypertext links for columns that need them
+  my %url_cols = (
+    	       'Peptide Accession' => "$CGI_BASE_DIR/PeptideAtlas/GetPeptide?_tab=3&atlas_build_id=$atlas_build_id&searchWithinThis=Peptide+Name&searchForThis=%V&action=QUERY",
+  );
+
+  my %hidden_cols;
+
+  $sbeams->displayResultSet(
+      resultset_ref=>$resultset_ref,
+      query_parameters_ref=>$query_parameters_ref,
+      #rs_params_ref=>\%rs_params,
+      url_cols_ref=>\%url_cols,
+      hidden_cols_ref=>\%hidden_cols,
+      #max_widths=>\%max_widths,
+      column_titles_ref=>$column_titles_ref,
+      base_url=>$base_url,
+  );
+
+  return 1;
+
+} # end getBestPeptidesDisplay
+
+
+
+###############################################################################
+# sortBySuitabilityScore
+###############################################################################
+sub sortBySuitabilityScore {
+  my $METHOD = 'sortBySuitabilityScore';
+  my $self = shift || die ("self not passed");
+  my %args = @_;
+
+  #### Process parameters
+  my $resultset_ref = $args{resultset_ref}
+    or die("ERROR[$METHOD]: Parameter resultset_ref not passed");
+
+
+  my $n_rows = scalar(@{$resultset_ref->{data_ref}});
+  my $cols = $resultset_ref->{column_hash_ref};
+
+  my @rows = @{$resultset_ref->{data_ref}};
+  my @newrows = sort bySuitabilityScore @rows;
+
+  $resultset_ref->{data_ref} = \@newrows;
+
+  return $resultset_ref;
+
+} # end sortBySuitabilityScore
+
+
+###############################################################################
+# bySuitabilityScore
+###############################################################################
+sub bySuitabilityScore {
+
+  return $b->[3] <=> $a->[3];
+
+} # end bySuitabilityScore
+
+
+
+
+###############################################################################
+=head1 BUGS
+
+Please send bug reports to SBEAMS-devel@lists.sourceforge.net
+
+=head1 AUTHOR
+
+Eric W. Deutsch (edeutsch@systemsbiology.org)
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
+###############################################################################
+1;
+
+__END__
+###############################################################################
+###############################################################################
+###############################################################################
