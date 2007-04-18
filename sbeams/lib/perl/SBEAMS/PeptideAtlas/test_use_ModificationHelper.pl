fc63ace3d2a9fb58d18995da4d0e49bc2aa5c8ba
nking <nking@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2007-04-18 05:12:08 +0000

    added test for new method

--- sbeams/lib/perl/SBEAMS/PeptideAtlas/test_use_ModificationHelper.pl
+++ sbeams/lib/perl/SBEAMS/PeptideAtlas/test_use_ModificationHelper.pl
@@ -4,18 +4,17 @@ use ModificationHelper;
 
 my $helper = new ModificationHelper();
 
-my @masses = $helper->getMasses("C[330]AT");
 
+my @masses = $helper->getMasses("C[330]AT");
 if ( $#masses != 2) {
     print "failed\n";
 }
 
-my @aa = $helper->getUnmodifiedAAs("C[330]AT");
 
+my @aa = $helper->getUnmodifiedAAs("C[330]AT");
 if ( $#aa != 2) {
     print "failed\n";
 }
-
 if ($aa[0] ne "C") {
     print "failed\n";
 }
@@ -26,12 +25,50 @@ if ($aa[2] ne "T") {
     print "failed\n";
 }
 
+
 my $mass = $helper->getMass("C");
 if ( abs($mass - 103.01) > 0.2) {
      print "failed\n";
 }
 
 
+my @modAAs = $helper->getModifiedAAs("C[330]AT");
+if ( $#modAAs != 2) {
+    print "failed\n";
+}
+if ($modAAs[0] ne "C[330]") {
+    print "failed\n";
+}
+if ($modAAs[1] ne "A") {
+    print "failed\n";
+}
+if ($modAAs[2] ne "T") {
+    print "failed\n";
+}
+
+
+@modAAs = $helper->getModifiedAAs("AC[330]AT");
+if ( $#modAAs != 3) {
+    print "failed\n";
+}
+if ($modAAs[0] ne "A") {
+    print "failed\n";
+}
+if ($modAAs[1] ne "C[330]") {
+    print "failed\n";
+}
+if ($modAAs[2] ne "A") {
+    print "failed\n";
+}
+if ($modAAs[3] ne "T") {
+    print "failed\n";
+}
+
+
+@masses = $helper->getMasses("AC[330]AT");
+if ( $#masses != 3) {
+    print "failed\n";
+}
 for (my $i = 0; $i <= $#masses; $i++) {
     print "$masses[$i]\n";
 }
