7e782a48367f3a50e18c50f0ca8baf4651b30d4d
edeutsch <gasif555@gmail.com>
2007-06-22 06:22:31 +0000

    added annotation tables as well as proteotypic peptide table definitions

--- sbeams/lib/perl/SBEAMS/PeptideAtlas/Tables.pm
+++ sbeams/lib/perl/SBEAMS/PeptideAtlas/Tables.pm
@@ -70,9 +70,18 @@ use vars qw(@ISA @EXPORT
     $TBAT_NIST_LIBRARY_SPECTRUM_PEAK
     $TBAT_NIST_LIBRARY_SPECTRUM_COMMENT
     $TBAT_NIST_SPECTRUM_TYPE
+
     $TBAT_SPECTRUM_IDENTIFICATION
     $TBAT_SPECTRUM
     $TBAT_SPECTRUM_PEAK
+
+    $TBAT_SPECTRUM_ANNOTATION
+    $TBAT_SPECTRUM_ANNOTATION_LEVEL
+    $TBAT_PEPTIDE_ANNOTATION
+    $TBAT_MODIFIED_PEPTIDE_ANNOTATION
+    $TBAT_TRANSITION_SUITABILITY_LEVEL
+
+    $TBAT_PROTEOTYPIC_PEPTIDE
 );
 
 
@@ -142,10 +151,18 @@ require Exporter;
     $TBAT_NIST_LIBRARY_SPECTRUM_PEAK
     $TBAT_NIST_LIBRARY_SPECTRUM_COMMENT
     $TBAT_NIST_SPECTRUM_TYPE
+
     $TBAT_SPECTRUM_IDENTIFICATION
     $TBAT_SPECTRUM
     $TBAT_SPECTRUM_PEAK
 
+    $TBAT_SPECTRUM_ANNOTATION
+    $TBAT_SPECTRUM_ANNOTATION_LEVEL
+    $TBAT_PEPTIDE_ANNOTATION
+    $TBAT_MODIFIED_PEPTIDE_ANNOTATION
+    $TBAT_TRANSITION_SUITABILITY_LEVEL
+
+    $TBAT_PROTEOTYPIC_PEPTIDE
 );
 
 
@@ -204,12 +221,21 @@ $TBAT_ATLAS_SEARCH_BATCH_PARAMETER_SET = "${mod}atlas_search_batch_parameter_set
 
 $TBAT_SEARCH_KEY                = "${mod}search_key";
 
-$TBAT_NIST_LIBRARY              = "${mod}NIST_library";
-$TBAT_NIST_LIBRARY_SPECTRUM     = "${mod}NIST_library_spectrum";
+$TBAT_NIST_LIBRARY                   = "${mod}NIST_library";
+$TBAT_NIST_LIBRARY_SPECTRUM          = "${mod}NIST_library_spectrum";
 $TBAT_NIST_LIBRARY_SPECTRUM_PEAK     = "${mod}NIST_library_spectrum_peak";
-$TBAT_NIST_LIBRARY_SPECTRUM_COMMENT     = "${mod}NIST_library_spectrum_comment";
-$TBAT_NIST_SPECTRUM_TYPE     = "${mod}NIST_spectrum_type";
-$TBAT_SPECTRUM_IDENTIFICATION     = "${mod}spectrum_identification";
-$TBAT_SPECTRUM     = "${mod}spectrum";
-$TBAT_SPECTRUM_PEAK     = "${mod}spectrum_peak";
+$TBAT_NIST_LIBRARY_SPECTRUM_COMMENT  = "${mod}NIST_library_spectrum_comment";
+$TBAT_NIST_SPECTRUM_TYPE             = "${mod}NIST_spectrum_type";
+
+$TBAT_SPECTRUM_IDENTIFICATION    = "${mod}spectrum_identification";
+$TBAT_SPECTRUM                   = "${mod}spectrum";
+$TBAT_SPECTRUM_PEAK              = "${mod}spectrum_peak";
+
+$TBAT_SPECTRUM_ANNOTATION           = "${mod}spectrum_annotation";
+$TBAT_SPECTRUM_ANNOTATION_LEVEL     = "${mod}spectrum_annotation_level";
+$TBAT_PEPTIDE_ANNOTATION            = "${mod}peptide_annotation";
+$TBAT_MODIFIED_PEPTIDE_ANNOTATION   = "${mod}modified_peptide_annotation";
+$TBAT_TRANSITION_SUITABILITY_LEVEL  = "${mod}transition_suitability_level";
+
+$TBAT_PROTEOTYPIC_PEPTIDE           = "${mod}proteotypic_peptide";
 
