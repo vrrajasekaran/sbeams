0331ce022ca6eba3c7125a0688393fbb1e1f63e5
edeutsch <edeutsch@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2007-06-23 18:18:50 +0000

    added code displaying information about theoretical proteotypic peptide information

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/PeptideAtlas/BestPeptideSelector.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/PeptideAtlas/BestPeptideSelector.pm
@@ -31,6 +31,8 @@ use SBEAMS::Connection::Tables;
 use SBEAMS::Connection::Settings;
 use SBEAMS::PeptideAtlas::Tables;
 
+use Data::Dumper;
+
 
 ###############################################################################
 # Global variables
@@ -243,6 +245,125 @@ sub getBestPeptidesDisplay {
   return 1;
 
 } # end getBestPeptidesDisplay
+
+
+
+###############################################################################
+# getHighlyObservablePeptides -- Selects the best piptides from some application
+###############################################################################
+sub getHighlyObservablePeptides {
+  my $METHOD = 'getHighlyObservablePeptides';
+  my $self = shift || die ("self not passed");
+  my %args = @_;
+
+  #### Process parameters
+  my $atlas_build_id = $args{atlas_build_id}
+    or die("ERROR[$METHOD]: Parameter atlas_build_id not passed");
+
+  my $biosequence_id = $args{biosequence_id}
+    or die("ERROR[$METHOD]: Parameter biosequence_id not passed");
+
+
+  #### Define the desired columns in the query
+  #### [friendly name used in url_cols,SQL,displayed column title]
+  my @column_array = (
+    ["peptide_accession","P.peptide_accession","Peptide Accession"],
+    ["preceding_residue","PTP.preceding_residue","Pre AA"],
+    ["peptide_sequence","PTP.theo_tryptic_peptide_sequence","Peptide Sequence"],
+    ["following_residue","PTP.following_residue","Fol AA"],
+    ["suitability_score","(PTP.indiana_score+(CASE WHEN PTP.theo_tryptic_peptide_sequence LIKE '\%C\%' THEN PTP.parag_score_ICAT ELSE PTP.parag_score_ESI END))/2","Suitability Score"],
+    ["detectabilitypredictor_score","PTP.indiana_score","Detectability Predictor Score"],
+    ["peptidesieve_score","(CASE WHEN PTP.theo_tryptic_peptide_sequence LIKE '\%C\%' THEN PTP.parag_score_ICAT ELSE PTP.parag_score_ESI END)","PeptideSieve Score"],
+    #["parag_score_ESI","PTP.parag_score_ESI","PM ESI"],
+    #["parag_score_ICAT","PTP.parag_score_ICAT","PM ICAT"],
+  );
+  my %colnameidx = ();
+  my @column_titles = ();
+  my $columns_clause = $sbeams->build_SQL_columns_list(
+    column_array_ref=>\@column_array,
+    colnameidx_ref=>\%colnameidx,
+    column_titles_ref=>\@column_titles
+  );
+
+  #### Define a query to return peptides for this protein
+  my $sql = qq~
+     SELECT $columns_clause
+     FROM $TBAT_PROTEOTYPIC_PEPTIDE PTP
+     LEFT JOIN $TBAT_PEPTIDE P
+          ON ( PTP.matched_peptide_id = P.peptide_id )
+     LEFT JOIN $TBAT_BIOSEQUENCE BS
+          ON ( PTP.matched_biosequence_id = BS.biosequence_id )
+     LEFT JOIN $TBAT_DBXREF DBX ON ( BS.dbxref_id = DBX.dbxref_id )
+    WHERE 1 = 1
+	  AND PTP.matched_biosequence_id = $biosequence_id
+          AND PTP.indiana_score >= 0.5
+    ORDER BY PTP.indiana_score+(CASE WHEN PTP.theo_tryptic_peptide_sequence LIKE '\%C\%' THEN PTP.parag_score_ICAT ELSE PTP.parag_score_ESI END) DESC
+  ~;
+
+  #### Fetch the results from the database server
+  my %resultset = ();
+  my $resultset_ref = \%resultset;
+  $sbeams->fetchResultSet(
+    sql_query=>$sql,
+    resultset_ref=>$resultset_ref,
+  );
+
+
+  my $result;
+  $result->{resultset_ref} = $resultset_ref;
+  $result->{column_titles_ref} = \@column_titles;
+
+  return $result;
+
+} # end getHighlyObservablePeptides
+
+
+###############################################################################
+# getHighlyObservablePeptidesDisplay -- Selects the best piptides from some application
+###############################################################################
+sub getHighlyObservablePeptidesDisplay {
+  my $METHOD = 'getHighlyObservablePeptidesDisplay';
+  my $self = shift || die ("self not passed");
+  my %args = @_;
+
+  #### Process parameters
+  my $atlas_build_id = $args{atlas_build_id}
+    or die("ERROR[$METHOD]: Parameter atlas_build_id not passed");
+
+  my $best_peptide_information = $args{best_peptide_information}
+    or die("ERROR[$METHOD]: Parameter best_peptide_information not passed");
+
+  my $query_parameters_ref = $args{query_parameters_ref}
+    or die("ERROR[$METHOD]: Parameter query_parameters_ref not passed");
+
+  my $base_url = $args{base_url}
+    or die("ERROR[$METHOD]: Parameter base_url not passed");
+
+
+  my $resultset_ref = $best_peptide_information->{resultset_ref};
+  my $column_titles_ref = $best_peptide_information->{column_titles_ref};
+
+  #### Define the hypertext links for columns that need them
+  my %url_cols = (
+    	       'Peptide Accession' => "$CGI_BASE_DIR/PeptideAtlas/GetPeptide?_tab=3&atlas_build_id=$atlas_build_id&searchWithinThis=Peptide+Name&searchForThis=%V&action=QUERY",
+  );
+
+  my %hidden_cols;
+
+  $sbeams->displayResultSet(
+      resultset_ref=>$resultset_ref,
+      query_parameters_ref=>$query_parameters_ref,
+      #rs_params_ref=>\%rs_params,
+      url_cols_ref=>\%url_cols,
+      hidden_cols_ref=>\%hidden_cols,
+      #max_widths=>\%max_widths,
+      column_titles_ref=>$column_titles_ref,
+      base_url=>$base_url,
+  );
+
+  return 1;
+
+} # end getHighlyObservablePeptidesDisplay
 
 
 
