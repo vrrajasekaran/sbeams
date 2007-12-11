4e1de3b1cc87225aee64d21571ed02bc30efb05d
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2007-12-11 09:20:02 +0000

    script to export Atlas build as XML

---
+++ sbeams/trunk/sbeams/lib/scripts/PeptideAtlas/export_atlas_build.pl
@@ -0,0 +1,117 @@
+#!/usr/local/bin/perl -w
+
+
+# This program will export an atlas build as an sbeams table XML document.  
+# See notes at /doc/PeptideAtlas/export_atlas_build.txt for more details.
+
+use DBI;
+use Getopt::Long;
+use FindBin qw( $Bin );
+
+use lib "$Bin/../../perl";
+use SBEAMS::Connection::Settings qw( $DBCONFIG $DBINSTANCE );
+use SBEAMS::Connection;
+#use SBEAMS::PeptideAtlas::Settings;
+use strict;
+
+my $sbeams = SBEAMS::Connection->new();
+
+$|++; # don't buffer output
+my $args = processArgs();
+
+{ # MAIN
+
+  printUsage( "Missing required parameter build_id" ) unless $args->{build_id};
+
+  my $file = write_export_xml();
+  run_data_export( $file );
+}
+
+sub run_data_export {
+  my $expfile = shift;
+  my $outfile = "/tmp/atlas_build_$args->{build_id}.xml";
+
+  # Make the user deal with this
+  die "File $outfile exists" if -e $outfile;
+
+  my $synstr = 'matched_biosequence_id=biosequence_id,lab_id=organization_id,primary_contact_id=contact_id,supervisor_contact_id=contact_id,PI_contact_id=contact_id,parent_organization_id=organization_id,department_id=organization_id,group_id=organization_id';
+
+  my $cmd = "$Bin/../Core/DataExport.pl -c $expfile -o $outfile -r -q -s $synstr -p 0";
+  my @result = system( "$cmd" );
+
+  my $pcmd = "perl -pi -e 's/(atlas_build_id=)$args->{build_id}(&amp)/" . '${1}1${2}' . "/' $outfile";
+  print "$pcmd\n";
+  @result = system( "$pcmd" );
+}
+
+
+sub write_export_xml {
+  undef local $/;
+  my $xml =<DATA>;
+#  my $xml = '<export_command_list><export_data table_name="AT_search_key" qualifiers="atlas_build_id=ATLAS_BUILD_ID"></export_data></export_command_list>';
+
+  for my $table ( qw( TBAT_PEPTIDE_INSTANCE TBAT_ATLAS_BUILD_SEARCH_BATCH 
+                      TBAT_MODIFIED_PEPTIDE_INSTANCE TBAT_ATLAS_BUILD_SAMPLE  ) ) {
+    my $realname = $sbeams->evalSQL( '$' . $table );
+    $xml =~ s/$table/$realname/g;
+  }
+  
+  $xml =~ s/ATLAS_BUILD_ID/$args->{build_id}/g;
+  die "TBAT!" if $xml =~ /TBAT/;
+
+  my $file = "/tmp/atlas_exp_$args->{build_id}.exp";
+
+  # Make the user deal with this
+  die "File $file exists" if -e $file;
+
+  open( XML, ">$file" ) || die "Unable to open file: $file";
+  print XML $xml;
+  close XML;
+  return( $file );
+}
+
+sub printUsage {
+  my $err = shift || '';
+  print( <<"  EOU" );
+   $err
+   
+   Usage: $0 -b build_id
+
+    -b, --build_id      Specify atlas build ID to export 
+     
+  EOU
+  exit;
+}
+
+sub processArgs {
+  my %args;
+  unless( GetOptions ( \%args, 'build_id:i' ) ) {
+    printUsage("Error with options, please check usage:");
+  }
+
+  return \%args;
+}
+
+# Removed from table list; 
+#<export_data table_name="AT_sample_publication" qualifiers="sample_id IN ( SELECT DISTINCT sample_id FROM TBAT_ATLAS_BUILD_SAMPLE WHERE atlas_build_id=ATLAS_BUILD_ID )"></export_data>
+
+#<export_data table_name="AT_atlas_search_batch_parameter" qualifiers="atlas_search_batch_id IN ( SELECT DISTINCT atlas_search_batch_id FROM TBAT_ATLAS_BUILD_SEARCH_BATCH WHERE atlas_build_id=ATLAS_BUILD_ID )"></export_data>
+#  <export_data table_name="AT_atlas_search_batch_parameter_set" qualifiers="atlas_search_batch_id IN ( SELECT DISTINCT atlas_search_batch_id FROM TBAT_ATLAS_BUILD_SEARCH_BATCH WHERE atlas_build_id=ATLAS_BUILD_ID )"></export_data>
+#
+__DATA__
+<export_command_list>
+  <export_data table_name="AT_atlas_build_sample" qualifiers="atlas_build_id=ATLAS_BUILD_ID"></export_data>
+  <export_data table_name="AT_default_atlas_build" qualifiers="atlas_build_id=ATLAS_BUILD_ID"></export_data>
+  <export_data table_name="AT_spectra_description_set" qualifiers="atlas_build_id=ATLAS_BUILD_ID"></export_data>
+  <export_data table_name="AT_search_key" qualifiers="atlas_build_id=ATLAS_BUILD_ID"></export_data>
+
+  <export_data table_name="AT_peptide_instance_search_batch" qualifiers="peptide_instance_id IN ( SELECT DISTINCT peptide_instance_id FROM TBAT_PEPTIDE_INSTANCE WHERE atlas_build_id=ATLAS_BUILD_ID )"></export_data>
+<export_data table_name="AT_peptide_instance_sample" qualifiers="peptide_instance_id IN ( SELECT DISTINCT peptide_instance_id FROM TBAT_PEPTIDE_INSTANCE WHERE atlas_build_id=ATLAS_BUILD_ID )"></export_data>
+  <export_data table_name="AT_peptide_mapping" qualifiers="peptide_instance_id IN ( SELECT DISTINCT peptide_instance_id FROM TBAT_PEPTIDE_INSTANCE WHERE atlas_build_id=ATLAS_BUILD_ID )"></export_data>
+
+
+
+  <export_data table_name="AT_modified_peptide_instance_sample" qualifiers="modified_peptide_instance_id IN ( select modified_peptide_instance_id FROM TBAT_MODIFIED_PEPTIDE_INSTANCE WHERE peptide_instance_id IN ( SELECT DISTINCT peptide_instance_id FROM TBAT_PEPTIDE_INSTANCE WHERE atlas_build_id=ATLAS_BUILD_ID ) )"></export_data>
+  <export_data table_name="AT_modified_peptide_instance_search_batch" qualifiers="modified_peptide_instance_id IN ( select modified_peptide_instance_id FROM TBAT_MODIFIED_PEPTIDE_INSTANCE WHERE peptide_instance_id IN ( SELECT DISTINCT peptide_instance_id FROM TBAT_PEPTIDE_INSTANCE WHERE atlas_build_id=ATLAS_BUILD_ID ) )"></export_data>
+
+</export_command_list>
