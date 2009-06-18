0792cae06c51bf337595df664ceee96ffb1ebd12
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2009-06-18 20:58:11 +0000

    storable tests.  duh.

---
+++ sbeams/lib/perl/t/Core/storableTest.pl
@@ -0,0 +1,39 @@
+#!/usr/local/bin/perl -w
+
+use strict;
+use Storable qw(nstore retrieve);
+use Test::More tests => 4;
+use Test::Harness;
+
+ok( store_array(), 'Store array');
+ok( retrieve_array(), 'Retrieve array');
+ok( store_hash(), 'Store hash');
+ok( retrieve_hash(), 'Retrieve hash');
+
+sub store_array {
+  my @array = ( 'PeptideAtlas_atlas_build_id', 123 );
+  nstore(\@array, 'array.sto') || return 0;
+}
+
+sub store_hash {
+  my %hash = ( PeptideAtlas_atlas_build_id => 123 );
+  nstore(\%hash, 'hash.sto') || return 0;
+
+}
+
+sub retrieve_array {
+  my $arrayref = retrieve("array.sto") || return 0;
+  my $ok = $arrayref->[0];
+  return $ok
+}
+
+sub retrieve_hash {
+  my $hashref = retrieve("hash.sto") || return 0;
+  my $ok = 0;
+  for my $k ( keys( %$hashref ) ) {
+    $ok = $k;
+  }
+  return $ok
+}
+
+              
