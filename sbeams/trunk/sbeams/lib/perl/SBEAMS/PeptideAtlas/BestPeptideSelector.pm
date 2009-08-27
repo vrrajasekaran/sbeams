a1c8781bc5f00f90dc3b8bec86374230bbdda73c
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2009-08-27 06:37:56 +0000

    Added multi-build support, explicit bioseq_set, build weighting.

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/PeptideAtlas/BestPeptideSelector.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/PeptideAtlas/BestPeptideSelector.pm
@@ -212,6 +212,11 @@ sub getBestPeptides {
       push @annot, 'MC';
 		}
 
+    if ( $args{build_weight} ) {
+      my $build_id = $resultset_ref->{data_ref}->[$i]->[$cols->{atlas_build}];
+      $suitability_score *= $args{build_weight}->{$build_id};
+    }
+
     $resultset_ref->{data_ref}->[$i]->[$cols->{suitability_score}] =
       sprintf("%.2f",$suitability_score);
 
@@ -855,6 +860,371 @@ sub make_sort_headings {
   return \@marked;
 }
 
+# Copied shamelessly from g_p_o_p, but don't want to mess up the former yet.
+sub get_pabst_multibuild_observed_peptides_depr {
+
+  my $self = shift;
+  my %args = @_;
+
+  return undef unless $args{atlas_build};
+  return undef unless ref($args{atlas_build}) eq 'ARRAY';
+
+  my @builds = @{$args{atlas_build}};
+
+  my @weights;
+  if(  ref($args{build_weights}) eq 'ARRAY' ) {
+    @weights = @{$args{build_weights}};
+  } else { 
+    @weights = map( 1 , @builds);
+  }
+
+  my $cnt = 0;
+  my %build2weight;
+  for my $b ( @builds ) {
+    $build2weight{$b} = $weights[$cnt];
+    $cnt++;
+  }
+
+  my $build_where = 'WHERE PI.atlas_build_id IN ( ' . join( ',', @builds ) . ')';
+
+  # array of bioseq names for which to fetch peptides
+  my $name_in = $args{protein_in_clause} || '';
+
+  # minimum n_obs to consider as observed
+  my $nobs_and = $args{min_nobs_clause} || ''; 
+
+  my $name_like = $args{name_like} || '';
+
+  my $pepsql =<<"  END"; 
+  SELECT DISTINCT biosequence_name, 
+                  preceding_residue,
+                  peptide_sequence,
+                  following_residue,           
+                  CASE WHEN empirical_proteotypic_score IS NULL THEN .5  
+                       ELSE  empirical_proteotypic_score 
+                  END AS empirical_proteotypic_score,
+                  '' AS suitability_score,
+                  '' AS predicted_suitability_score,
+                  '' AS merged_score,
+                  STR(molecular_weight, 7, 4) Molecular_weight,
+                  STR(P.SSRCalc_relative_hydrophobicity,7,2) AS "SSRCalc_relative_hydrophobicity",
+                  PI.n_protein_mappings AS "n_protein_mappings",
+                  PI.n_genome_locations AS "n_genome_locations",
+                  STR(PI.best_probability,7,3) AS "best_probability",
+                  PI.n_observations AS "n_observations",
+                  '' as annotations,
+                  PI.atlas_build_id,
+                  preceding_residue || peptide_sequence || following_residue,
+                  P.peptide_accession
+  FROM $TBAT_PEPTIDE_INSTANCE PI
+  JOIN $TBAT_PEPTIDE P ON ( PI.peptide_id = P.peptide_id )
+  JOIN $TBAT_PEPTIDE_MAPPING PM ON ( PI.peptide_instance_id = PM.peptide_instance_id )
+  JOIN $TBAT_BIOSEQUENCE BS ON ( PM.matched_biosequence_id = BS.biosequence_id )
+  $build_where
+  $nobs_and
+  $name_like
+  $name_in
+  ORDER BY biosequence_name ASC, preceding_residue || peptide_sequence || following_residue, atlas_build_id ASC
+ 
+  END
+
+  $self->{_cached_acc} ||= {};
+
+  my $sth = $sbeams->get_statement_handle( $pepsql );
+  # Big hash of proteins
+  my $pep_cnt;
+  my %proteins;
+
+  # New feature, we want to use the best EPS score from any build, where
+  # the ESP might be adjusted by a build weight.  So we'll cache peptides until
+  # we have a new one ( prev + seq + follow ), then pick the best and move on.
+  my %peptides;
+
+  my $curr_prot = '';
+  my $prev_prot = '';
+  my @sorted;
+  my $curr_pep = '';
+  my $prev_pep = '';
+  my $curr_build;
+  while( my @row = $sth->fetchrow_array() ) {
+
+    # Each protein is an arrayref
+    $curr_prot = $row[0];
+    $proteins{$curr_prot} ||= [];
+
+    $prev_prot ||= $curr_prot;
+
+    # pop off peptide accession and cache
+    my $pa = pop @row;
+    $self->{_cached_acc}->{$row[2]} = $pa;
+
+    # Also pop concat peptide
+    $curr_pep = pop @row;
+#    print "CURRENTS are $curr_pep and $curr_prot!\n";
+
+    $curr_build = $row[15];
+
+    # Each protein is an arrayref
+    $peptides{$curr_pep} ||= [];
+
+#    print "CP is $curr_pep, CB is $curr_build\n";
+
+
+#    print "pre-adjust score is $row[4]\n";
+    # Make build 'weight' adjustment
+#    $row[4] *= $build2weight{$curr_build};
+#print "$curr_prot - $row[2] adjusted to $row[4] with $build2weight{$curr_build}\n"; 
+
+#    if ( !$prev_pep ) { # first time through
+#      push @{$peptides{$curr_pep}}, \@row;
+#    } elsif ( $prev_pep eq $curr_pep ) { # repeat peptide
+#      push @{$peptides{$curr_pep}}, \@row;
+#    } else { 
+     
+    print "pushing $row[2] for build $row[15]\n";
+    push @{$peptides{$curr_pep}}, \@row;
+
+    # new peptide, this is where the action is!
+    if ( $prev_pep && $prev_pep ne $curr_pep ) { 
+#print "In sortie!\n";
+      # The score is in row 4
+#print "peptides has " . scalar( @{$peptides{$prev_pep}} ).  "entries\n";
+#      @sorted = sort { $b->[4] <=> $a->[4] } @{$peptides{$prev_pep}};
+#print "sorted has " . scalar( @sorted ).  " entries\n";
+
+      # We will use first entry     
+#      push @{$proteins{$curr_prot}}, $sorted[0];
+#print "Using score $sorted[0]->[4] from build $sorted[0]->[15] ( $build2weight{$sorted[0]->[15]} )\n"; 
+
+      my $skip = 0;
+      for my $build ( @{$args{atlas_build}} ) {
+        print "considering build $build\n";
+        for my $buildpep ( @{$peptides{$prev_pep}} ) {
+          if ( $buildpep->[15] == $build ) {
+            print "build $build is the winner for $buildpep->[2]\n";
+            push @{$proteins{$curr_prot}}, $buildpep;
+            $skip++;
+            last;
+          }
+        }
+        last if $skip;
+      }
+
+#print " > score is $s->[4] from build $s->[15] ( $build2weight{$s->[15]} )\n";
+
+    } elsif ( $prev_pep ) {
+      # Reset peptide storage
+      if ( $prev_prot ne $curr_prot ) {
+        # Reset peptide hash for each new protein.
+        $peptides{$prev_pep} = [];
+      }
+    }
+
+    $prev_pep = $curr_pep;
+    $pep_cnt++;
+  }
+
+  # Have to collect one straggler
+  # Each protein is an arrayref
+  $proteins{$curr_prot} ||= [];
+  my $skip = 0;
+  for my $build ( @{args{atlas_build}} ) {
+    for my $buildpep ( @{$peptides{$prev_pep}} ) {
+      if ( $buildpep->[15] == $build ) {
+        print "build $build is the winner for $buildpep->[2]\n";
+        push @{$proteins{$curr_prot}}, $buildpep;
+        $skip++;
+        last;
+      }
+    }
+    last if $skip;
+  }
+  
+
+#print " > score is $s->[4] from build $s->[15] ( $build2weight{$s->[15]} )\n";
+
+#  push @{$proteins{$curr_prot}}, $sorted[0];
+
+  
+#  print STDERR "Saw a respectable " . scalar( keys( %proteins ) ) . "  total proteins and $pep_cnt peptides\n";
+  print STDERR "Observed " . scalar( keys( %proteins ) ) . "  total proteins and $pep_cnt peptides\n" if $args{verbose};
+#  my $cnt = 0; for my $k ( keys ( %proteins ) ) { print "$k\n"; last if $cnt++ >= 10; }
+  
+  my $headings = $self->get_pabst_headings( as_col_hash => 1 );
+  my $scores = $self->get_pabst_penalty_values();
+  # Score adjustment for observed peptides!!!
+  # Bonus obs deprecated.
+  my $obs_adjustment = ( defined $args{bonus_obs} ) ? $args{bonus_obs} : 
+                       ( defined $scores->{obs} ) ? $scores->{obs} : 1;
+
+  my %protein_hash;
+  for my $prot ( keys( %proteins ) ) {
+#    for my $row ( @{$proteins{$prot}} ) { print STDERR "Before EPS is $row->[4], SS is $row->[5] for $row->[2]\n"; }
+
+    $self->getBestPeptides( resultset_ref => { data_ref => $proteins{$prot}, 
+                          column_hash_ref => $headings },
+                                 annotate => 1,
+                                ss_adjust => $obs_adjustment,
+                             build_weight => \%build2weight,
+                          );
+    $protein_hash{$prot} ||= {};
+    for my $row ( @{$proteins{$prot}} ) {
+      $protein_hash{$prot}->{$row->[1].$row->[2].$row->[3]} = $row;
+#      print STDERR "After EPS is $row->[4], SS is $row->[5] for $row->[2]\n";
+    }
+
+  }
+  return \%protein_hash;
+}
+
+
+# Copied shamelessly from g_p_o_p, but don't want to mess up the former yet.
+sub get_pabst_multibuild_observed_peptides {
+
+  my $self = shift;
+  my %args = @_;
+
+  return undef unless $args{atlas_build};
+  return undef unless ref($args{atlas_build}) eq 'ARRAY';
+
+  my @builds = @{$args{atlas_build}};
+
+  my @weights;
+  if(  ref($args{build_weights}) eq 'ARRAY' ) {
+    @weights = @{$args{build_weights}};
+  } else { 
+    @weights = map( 1 , @builds);
+  }
+
+  my $cnt = 0;
+  my %build2weight;
+  for my $b ( @builds ) {
+    $build2weight{$b} = $weights[$cnt];
+    $cnt++;
+  }
+
+  my $build_where = 'WHERE PI.atlas_build_id IN ( ' . join( ',', @builds ) . ')';
+
+  # array of bioseq names for which to fetch peptides
+  my $name_in = $args{protein_in_clause} || '';
+
+  # minimum n_obs to consider as observed
+  my $nobs_and = $args{min_nobs_clause} || ''; 
+
+  my $name_like = $args{name_like} || '';
+
+  my $pepsql =<<"  END"; 
+  SELECT DISTINCT biosequence_name, 
+                  preceding_residue,
+                  peptide_sequence,
+                  following_residue,           
+                  CASE WHEN empirical_proteotypic_score IS NULL THEN .5  
+                       ELSE  empirical_proteotypic_score 
+                  END AS empirical_proteotypic_score,
+                  '' AS suitability_score,
+                  '' AS predicted_suitability_score,
+                  '' AS merged_score,
+                  STR(molecular_weight, 7, 4) Molecular_weight,
+                  STR(P.SSRCalc_relative_hydrophobicity,7,2) AS "SSRCalc_relative_hydrophobicity",
+                  PI.n_protein_mappings AS "n_protein_mappings",
+                  PI.n_genome_locations AS "n_genome_locations",
+                  STR(PI.best_probability,7,3) AS "best_probability",
+                  PI.n_observations AS "n_observations",
+                  '' as annotations,
+                  PI.atlas_build_id,
+                  preceding_residue || peptide_sequence || following_residue,
+                  P.peptide_accession
+  FROM $TBAT_PEPTIDE_INSTANCE PI
+  JOIN $TBAT_PEPTIDE P ON ( PI.peptide_id = P.peptide_id )
+  JOIN $TBAT_PEPTIDE_MAPPING PM ON ( PI.peptide_instance_id = PM.peptide_instance_id )
+  JOIN $TBAT_BIOSEQUENCE BS ON ( PM.matched_biosequence_id = BS.biosequence_id )
+  $build_where
+  $nobs_and
+  $name_like
+  $name_in
+  ORDER BY biosequence_name ASC, preceding_residue || peptide_sequence || following_residue, atlas_build_id ASC
+ 
+  END
+
+  $self->{_cached_acc} ||= {};
+
+  my $sth = $sbeams->get_statement_handle( $pepsql );
+
+  my $pep_cnt;
+
+  # Big hash of proteins
+  my %proteins;
+
+  # New feature, we want to use the best EPS score from any build, where
+  # the ESP might be adjusted by a build weight.  So we'll cache peptides until
+  # we have a new one ( prev + seq + follow ), then pick the best and move on.
+  my %peptides;
+
+  while( my @row = $sth->fetchrow_array() ) {
+
+    # Each protein is an arrayref
+    my $curr_prot = $row[0];
+
+    # pop off peptide accession and cache
+    my $pa = pop @row;
+    $self->{_cached_acc}->{$row[2]} = $pa;
+
+    # Also pop concat peptide
+    my $curr_pep = pop @row;
+
+    my $curr_build = $row[15];
+
+    # Each protein is an arrayref
+    $peptides{$curr_prot} ||= {};
+    $peptides{$curr_prot}->{$curr_pep} ||= {};
+    $peptides{$curr_prot}->{$curr_pep}->{$curr_build} = \@row;
+
+  }
+
+  for my $prot ( sort( keys( %peptides ) ) ) {
+    for my $pep ( sort( keys( %{$peptides{$prot}} ) ) ) {
+      my $skip = 0;
+      for my $build ( @{$args{atlas_build}} ) {
+#        print "considering build $build\n";
+        if ( $peptides{$prot}->{$pep}->{$build} ) {
+          push @{$proteins{$prot}}, $peptides{$prot}->{$pep}->{$build};
+          last; 
+        }
+      }
+
+    }
+  }
+
+#  print STDERR "Saw a respectable " . scalar( keys( %proteins ) ) . "  total proteins and $pep_cnt peptides\n";
+  print STDERR "Observed " . scalar( keys( %proteins ) ) . "  total proteins and $pep_cnt peptides\n" if $args{verbose};
+#  my $cnt = 0; for my $k ( keys ( %proteins ) ) { print "$k\n"; last if $cnt++ >= 10; }
+  
+  my $headings = $self->get_pabst_headings( as_col_hash => 1 );
+  my $scores = $self->get_pabst_penalty_values();
+  # Score adjustment for observed peptides!!!
+  # Bonus obs deprecated.
+  my $obs_adjustment = ( defined $args{bonus_obs} ) ? $args{bonus_obs} : 
+                       ( defined $scores->{obs} ) ? $scores->{obs} : 1;
+
+  my %protein_hash;
+  for my $prot ( keys( %proteins ) ) {
+#    for my $row ( @{$proteins{$prot}} ) { print STDERR "Before EPS is $row->[4], SS is $row->[5] for $row->[2]\n"; }
+
+    $self->getBestPeptides( resultset_ref => { data_ref => $proteins{$prot}, 
+                          column_hash_ref => $headings },
+                                 annotate => 1,
+                                ss_adjust => $obs_adjustment,
+                             build_weight => \%build2weight,
+                          );
+    $protein_hash{$prot} ||= {};
+    for my $row ( @{$proteins{$prot}} ) {
+      $protein_hash{$prot}->{$row->[1].$row->[2].$row->[3]} = $row;
+#      print STDERR "After EPS is $row->[4], SS is $row->[5] for $row->[2]\n";
+    }
+
+  }
+  return \%protein_hash;
+}
 sub get_pabst_observed_peptides {
   my $self = shift;
   my %args = @_;
@@ -890,6 +1260,7 @@ sub get_pabst_observed_peptides {
                   STR(PI.best_probability,7,3) AS "best_probability",
                   PI.n_observations AS "n_observations",
                   '' as annotations,
+                  PI.atlas_build_id,
                   P.peptide_accession
   FROM $TBAT_PEPTIDE_INSTANCE PI
   JOIN $TBAT_PEPTIDE P ON ( PI.peptide_id = P.peptide_id )
@@ -901,7 +1272,6 @@ sub get_pabst_observed_peptides {
   $name_in
   ORDER BY biosequence_name, suitability_score DESC
   END
-
 
   $self->{_cached_acc} ||= {};
 
@@ -915,18 +1285,10 @@ sub get_pabst_observed_peptides {
     my $pa = pop @row;
     $self->{_cached_acc}->{$row[2]} = $pa;
 
-     # Adjust the score with a PBR!
-#     my $row = $pep_sel->pabst_evaluate_peptides( peptides => [\@row], seq_idx => 2, follow_idx => 3, score_idx => 4 );
-#     @row = @{$row->[0]};
-
     # Each protein is an arrayref
     $proteins{$row[0]} ||= [];
     push @{$proteins{$row[0]}}, \@row;
 
-    # Each protein is a hashref
-#    $proteins{$row[0]} ||= {};
-    # That hashref points to the row, keyed by sequence w/ flanking AA
-#    $proteins{$row[0]}->{$row[1].$row[2].$row[3]} = \@row;
     $pep_cnt++;
   }
 #  print STDERR "Saw a respectable " . scalar( keys( %proteins ) ) . "  total proteins and $pep_cnt peptides\n";
@@ -936,10 +1298,9 @@ sub get_pabst_observed_peptides {
   my $headings = $self->get_pabst_headings( as_col_hash => 1 );
   my $scores = $self->get_pabst_penalty_values();
   # Score adjustment for observed peptides!!!
+  # Bonus obs deprecated.
   my $obs_adjustment = ( defined $args{bonus_obs} ) ? $args{bonus_obs} : 
                        ( defined $scores->{obs} ) ? $scores->{obs} : 1;
-
-  $log->debug( "obs adjustment is $obs_adjustment, from $scores->{W}? " );
 
   my %protein_hash;
   for my $prot ( keys( %proteins ) ) {
@@ -955,8 +1316,6 @@ sub get_pabst_observed_peptides {
       $protein_hash{$prot}->{$row->[1].$row->[2].$row->[3]} = $row;
 #      print STDERR "After EPS is $row->[4], SS is $row->[5] for $row->[2]\n";
     }
-#  my $n_rows = scalar(@{$resultset_ref->{data_ref}});
-#  my $cols = $resultset_ref->{column_hash_ref};
 
   }
   return \%protein_hash;
@@ -980,6 +1339,7 @@ sub get_pabst_headings {
                      best_probability
                      n_observations
                      annotations
+                     atlas_build
                      synthesis_score
                      syntheis_adjusted_score );
 
@@ -1003,9 +1363,21 @@ sub get_pabst_theoretical_peptides {
   my $self = shift;
   my %args = @_;
 
-  return undef unless $args{atlas_build};
+  return undef unless ($args{atlas_build} || $args{bioseq_set} );
 
-  my $build_where = "WHERE AB.atlas_build_id = $args{atlas_build}";
+  if ( !$atlas ) {
+    $atlas = SBEAMS::PeptideAtlas->new();
+  }
+
+  # Can run in one of two modes.
+  # Old-school, provide atlas_build_id
+  if ( $args{atlas_build} && !$args{bioseq_set} ) {
+    $args{bioseq_set} = $atlas->getBuildBiosequenceSetID( build_id => $args{atlas_build} );
+  }
+  # Gnu School, explicit build_id
+
+  my $where = "WHERE BS.biosequence_set_id = $args{bioseq_set}";
+
   my $name_in = $args{protein_in_clause} || '';
 
   my $pepsql =<<"  END"; 
@@ -1025,17 +1397,16 @@ sub get_pabst_theoretical_peptides {
                   n_genome_locations AS "n_genome_locations",
                   'n/a' as best_probability,
                   0 as n_observations,
-                  '' as annotations
+                  '' as annotations,
+                  '' as atlas_build
   FROM $TBAT_PROTEOTYPIC_PEPTIDE PP
   JOIN $TBAT_PROTEOTYPIC_PEPTIDE_MAPPING PM ON ( PP.proteotypic_peptide_id = PM.proteotypic_peptide_id )
   JOIN $TBAT_BIOSEQUENCE BS ON ( PM.source_biosequence_id = BS.biosequence_id )
-  JOIN $TBAT_ATLAS_BUILD AB ON ( AB.biosequence_set_id = BS.biosequence_set_id ) 
-  $build_where
+  $where
   $name_in
   ORDER BY biosequence_name, suitability_score DESC
   END
 
-#  print STDERR $pepsql; exit;
 
 
   my $sth = $sbeams->get_statement_handle( $pepsql );
@@ -1087,6 +1458,7 @@ sub merge_pabst_peptides {
   print STDERR "Merging peptides from a total of " . scalar( keys( %$theo ) ) . " proteins\n" if $args{verbose};
 
   # loop over keys of theoretical list - all proteins are represented
+  # unless of course there are none - should this work without any theo?
   for my $prot ( sort( keys( %$theo ) ) ) {
 
     $cnt++;
@@ -1119,8 +1491,9 @@ sub merge_pabst_peptides {
 # 12 best_probability
 # 13 n_observations
 # 14 annotations
-# 15 synthesis_score
-# 16 syntheis_adjusted_score
+# 15 atlas build
+# 16 synthesis_score
+# 17 syntheis_adjusted_score
 
     my $naa = 'na';
     $naa = $sbeams->makeInactiveText($naa) if $sbeams->output_mode() =~ /html/i;
@@ -1166,10 +1539,11 @@ sub merge_pabst_peptides {
                                             );
     @peptides = @{$row};
 
+
 #    for my $p ( @peptides ) { print STDERR join( ':', @$p ) . "\n" if $p->[2] eq 'LNLSENYTLSISNAR'; }
 #    print STDERR scalar( @{$peptides[0]} ) . " COLS\n";
     # OK, we have a merged array of peptides with scores.  Sort and return
-    @peptides = sort { $b->[16] <=> $a->[16] } @peptides;
+    @peptides = sort { $b->[17] <=> $a->[17] } @peptides;
 
 #    for my $p ( @peptides ) { print STDERR join( ':', @$p ) . "\n" if $p->[2] eq 'LNLSENYTLSISNAR'; }
 
@@ -1178,6 +1552,8 @@ sub merge_pabst_peptides {
     if ( $args{n_peptides} ) {
       my $cnt = 0;
       for my $pep ( @peptides ) {
+        #$pep->[15] = sprintf( "%0d", $pep->[15] );
+#        $pep->[15] = 12345; #xxxxxxxsprintf( "%0d", $pep->[15] );
         push @final_protein_list, $pep;
         $cnt++;
         last if $cnt >= $args{n_peptides};
