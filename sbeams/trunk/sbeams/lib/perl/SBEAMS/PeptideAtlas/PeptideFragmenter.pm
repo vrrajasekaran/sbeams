116acc6104de7522d668bb1fe2fa1f5e83ba9e80
lmendoza <lmendoza@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2010-11-29 18:50:59 +0000

    Eric's peptide fragmenter code

---
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/PeptideAtlas/PeptideFragmenter.pm
@@ -0,0 +1,262 @@
+package PeptideFragmenter;
+
+use strict;
+use warnings;
+
+my $DEBUG = 0;
+
+use lib "/net/dblocal/www/html/dev2/sbeams/lib/perl";
+use SBEAMS::Proteomics::AminoAcidModifications;
+my $AAmodifications = new SBEAMS::Proteomics::AminoAcidModifications;
+
+my %AAmasses = %{InitializeMass(1)};
+
+use vars qw( $mzMinimum $mzMaximum );
+
+
+###############################################################################
+# Constructor
+###############################################################################
+sub new {
+    my $this = shift;
+    my $class = ref($this) || $this;
+    my $self = {};
+    bless $self, $class;
+    $self->setMzMinimum(0);
+    $self->setMzMaximum(99000);
+    return($self);
+}
+
+
+###############################################################################
+# setDEBUG
+#
+###############################################################################
+sub setDEBUG {
+  my $SUB_NAME = "setDEBUG";
+  my $self = shift || croak("parameter self not passed");
+  my $value = shift;
+  croak("ERROR: value not passed to $SUB_NAME") if (!defined($value));
+
+  $DEBUG = $value;
+  return;
+}
+
+
+###############################################################################
+# setMzMinimum
+#
+###############################################################################
+sub setMzMinimum {
+  my $SUB_NAME = "setMzMinimum";
+  my $self = shift || croak("parameter self not passed");
+  my $value = shift;
+  die("ERROR: value not passed to $SUB_NAME") if (!defined($value));
+
+  $mzMinimum = $value;
+  return;
+}
+
+
+###############################################################################
+# setMzMaximum
+#
+###############################################################################
+sub setMzMaximum {
+  my $SUB_NAME = "setMzMaximum";
+  my $self = shift || croak("parameter self not passed");
+  my $value = shift;
+  die("ERROR: value not passed to $SUB_NAME") if (!defined($value));
+
+  $mzMaximum = $value;
+  return;
+}
+
+
+
+###############################################################################
+# getExpectedFragments
+###############################################################################
+sub getExpectedFragments {
+  my $METHOD = 'getExpectedFragments';
+  my $self = shift || die ("self not passed");
+  my %args = @_;
+
+  #### Process parameters
+  my $modifiedSequence = $args{modifiedSequence}
+    or die("ERROR[$METHOD]: Parameter modifiedSequence not passed");
+  my $charge = $args{charge}
+    or die("ERROR[$METHOD]: Parameter charge not passed");
+
+  my @residues = Fragment($modifiedSequence);
+  my $length = scalar(@residues);
+  my $pluses = '+++++++++++++';
+  my @masses;
+
+  my $H = $AAmasses{"h"};
+  my $totalMass;
+
+  for (my $i=0; $i<$length; $i++) {
+    #print "residues[$i]=$residues[$i]\n";
+    my $mass = $AAmasses{$residues[$i]};
+    unless ($mass) {
+      if ($residues[$i] =~ /(\w)\[(\d+)\]/) {
+	$mass = $AAmasses{$1};
+	if ($AAmodifications->{supported_modifications}->{monoisotopic}->{$residues[$i]}) {
+	  $mass += $AAmodifications->{supported_modifications}->{monoisotopic}->{$residues[$i]};
+	} else {
+	  die("ERROR: Unable to find mass for '$residues[$i]'");
+	}
+      }
+      unless ($mass) {
+	die("ERROR: Unable to find mass for '$residues[$i]'");
+      }
+    }
+    #print "residues[$i] = $residues[$i] = $mass\n";
+    $masses[$i] = $mass;
+    $totalMass += $mass;
+  }
+
+
+  my (@Bions,@Yions,@indices,@rev_indices,@productIons);
+  my (@Bbonds, @Ybonds);
+
+  my %tmp = (
+    mz => ( ( 2 * $H + $AAmasses{"o"} + $totalMass ) + $charge * $H ) / $charge,
+    series => 'precursor',
+    charge => $charge,
+    label => "precursor",
+  );
+  push(@productIons,\%tmp);
+
+
+  for (my $iCharge=1; $iCharge<=$charge; $iCharge++) {
+
+    my $Bion = 0;
+    my $Yion = 2 * $H + $AAmasses{"o"} + $totalMass;
+
+    #### Compute the ion masses
+    for (my $i = 0; $i<$length; $i++) {
+      $Bion += $masses[$i];
+      $Yion -= $masses[$i-1] if ($i > 0);
+
+      #### B index & Y index
+      $indices[$i] = $i+1;
+      $rev_indices[$i] = $length-$i;
+
+      #### B ion mass & Y ion mass
+      $Bions[$i] = ($Bion + $iCharge*$H)/$iCharge;
+      $Yions[$i] = ($Yion + $iCharge*$H)/$iCharge;
+
+      #### Bonds
+      $Bbonds[$i] = $residues[$i].($residues[$i+1]||' ');
+      $Ybonds[$i] = ($residues[$i-1]||' ').$residues[$i];
+
+      my %tmp = (
+        mz => $Bions[$i],
+        series => 'b',
+        ordinal => $indices[$i],
+        charge => $iCharge,
+        label => "b".$indices[$i].substr($pluses,0,$iCharge),
+        bond => $Bbonds[$i],
+      );
+      push(@productIons,\%tmp);
+
+      my %tmp2 = (
+        mz => $Yions[$i],
+        series => 'y',
+        ordinal => $rev_indices[$i],
+        charge => $iCharge,
+        label => "y".$rev_indices[$i].substr($pluses,0,$iCharge),
+        bond => $Ybonds[$i],
+      );
+      push(@productIons,\%tmp2);
+
+      #printf("%i  %10.3f  %2i  %6s  %2i  %10.3f\n",$iCharge,$Bions[$i],$indices[$i],$residues[$i],$rev_indices[$i],$Yions[$i]);
+    }
+  }
+
+  my @sortedProductIons = sort byMz @productIons;
+  foreach my $ion ( @sortedProductIons ) {
+    #printf("%10.4f  %s  %s\n",$ion->{mz},$ion->{label},$ion->{bond});
+  }
+
+
+  return(\@sortedProductIons);
+}
+
+
+
+###############################################################################
+# byMz
+###############################################################################
+sub byMz {
+  return $a->{mz} <=> $b->{mz};
+}
+
+
+###############################################################################
+# numerically
+###############################################################################
+sub numerically {
+  return $a <=> $b;
+}
+
+
+###############################################################################
+# Fragment
+###############################################################################
+sub Fragment {
+    my $peptide = shift;
+    my $length = length($peptide);
+    my @residues = ();
+    my $i;
+
+    for ($i=0; $i<$length; $i++) {
+      if (substr($peptide,$i+1,1) eq '[') {
+	if (substr($peptide,$i+5,1) eq ']') {
+	  push (@residues, substr($peptide,$i,6));
+	  $i = $i + 5;
+	} elsif (substr($peptide,$i+4,1) eq ']') {
+	  push (@residues, substr($peptide,$i,5));
+	  $i = $i + 4;
+	} else {die("Blech!");}
+      } elsif (substr($peptide,$i+1,1) =~ /\W/) {
+        push (@residues, substr($peptide,$i,2));
+        $i = $i + 1;
+      } else {
+        push (@residues, substr($peptide,$i,1));
+      }
+    }
+
+    #### Return residue array
+    return @residues;
+}
+
+
+###############################################################################
+# InitializeMasses
+###############################################################################
+sub InitializeMass {
+    my $masstype = shift;
+    my %AAmassestmp = ();
+    my ($code, $avg, $mono);
+
+    #### AminoAcidMasses contains the mass info
+    open (MASSFILE,'/net/dblocal/www/html/sbeams/cgi/Proteomics/AminoAcidMasses.dat') ||
+      die "unable to open AminoAcidMasses.dat file!\n";
+    while (<MASSFILE>) {
+      #### Ignore header line
+      next if /^CODE/;
+      ($code,$avg,$mono) = split;
+      if ($masstype) {
+        $AAmassestmp{$code} = $mono;
+      } else {
+        $AAmassestmp{$code} = $avg;
+      }
+    }
+
+    close MASSFILE;
+    #### Return references to AAmasses
+    return (\%AAmassestmp);
+}
