a9193050a36b8bc95e3c5cb8faaf8180092be2f4
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2012-02-28 01:10:42 +0000

    Added code to add spectral links

--- sbeams/lib/perl/SBEAMS/PeptideAtlas/ConsensusSpectrum.pm
+++ sbeams/lib/perl/SBEAMS/PeptideAtlas/ConsensusSpectrum.pm
@@ -179,6 +179,7 @@ sub get_spectrum_peaks {
 	my %args = @_;
 
   for my $arg ( qw( file_path entry_idx ) ) {
+		$log->printStack( 'warn' );
     die $arg unless defined $args{$arg};
   }
 
@@ -371,41 +372,72 @@ sub get_top_n_peaks {
   return \%peaks;
 }
 
-sub getConsensusLinks {
+
+##### Start safe
+
+sub getConsensusLinksSAVE {
 
   my $self = shift;
+
+  my %args = @_;
+  my $seq_and = '';
+  if ( $args{modified_sequence} ) {
+    my $sequence = $args{modified_sequence};
+    $sequence =~ s/\[\d+\]//g;
+    $seq_and = "AND sequence = '$sequence'\n";
+  }
 
   # Force production for now
   $TBAT_CONSENSUS_LIBRARY_SPECTRUM = 'peptideatlas.dbo.consensus_library_spectrum';
   $TBAT_CONSENSUS_LIBRARY = 'peptideatlas.dbo.consensus_library';
 
-  my %libs = ( it => {}, qtof => {}, qtrap => {}, CE => {},
+  my %libs = ( it => {}, qtof => {}, qtrap => {}, CE => {}, qqq => {},
           low => {}, mlow => {}, medium => {}, mhigh => {}, high=> {} );
 
+  print "Getting IT libs<BR>\n";
   my $it_sql = qq~
   SELECT modified_sequence, consensus_library_spectrum_id, charge
   FROM $TBAT_CONSENSUS_LIBRARY_SPECTRUM 
   WHERE consensus_library_id = 16
+  $seq_and
   ~;
   my $sth = $sbeams->get_statement_handle( $it_sql );
   while ( my @row = $sth->fetchrow_array() ) {
     $libs{it}->{$row[0]. $row[2]} = $row[1];
   }
+  print "Getting qqq libs with $seq_and<BR>\n";
+
+  my $qqq_sql = qq~
+  SELECT modified_sequence, consensus_library_spectrum_id, charge
+  FROM $TBAT_CONSENSUS_LIBRARY_SPECTRUM 
+  WHERE consensus_library_id = 292
+  $seq_and
+  ~;
+  my $sth = $sbeams->get_statement_handle( $qqq_sql );
+  while ( my @row = $sth->fetchrow_array() ) {
+    $libs{qqq}->{$row[0]. $row[2]} = $row[1];
+  }
+
+  print "Getting qtof libs<BR>\n";
 
   my $qtof_sql = qq~
   SELECT modified_sequence, consensus_library_spectrum_id, charge
-  FROM peptideatlas_test.dbo.consensus_library_spectrum 
-  WHERE consensus_library_id = 27
+  FROM $TBAT_CONSENSUS_LIBRARY_SPECTRUM
+  WHERE consensus_library_id = 279
+  $seq_and
   ~;
+#  print $qtof_sql;
   my $sth = $sbeams->get_statement_handle( $qtof_sql );
   while ( my @row = $sth->fetchrow_array() ) {
     $libs{qtof}->{$row[0]. $row[2]} = $row[1];
   }
 
+  print "Getting qtrap libs<BR>\n";
   my $qtrap_sql = qq~
   SELECT modified_sequence, consensus_library_spectrum_id, charge
-  FROM peptideatlas_test.dbo.consensus_library_spectrum 
-  WHERE consensus_library_id = 28
+  FROM $TBAT_CONSENSUS_LIBRARY_SPECTRUM
+  WHERE consensus_library_id = 282
+  $seq_and
   ~;
   my $sth = $sbeams->get_statement_handle( $qtrap_sql );
   while ( my @row = $sth->fetchrow_array() ) {
@@ -415,21 +447,81 @@ sub getConsensusLinks {
 
 #  my %libmap = ( 21 => 'medium', 22 => 'high', 23 => 'low', 24 => 'mhigh', 25 => 'mlow', 26 => 'avg' );
 #  my %libmap = ( 29 => 'medium', 30 => 'high', 31 => 'low', 32 => 'mhigh', 33 => 'mlow', 27 => 'avg' );
-  my %libmap = ( 242 => 'low',
-                 243 => 'mlow', 
-                 244 => 'medium',
-                 245 => 'mhigh', 
-                 246 => 'high' );
+  print "Getting CE libs<BR>\n";
+  my %libmap = ( 277 => 'low',
+                 278 => 'mlow', 
+                 279 => 'medium',
+                 280 => 'mhigh', 
+                 281 => 'high' );
 	my $libs = join( ',', keys( %libmap ));
   my $ce_sql = qq~
   SELECT modified_sequence, consensus_library_spectrum_id, charge, consensus_library_id
   FROM peptideatlas.dbo.consensus_library_spectrum 
-  WHERE consensus_library_id IN ( $libs ); 
+  WHERE consensus_library_id IN ( $libs ) 
+  $seq_and
   ~;
   my $sth = $sbeams->get_statement_handle( $ce_sql );
   while ( my @row = $sth->fetchrow_array() ) {
     $libs{$libmap{$row[3]}}->{$row[0]. $row[2]} = $row[1];
     $libs{CE}->{$row[0]. $row[2]}++;
+  }
+
+  return \%libs;
+
+}
+
+
+######################## END SAFE
+
+
+sub getConsensusLinks {
+
+  my $self = shift;
+
+  my %args = @_;
+  my $seq_and = '';
+  if ( $args{modified_sequence} ) {
+    my $sequence = $args{modified_sequence};
+    $sequence =~ s/\[\d+\]//g;
+    $seq_and = "AND sequence = '$sequence'\n";
+  }
+
+  # Force production for now
+  $TBAT_CONSENSUS_LIBRARY_SPECTRUM = 'peptideatlas.dbo.consensus_library_spectrum';
+  $TBAT_CONSENSUS_LIBRARY = 'peptideatlas.dbo.consensus_library';
+
+  my %libs = ( it => {}, qtof => {}, qtrap => {}, CE => {}, qqq => {},
+          low => {}, mlow => {}, medium => {}, mhigh => {}, high=> {} );
+
+  return \%libs if $sbeams->isGuestUser();
+
+  my %libmap = ( 277 => 'low',
+                 278 => 'mlow', 
+                 279 => 'medium',
+                 280 => 'mhigh', 
+                 281 => 'high',
+                 16 => 'it',
+                 293 => 'qqq',
+#                 279 => 'qtof',
+                 282 => 'qtrap',
+                 
+                 );
+
+	my $libs = join( ',', keys( %libmap ));
+  my $ce_sql = qq~
+  SELECT modified_sequence, consensus_library_spectrum_id, charge, consensus_library_id
+  FROM peptideatlas.dbo.consensus_library_spectrum 
+  WHERE consensus_library_id IN ( $libs ) 
+  $seq_and
+  ~;
+  my $sth = $sbeams->get_statement_handle( $ce_sql );
+  while ( my @row = $sth->fetchrow_array() ) {
+    $libs{$libmap{$row[3]}}->{$row[0]. $row[2]} = $row[1];
+    if ( $row[3] > 276 && $row[3] < 282 ) {
+      $libs{CE}->{$row[0]. $row[2]}++;
+    }
+    next unless $row[3] == 279;
+    $libs{qtof}->{$row[0]. $row[2]} = $row[1];
   }
 
   return \%libs;
