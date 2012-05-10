42b2b4b956ac7088903fe370496e5348c567e507
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2012-05-10 05:33:43 +0000

    Fixed export SQL.

--- sbeams/trunk/sbeams/lib/scripts/PeptideAtlas/export_pabst_builds.pl
+++ sbeams/trunk/sbeams/lib/scripts/PeptideAtlas/export_pabst_builds.pl
@@ -80,62 +80,74 @@ sub export_build {
 
   $col_string =~ s/modified_peptide_sequence/PI.modified_peptide_sequence/;
   $col_string =~ s/transition_source/instrument_type_name/;
-  $col_string =~ s/SSRCalc_relative_hydrophobicity/elution_time/;
+#  $col_string =~ s/SSRCalc_relative_hydrophobicity/elution_time/;
   $col_string =~ s/n_observations/PII.n_observations/;
   $col_string =~ s/biosequence_name/BS.biosequence_name/;
    
-  my $sql = qq~
-  SELECT DISTINCT  
-  $col_string, PTI.is_predicted
-  FROM PeptideAtlas.dbo.pabst_tmp_peptide PP 
-  JOIN PeptideAtlas.dbo.pabst_tmp_peptide_ion PI 
-  ON PP.pabst_peptide_id = PI.pabst_peptide_id
-  JOIN PeptideAtlas.dbo.pabst_tmp_peptide_mapping PM 
-  ON PM.pabst_peptide_id = PP.pabst_peptide_id
-  JOIN PeptideAtlas.dbo.pabst_tmp_peptide_ion_instance PII 
-  ON PI.pabst_peptide_ion_id = PII.pabst_peptide_ion_id
-  JOIN PeptideAtlas.dbo.pabst_tmp_transition PT 
-  ON PT.pabst_peptide_ion_id = PI.pabst_peptide_ion_id 
-  JOIN PeptideAtlas.dbo.pabst_tmp_transition_instance PTI 
-  ON PT.pabst_transition_id = PTI.pabst_transition_id 
-  JOIN PeptideAtlas.dbo.instrument_type IT 
-  ON ( IT.instrument_type_id = PTI.source_instrument_type_id  AND
-       IT.instrument_type_id = PII.source_instrument_type_id ) 
-  JOIN PeptideAtlas.dbo.pabst_tmp_source_priority PSP 
-  ON ( PSP.source_instrument_type_id = PTI.source_instrument_type_id )
-  JOIN peptideatlas.dbo.biosequence BS
-  ON BS.biosequence_id = PM.biosequence_id 
-  LEFT JOIN PeptideAtlas.dbo.elution_time ET ON ET.modified_peptide_sequence = PP.peptide_sequence
-  LEFT JOIN PeptideAtlas.dbo.elution_time_type ETT ON ET.elution_time_type_id = ETT.elution_time_type_id
-  WHERE  PP.pabst_build_id IN ( $args{build_id} )
-  AND ( BS.biosequence_name LIKE 'P%'  )
-  AND (elution_time_type = 'SSRCalc' OR elution_time_type IS NULL)
-  ORDER BY biosequence_name, 
-  PTI.is_predicted ASC,
-  synthesis_adjusted_score DESC,
-  PI.modified_peptide_sequence,
-  instrument_type_name DESC,
-  PII.max_precursor_intensity DESC,
-  PII.n_observations DESC,
-  relative_intensity DESC,
-  ion_rank ASC
-  ~;
+  for my $aa ( 'A' .. 'Z' ) {
+    print STDERR "Working on $aa: " . time() . "\n";
 
-#  print STDERR "\tpreparing SQL " . time() . "\n" if $args->{verbose};
-  my $sth = $sbeams->get_statement_handle( $sql );
-  print STDERR "\tprinting " . time() . "\n" if $args->{verbose};
-  while ( my @row = $sth->fetchrow_array() ) {
-    for my $i ( 6, 8 ) {
-      $row[$i] = sprintf( "%0.2f", $row[$i] );
-    }
-    for my $i ( 4, 13, 14 ) {
-      $row[$i] = sprintf( "%0.1f", $row[$i] );
-    }
-    $row[12] = int( $row[12] );
-    $row[5] .= '(pred)' if $row[16] eq 'Y';
-    print FIL join( "\t", @row[0..15] ) . "\n";
+    my $sql = qq~
+    SELECT DISTINCT  
+    $col_string, PTI.is_predicted
+    FROM PeptideAtlas.dbo.pabst_tmp_peptide PP 
+  
+    JOIN PeptideAtlas.dbo.pabst_tmp_peptide_ion PI 
+    ON PP.pabst_peptide_id = PI.pabst_peptide_id
+  
+    JOIN PeptideAtlas.dbo.pabst_tmp_peptide_mapping PM 
+    ON PM.pabst_peptide_id = PP.pabst_peptide_id
     
+    JOIN PeptideAtlas.dbo.pabst_tmp_peptide_ion_instance PII 
+    ON PI.pabst_peptide_ion_id = PII.pabst_peptide_ion_id
+    
+    JOIN PeptideAtlas.dbo.pabst_tmp_transition PT 
+    ON PT.pabst_peptide_ion_id = PI.pabst_peptide_ion_id 
+    
+    JOIN PeptideAtlas.dbo.pabst_tmp_transition_instance PTI 
+    ON PT.pabst_transition_id = PTI.pabst_transition_id 
+    
+    JOIN PeptideAtlas.dbo.instrument_type IT 
+    ON ( IT.instrument_type_id = PTI.source_instrument_type_id  AND
+         IT.instrument_type_id = PII.source_instrument_type_id ) 
+    
+    JOIN PeptideAtlas.dbo.pabst_tmp_source_priority PSP 
+    ON ( PSP.source_instrument_type_id = PTI.source_instrument_type_id )
+    
+    JOIN peptideatlas.dbo.biosequence BS
+    ON BS.biosequence_id = PM.biosequence_id 
+    
+    WHERE  PP.pabst_build_id IN ( $args{build_id} )
+    AND ( BS.biosequence_name LIKE '$aa%'  )
+    ORDER BY biosequence_name, 
+    PTI.is_predicted ASC,
+    synthesis_adjusted_score DESC,
+    PI.modified_peptide_sequence,
+    instrument_type_name DESC,
+    PII.max_precursor_intensity DESC,
+    PII.n_observations DESC,
+    relative_intensity DESC,
+    ion_rank ASC
+    ~;
+  
+  #  print STDERR "\tpreparing SQL " . time() . "\n" if $args->{verbose};
+    my $sth = $sbeams->get_statement_handle( $sql );
+    print STDERR "\tprinting " . time() . "\n" if $args->{verbose};
+    while ( my @row = $sth->fetchrow_array() ) {
+      for my $i ( 6, 8 ) {
+        $row[$i] = sprintf( "%0.2f", $row[$i] );
+      }
+      for my $i ( 4, 13, 14 ) {
+        $row[$i] = sprintf( "%0.1f", $row[$i] );
+      }
+      $row[12] = int( $row[12] );
+      $row[5] .= '(pred)' if $row[16] eq 'Y';
+      print FIL join( "\t", @row[0..15] ) . "\n";
+      
+    }
+    print STDERR "Done: " . time() . "\n";
   }
+  
   close FIL;
   return;
 
@@ -155,7 +167,7 @@ sub printUsage {
 
 sub processArgs {
   my %args;
-  unless( GetOptions ( \%args, 'build_id=i@', 'verbose', 'force' ) ) {
+  unless( GetOptions ( \%args, 'build_id=i@', 'verbose', 'force', 'yonly', 'noneutralloss' ) ) {
     printUsage("Error with options, please check usage:");
   }
   die "need build_id " unless $args{build_id};
