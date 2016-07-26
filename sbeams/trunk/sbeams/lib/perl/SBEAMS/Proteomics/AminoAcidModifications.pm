55200e8a5e2e136b1167489204a14b9aff15a969
dcampbel <dcampbel@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2016-07-26 20:55:36 +0000

    Added method to fetch modification definitions, added unimod defs for mods seen in PeptideAtlas but lacking a definition.

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/Proteomics/AminoAcidModifications.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/Proteomics/AminoAcidModifications.pm
@@ -8,7 +8,7 @@ package SBEAMS::Proteomics::AminoAcidModifications;
 #               stores relevant modification information keyed by
 #               the simplified TPP C[160] notation.
 #
-# SBEAMS is Copyright (C) 2000-2005 Institute for Systems Biology
+# SBEAMS is Copyright (C) 2000-2016 Institute for Systems Biology
 # This program is governed by the terms of the GNU General Public License (GPL)
 # version 2 as published by the Free Software Foundation.  It is provided
 # WITHOUT ANY WARRANTY.  See the full description of GPL terms in the
@@ -51,7 +51,7 @@ sub new {
       'C[161]' => 58.005479,    # Search ERROR?? Maybe not, maybe Carboxymethyl?? Latin Square?
       'C[174]' => 71.037114,    # Propionamide (Acrylamide adduct) used initially by IPAS (UniMod)
       'C[177]' => 74.055944,    # Propionamide:2H(3) (Acrylamide_heavy) used initially by IPAS (UniMod)
-      'C[208]' =>	105.057849,   # S-pyridylethylation 
+      'C[208]' =>	105.057849,   # S-pyridylethylation
       'C[303]' => 200.0,        # ? ABI cl-ICAT light from Bernd HuBCellRaft data
       'C[312]' => 209.0,        # ? ABI cl-ICAT heavy from Bernd HuBCellRaft data
       'C[245]' => 142.0,        # ? light from jwatts HumanTCellJCaM2.5 data
@@ -66,12 +66,17 @@ sub new {
       'E[111]' => -18.010565,   # Pyro-glu from E (UniMod)
       'F[157]' => 10.027228,    # 13C(9)15N(1) Silac label (Unimod)
       'I[119]' => 6.020129,     # 13C(6) Silac label (Unimod)
-      'L[116]' =>	3.018830 ,    #	D3_Label 
-      'L[123]' => 10.062767,    # D10_Label 
+      'L[116]' =>	3.018830 ,    #	D3_Label
+      'L[123]' => 10.062767,    # D10_Label
 
       'K[132]' => 4.02511,	    # D4_Label
       'K[272]' => 144.102063,   # ABI iTRAQ (UniMod)
-      'K[156]' => 28.031300,    # di-Methylation  (Unimod)
+      'K[142]' => 14.015650,    # Methylation  (Unimod 34)
+      'K[156]' => 28.031300,    # di-Methylation  (Unimod 36)
+      'K[188]' => 42.04695,     # tri-Methylation  (Unimod 37)
+      'K[188]' => 42.010565,    # Acetylation  (Unimod 1); K[188] also trimethylation
+      'K[196]' => 68.026215,    # Crotonyl (Unimod 1363)
+
       'K[300]' => 172.133363,   # ABI iTRAQ + di-Methylation
       'K[160]' => 32.056407,    # DiMethyl-CHD2   (Unimod)
       'K[164]' => 36.07567,     # DiMethyl-Heavy   (Unimod)
@@ -80,21 +85,34 @@ sub new {
       'K[273]' => 145.01975,    # Carboxyamidomethylated cleaved DSP-crosslinker
       'K[357]' => 229.162932,   # 6 plex
       'K[432]' => 304.199040,   # 8 plex
-      'K[467]' => 339.161662,   #	NHS-LC-Biotin 
+      'K[467]' => 339.161662,   #	NHS-LC-Biotin
 
       'L[119]' => 6.020129,     # 13C(6) Silac label (Unimod)
       'L[120]' => 7.017164,     # 13C(6)15N(1) Silac label (Unimod)
+
       'M[147]' => 15.994915,    # Oxidation (UniMod)
-      'M[163]' =>	31.989829,    #	Dioxidation (UniMod) 
+      'M[163]' =>	31.989829,    #	Dioxidation (UniMod)
+      'M[185]' => 42.010565,    # Acetylation  (Unimod 1)
+
       'N[115]' => 0.984016,     # Glyc-Asn (UniMod)
+
       'R[166]' => 10.008269,    # Silac (UniMod)
-      'R[162]' =>	6.020129,     # 13C(6) Silac label 
+      'R[162]' =>	6.020129,     # 13C(6) Silac label
+      'R[184]' => 28.0532,      # di-Methylation (Unimod)
+      'R[157]' => 0.984009,     # Citrullination (Unimod)
+
       'Q[111]' => -17.026549,   # Pyro-glu from Q (UniMod)
-      'S[167]' => 79.966331,    # Phosphorylation (UniMod)
-      'S[166]' => 79.966331,    # Phosphorylation (UniMod)
+
+      'S[167]' => 79.966331,    # Phosphorylation (UniMod 21) (incorrect)
+      'S[166]' => 79.966331,    # Phosphorylation (UniMod 21)
+      'S[129]' => 42.010565,    # Acetylation  (Unimod 1)
+
       'T[181]' => 79.966331,    # Phosphorylation (UniMod)
+      'T[143]' => 42.010565,    # Acetylation  (Unimod 1)
+
       'V[104]' => 5.016774,     # 13C(5) Silac label (Unimod)
       'V[105]' => 6.013809,     # 13C(5)15N(1) Silac label (Unimod)
+
       'Y[243]' => 79.966331,    # Phosphorylation (UniMod)
       'Y[307]' => 144.1059,     # 4 plex
 			},
@@ -145,8 +163,9 @@ sub new {
       'K[357]' => 229.2634,   # 6 plex
       'K[432]' => 304.3081,   # 8 plex
       'M[147]' => 15.9848,    # Oxidation (UniMod)
-      'M[163]' =>	31.9988,    #	Dioxidation (UniMod) 
+      'M[163]' =>	31.9988,    #	Dioxidation (UniMod)
       'N[115]' => 0.9848,     # Glyc-Asn (UniMod)
+
       'R[166]' => 9.9296,     # Silac (UniMod)
       'R[162]' =>	5.9559,     # 13C(6) Silac label
       'Q[111]' => -17.0305,   # Pyro-glu from Q (UniMod)
@@ -166,9 +185,103 @@ sub new {
   return($self);
 }
 
+sub get_modification_names {
+  my $self = shift;
+
+  my %modifications = (
+      'n[29]'  =>  'N-term di-Methylation', #  (Unimod)
+      'n[33]'  =>  'N-term DiMethyl-CHD2', #   (Unimod)
+      'n[37]'  =>  'N-term Dimethyl-Heavy', #  (Unimod)
+      'n[43]'  =>  'N-term Acetylation', #     (Unimod)
+      'n[44]'  =>  'N-term Carbamylation', #   (Unimod)
+      'n[145]' =>  'N-term iTRAQ', # (UniMod)
+      'n[230]' =>  'N-term iTRAQ 6 plex', #
+      'n[305]' =>  'N-term iTRAQ 8 plex', #
+
+      'A[85]'  =>  'Ala Methylation', # (Unimod) This may be a red herring
+      'C[143]' =>  'Cys Pyro-carbamidonmethyl', # (Unimod)
+      'C[148]' =>  'Cys Beta-methylthiolation', # (UniMod)
+      'C[160]' =>  'Cys Carbamidomethyl', #  (UniMod)Cys_CAM
+      'C[161]' =>  'Cys Carboxymethyl', # ?? Latin Square?
+      'C[174]' =>  'Cys Propionamide', #  (Acrylamide adduct) used initially by IPAS (UniMod)
+      'C[177]' =>  'Cys Propionamide:2H(3)', #  (Acrylamide_heavy) used initially by IPAS (UniMod)
+      'C[208]' =>	 'Cys S-pyridylethylation', #
+      'C[303]' =>  'Cys cl-ICAT light', #  from Bernd HuBCellRaft data
+      'C[312]' =>  'Cys cl-ICAT heavy', #  from Bernd HuBCellRaft data
+      'C[245]' =>  'Cys JW light', #  from jwatts HumanTCellJCaM2.5 data
+      'C[251]' =>  'Cys JW heavy', #  from jwatts HumanTCellJCaM2.5 data
+      'C[330]' =>  'Cys cl-ICAT light', #  (UniMod)
+      'C[339]' =>  'Cys cl-ICAT heavy', #  (Unimod)
+      'C[517]' =>  'Cys Glycopeptide capture', #  ?? (UniMod)
+      'C[545]' =>  'Cys old ICAT light', #  (UniMod)
+      'C[546]' =>  'Cys old ICAT light', #  (UniMod)
+      'C[553]' =>  'Cys old ICAT heavy', #  (UniMod)
+      'C[554]' =>  'Cys old ICAT heavy', #  (UniMod)
+      'E[111]' =>  'Glu GlyPyro-glu', #  from E (UniMod)
+      'F[157]' =>  'Phe 13C(9)15N(1) Silac label', #  (Unimod)
+      'I[119]' =>  'Ile 13C(6) Silac label', #  (Unimod)
+      'L[116]' =>  'Leu D3_Label', #
+      'L[123]' =>  'Leu D10_Label', #
+      'K[132]' =>  'Lys D4_Label', #
+      'K[272]' =>  'Lys ABI iTRAQ', #  (UniMod)
+      'K[156]' =>  'Lys di-Methylation', #   (Unimod)
+      'K[300]' =>  'Lys ABI iTRAQ + di-Methylation', #
+      'K[160]' =>  'Lys DiMethyl-CHD2', #    (Unimod)
+      'K[164]' =>  'Lys DiMethyl-Heavy', #    (Unimod)
+      'K[134]' =>	 'Lys Silac label', #  (UniMod)
+      'K[136]' =>  'Lys Silac label', #  (UniMod)
+      'K[273]' =>  'Lys Carboxyamidomethylated cleaved DSP-crosslinker', #
+      'K[357]' =>  'Lys 6 plex', #
+      'K[432]' =>  'Lys 8 plex', #
+      'K[467]' =>  'Lys NHS-LC-Biotin', #
+      'L[119]' =>  'Leu 13C(6) Silac label', #  (Unimod)
+      'L[120]' =>  'Leu 13C(6)15N(1) Silac label', #  (Unimod)
+      'M[147]' =>  'Met Oxidation', #  (UniMod)
+      'M[163]' =>  'Met Dioxidation', #  (UniMod)
+      'N[115]' =>  'Asn N-glycosylation', #  (UniMod)
+      'R[166]' =>  'Arg Silac', #  (UniMod)
+      'R[162]' =>	 'Arg 13C(6) Silac', #  label
+      'Q[111]' =>  'Gln Pyro-glu', #  from Q', #  (UniMod)
+      'S[167]' =>  'Ser Phosphorylation', #  (UniMod)
+      'S[166]' =>  'Ser Phosphorylation', #  (UniMod)
+      'T[181]' =>  'Thr Phosphorylation', #  (UniMod)
+      'V[104]' =>  'Val 13C(5) Silac label', #  (Unimod)
+      'V[105]' =>  'Val 13C(5)15N(1) Silac label', #  (Unimod)
+      'Y[243]' =>  'Tyr Phosphorylation', #  (UniMod)
+      'Y[307]' =>  'Tyr 4 plex',
+      'K[142]' =>  "Methylation  (Unimod 34)",
+      'K[156]' =>  "di-Methylation  (Unimod 36)",
+      'K[188]' =>  "tri-Methylation  (Unimod 37)",
+      'K[188]' =>  "Acetylation  (Unimod 1); K[188] also trimethylation",
+      'K[196]' =>  "Crotonyl (Unimod 1363)",
+      'M[185]' =>  "Acetylation  (Unimod 1)",
+      'R[184]' =>  "di-Methylation (Unimod)",
+      'R[157]' =>  "Citrullination (Unimod)",
+      'S[129]' =>  "Acetylation  (Unimod 1)",
+      'T[143]' =>  "Acetylation  (Unimod 1)",
+
+      # Avg masses, defined by EDeutsch software
+      'C[149]' => 'Cys [Avg] Methylthio', # EDeutsch 2016-06
+      'C[228]' => 'Cys [Avg] Nethylmaleimide', # EDeutsch 2016-06
+      'C[518]' => 'Cys [Avg] PEO-Iodoacetyl-LC-Biotin', # EDeutsch 2016-06
+      'K[162]' => 'Lys [Avg] Dimethyl:2H(4)13C(2)', # EDeutsch 2016-06
+      'K[170]' => 'Lys [Avg] Acetyl', # EDeutsch 2016-06
+      'K[242]' => 'Lys [Avg] Dicarbamidomethyl', # EDeutsch 2016-06
+      'Q[112]' => 'Gln [Avg] Pyroglu ', # EDeutsch 2016-06
+      'Q[129]' => 'Gln [Avg] Deamidated', # EDeutsch 2016-06
+      'S[201]' => 'Ser [Avg] GG', # EDeutsch 2016-06
+      'T[215]' => 'Thr [Avg] GG', # EDeutsch 2016-06
+      'Y[289]' => 'Tyr [Avg] Iodo', # EDeutsch 2016-06
+      'n[272]' => 'N-term [Avg] CLIP_TRAQ_3', # EDeutsch 2016-06
+      'n[35]' => 'N-term [Avg] Dimethyl:2H(4)13C(2)', # EDeutsch 2016-06
+      'n[58]' => 'N-term [Avg] Carbamidomethyl', # EDeutsch 2016-06
 
 
-###############################################################################
+  );
+
+  $self->{modification_names} = \%modifications;
+}
+
 
 1;
 
