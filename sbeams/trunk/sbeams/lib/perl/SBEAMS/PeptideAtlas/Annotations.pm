8552ec16a05889a4dd043a47d9e0fa079e7fd076
zsun <zsun@daa90f01-aafe-0310-8d34-a3b7110b82f3>
2019-02-26 17:46:12 +0000

    change NTT to ET

--- sbeams/trunk/sbeams/lib/perl/SBEAMS/PeptideAtlas/Annotations.pm
+++ sbeams/trunk/sbeams/lib/perl/SBEAMS/PeptideAtlas/Annotations.pm
@@ -421,7 +421,7 @@ sub get_column_defs {
     'Peptide Length' => 'Length of peptide', 
     'NET' => 'Highest number of enzymatic termini for this protein',
     'NMC' => 'Lowest number of missed cleavage for this protein',
-    'ESS' => 'Empirical suitability score, derived from peptide probability, EOS, and the number of times observed.  This is then adjusted sequence characteristics such as missed cleavage <SUP><FONT COLOR=RED>[MC]</FONT></SUP> or semi-tryptic <SUP><FONT COLOR=RED>[ST]</FONT></SUP>, or <BR> multiple genome locations <SUP><FONT COLOR=RED>[MGL]</FONT></SUP>.', 
+    'ESS' => 'Empirical suitability score, derived from peptide probability, EOS, and the number of times observed.  This is then adjusted sequence characteristics such as missed cleavage <SUP><FONT COLOR=RED>[MC]</FONT></SUP> or enzyme termini <SUP><FONT COLOR=RED>[ET]</FONT></SUP>, or <BR> multiple genome locations <SUP><FONT COLOR=RED>[MGL]</FONT></SUP>.', 
     'PSS' => 'Predicted suitability score, derived from combining publicly available algorithms (Peptide Sieve, STEPP, ESPP, APEX, Detectability Predictor)', 
 
     'STEPP' => 'Predicted peptide score calculated by STEPP algorithm',
